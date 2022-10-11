de193e918ea7f5162e97a5a199a2bf6a3b9e46f3
Ajil Oommen <ajil.oommen@egovernments.org>
2022-10-11 10:57:03 +0530

    [hlm-737]: Remove serializer model

--- lib/models/serializer_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:health_campaigns_flutter/models/address/address_model.dart';
-import 'package:health_campaigns_flutter/models/sync/sync_model.dart';
-
-abstract class SerializerModel {
-  SyncObjectModelType get type;
-}
