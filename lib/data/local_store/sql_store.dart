ef6f1ca4371ab3c5dfeb774a87aa6bad50a28e9d
Ajil Oommen <ajil.oommen@egovernments.org>
2022-10-11 10:57:27 +0530

    [hlm-737]: Move audit model to dir

--- lib/data/local_store/sql_store.dart
+++ lib/data/local_store/sql_store.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:health_campaigns_flutter/data/local_store/tables/delivery_table.dart';
 import 'package:health_campaigns_flutter/data/local_store/tables/campaign_registration_table.dart';
-import 'package:health_campaigns_flutter/models/individual/individual_model.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
