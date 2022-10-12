6b150c34d98aee6bea899b588e50462627d15d9e
Ajil Oommen <ajil.oommen@egovernments.org>
2022-10-12 12:35:30 +0530

    [hlm-737]: Complete theme base setup

--- packages/digit_ui_components/lib/theme/digit_theme.dart
+++ packages/digit_ui_components/lib/theme/digit_theme.dart
@@ -1,12 +1,26 @@
 library digit_theme;
 
 import 'package:digit_ui_components/theme/colors.dart';
+import 'package:digit_ui_components/theme/typography.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DigitTheme {
   static const DigitTheme _instance = DigitTheme._internal();
   static DigitTheme get instance => _instance;
 
-  DigitColors get colors => DigitColors();
+  DigitColors get colors => const DigitColors();
+
+  DigitMobileTypography get mobileTypography => DigitMobileTypography(
+        normalBase: GoogleFonts.roboto(),
+        displayBase: GoogleFonts.robotoCondensed(),
+        light: colors.davyGray,
+        normal: colors.woodsmokeBlack,
+      );
 
   const DigitTheme._internal();
+
+  ThemeData get mobileTheme {
+    return ThemeData(textTheme: mobileTypography.textTheme);
+  }
 }
