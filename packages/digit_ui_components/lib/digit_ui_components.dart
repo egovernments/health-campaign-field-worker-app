3297e7da82503c5ae48b6b70bbb3a32cc75b069a
Ajil Oommen <ajil.oommen@egovernments.org>
2022-10-12 11:36:35 +0530

    [hlm-737]: Add basic typography

--- packages/digit_ui_components/lib/digit_ui_components.dart
+++ packages/digit_ui_components/lib/digit_ui_components.dart
@@ -1,1 +1,14 @@
 library digit_ui_components;
+
+import 'package:google_fonts/google_fonts.dart';
+
+class DigitUiComponent {
+  static const DigitUiComponent _instance = DigitUiComponent._internal();
+  static DigitUiComponent get instance => _instance;
+
+  const DigitUiComponent._internal();
+
+  Future<void> initThemeComponents() async {
+    GoogleFonts.config.allowRuntimeFetching = false;
+  }
+}
--- packages/digit_ui_components/lib/theme/typography.dart
+++ packages/digit_ui_components/lib/theme/typography.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class DigitMobileTypography {
+  final Color? _textColorNormal;
+  final Color? _textColorLight;
+
+  const DigitMobileTypography({Color? normal, Color? light})
+      : _textColorLight = light,
+        _textColorNormal = normal;
+
+  TextStyle get roboto {
+    return GoogleFonts.roboto(color: _textColorNormal);
+  }
+
+  TextStyle get robotoLight {
+    return GoogleFonts.roboto(color: _textColorLight);
+  }
+
+  TextStyle get robotoCondensed {
+    return GoogleFonts.robotoCondensed(color: _textColorNormal);
+  }
+
+  TextStyle get robotoCondensedLight {
+    return GoogleFonts.robotoCondensed(color: _textColorLight);
+  }
+
+  TextTheme get textTheme {
+    return TextTheme(
+      headlineLarge: roboto.copyWith(fontSize: 24, fontWeight: FontWeight.w700),
+      headlineMedium:
+          roboto.copyWith(fontSize: 18, fontWeight: FontWeight.w700),
+      headlineSmall: roboto.copyWith(fontSize: 16, fontWeight: FontWeight.w700),
+    );
+  }
+}
