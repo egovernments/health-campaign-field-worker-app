483e04af9a56398cd0c7d3138cbf237d5af3a9ba
Ajil Oommen <ajil.oommen@egovernments.org>
2022-10-18 15:33:43 +0530

    [hlm-708]: Location service bloc

---
+++ packages/digit_components/lib/blocs/location/location.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:location/location.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'location.freezed.dart';
+
+typedef LocationStateEmitter = Emitter<LocationState>;
+
+/// LocationBLoC
+///
+/// Follow setup instructions for appropriate platform
+/// Android - <https://docs.page/Lyokone/flutterlocation/installation/android>
+/// iOS - <https://docs.page/Lyokone/flutterlocation/installation/ios>
+/// macOS - <https://docs.page/Lyokone/flutterlocation/installation/macos>
+class LocationBloc extends Bloc<LocationEvent, LocationState> {
+  LocationBloc() : super(const LocationState()) {
+    on<LoadLocationEvent>(_handleLoadLocation);
+  }
+
+  FutureOr<void> _handleLoadLocation(
+    LoadLocationEvent event,
+    LocationStateEmitter emit,
+  ) async {
+    emit(state.copyWith(loading: true));
+    try {
+      final location = await getLocation();
+      emit(state.copyWith(
+        latitude: location.latitude,
+        longitude: location.longitude,
+        accuracy: location.accuracy,
+      ));
+    } catch (error) {
+      rethrow;
+    } finally {
+      emit(state.copyWith(loading: false));
+    }
+  }
+}
+
+@freezed
+class LocationEvent with _$LocationEvent {
+  const factory LocationEvent.load() = LoadLocationEvent;
+}
+
+@freezed
+class LocationState with _$LocationState {
+  const factory LocationState({
+    double? latitude,
+    double? longitude,
+    double? accuracy,
+    @Default(false) bool loading,
+  }) = _LocationState;
+}
---
+++ packages/digit_components/test/bloc/location_test.dart
@@ -0,0 +1,52 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:digit_components/blocs/location/location.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:location/location.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockLocationPlatform extends Mock
+    with MockPlatformInterfaceMixin
+    implements LocationPlatform {}
+
+void main() {
+  const mockLat = -25.953724;
+  const mockLng = 32.588711;
+
+  group('Location bloc', () {
+    blocTest(
+      'loads with mock latLng',
+      build: () => LocationBloc(),
+      setUp: () {
+        final mock = MockLocationPlatform();
+        when(mock.getLocation).thenAnswer(
+          (invocation) async => LocationData(
+            latitude: mockLat,
+            longitude: mockLng,
+            accuracy: 1.0,
+          ),
+        );
+
+        setLocationInstance(mock);
+      },
+      act: (bloc) => bloc.add(const LoadLocationEvent()),
+      expect: () => [
+        isA<LocationState>().having(
+          (state) => state.loading,
+          'will enter loading state',
+          true,
+        ),
+        isA<LocationState>().having(
+          (state) => state.latitude,
+          'will fetch mocked lat',
+          mockLat,
+        ),
+        isA<LocationState>().having(
+          (state) => state.loading,
+          'will enter loading state',
+          false,
+        ),
+      ],
+    );
+  });
+}
