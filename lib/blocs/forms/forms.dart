033349f4fe7c97ad8f26525fcfb42adcd32a2840
Ajil Oommen <ajil.oommen@egovernments.org>
2022-10-19 14:58:55 +0530

    [hlm-914]: Intermittent updates

--- lib/blocs/forms/forms.dart
+++ lib/blocs/forms/forms.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:forms_engine/models/schema_object/schema_object.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:health_campaigns_flutter/data/fake_schema.dart';
+import 'package:health_campaigns_flutter/models/registration_delivery/registration_delivery.dart';
+import 'package:health_campaigns_flutter/utils/utils.dart';
 
 part 'forms.freezed.dart';
 
@@ -12,23 +15,44 @@ typedef FormsStateEmitter = Emitter<FormsState>;
 
 class FormsBloc extends Bloc<FormsEvent, FormsState> {
   FormsBloc() : super(const FormsState()) {
-    on<FormsLoadEvent>(_loadForm);
-    on<FormsUpdateEvent>(_updateForm);
+    on<FormsLoadEvent>(_handleLoadForm);
+    on<FormsUpdateEvent>(_handleUpdateForm);
+    on<FormsCreateMappingEvent>(_handleCreateMapping);
   }
 
-  FutureOr<void> _loadForm(FormsLoadEvent event, FormsStateEmitter emit) {
+  FutureOr<void> _handleLoadForm(FormsLoadEvent event, FormsStateEmitter emit) {
     final schema = SchemaObject.fromJson(json.decode(fakeSchema));
     emit(state.copyWith(schema: schema));
   }
 
-  void _updateForm(FormsUpdateEvent event, FormsStateEmitter emit) {
+  void _handleUpdateForm(FormsUpdateEvent event, FormsStateEmitter emit) {
     emit(state.copyWith(schema: event.object));
+  }
+
+  void _handleCreateMapping(
+    FormsCreateMappingEvent event,
+    FormsStateEmitter emit,
+  ) {
+    final propertiesMap = state.schema?.pages.entries
+        .map((e) => e.value)
+        .map((e) => e.toJson())
+        .expand((element) => element.entries);
+
+    if (propertiesMap == null) {
+      throw AppException('Invalid schema output. Data should not be empty');
+    }
+
+    final dataMap = Map.fromEntries(propertiesMap);
+    final mapperModel = RegistrationDeliveryMapperModel.fromJson(dataMap);
+    debugPrint('print: $mapperModel');
   }
 }
 
 @freezed
 class FormsEvent with _$FormsEvent {
   const factory FormsEvent.load() = FormsLoadEvent;
+
+  const factory FormsEvent.createMapping() = FormsCreateMappingEvent;
 
   const factory FormsEvent.update(SchemaObject object) = FormsUpdateEvent;
 }
