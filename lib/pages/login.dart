d43985d1a24352a887d02ca49502c273fac85e6a
Ajil Oommen <ajil.oommen@egovernments.org>
2022-10-21 10:56:36 +0530

    [hlm-991]: Login page tests

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,7 +2,6 @@ import 'package:digit_components/digit_components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:health_campaigns_flutter/blocs/auth/auth.dart';
-import 'package:health_campaigns_flutter/router/app_router.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -16,19 +15,19 @@ class LoginPage extends StatelessWidget {
       body: ScrollableContent(
         children: [
           DigitCard(
-            child: BlocBuilder<AuthBloc, AuthState>(
-              builder: (context, state) => Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Login',
-                    style: theme.textTheme.displayMedium,
-                  ),
-                  const DigitTextField(label: 'User ID'),
-                  const DigitTextField(label: 'Password'),
-                  const SizedBox(height: 16),
-                  DigitElevatedButton(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Login',
+                  style: theme.textTheme.displayMedium,
+                ),
+                const DigitTextField(label: 'User ID'),
+                const DigitTextField(label: 'Password'),
+                const SizedBox(height: 16),
+                BlocBuilder<AuthBloc, AuthState>(
+                  builder: (context, state) => DigitElevatedButton(
                     onPressed: state.loading
                         ? null
                         : () {
@@ -43,19 +42,19 @@ class LoginPage extends StatelessWidget {
                       child: Text('Login'),
                     ),
                   ),
-                  TextButton(
-                    onPressed: () => DigitDialog.show(
-                      context,
-                      title: 'Forgot Password?',
-                      content:
-                          'Please contact the administrator if you have forgotten your password',
-                      primaryActionLabel: 'OK',
-                      primaryAction: () => context.router.pop(),
-                    ),
-                    child: const Center(child: Text('Forgot Password?')),
+                ),
+                TextButton(
+                  onPressed: () => DigitDialog.show(
+                    context,
+                    title: 'Forgot Password?',
+                    content:
+                        'Please contact the administrator if you have forgotten your password',
+                    primaryActionLabel: 'OK',
+                    primaryAction: () => Navigator.pop(context),
                   ),
-                ],
-              ),
+                  child: const Center(child: Text('Forgot Password?')),
+                ),
+              ],
             ),
           ),
         ],
