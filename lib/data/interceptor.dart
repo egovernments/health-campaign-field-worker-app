ab3e7b7d06e26b46dfa1d14e63b98b651ab16563
Naveen <naveen.j@egovernments.org>
2022-10-31 00:39:57 +0530

    [hlm-1030]: removed debugger

--- lib/data/interceptor.dart
+++ lib/data/interceptor.dart
@@ -2,12 +2,10 @@
 
 import 'package:digit_components/utils/app_logger.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 
 class DioInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    debugPrint('body: ${options.data}');
     AppLogger.instance.info(options.uri);
     super.onRequest(options, handler);
   }
