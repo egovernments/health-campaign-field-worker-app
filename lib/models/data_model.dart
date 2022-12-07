44ab1f7864aa6032af0c7a34431d2763ea14660b
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-08 02:39:05 +0530

    [hlm-707]: partial project model

--- lib/models/data_model.dart
+++ lib/models/data_model.dart
@@ -2,5 +2,28 @@ library models;
 
 import 'package:dart_mappable/dart_mappable.dart';
 
+export 'project/project.dart';
+export 'data_model.mapper.g.dart';
+
 @MappableClass()
-abstract class DataModel {}
+abstract class DataModel {
+  final AuditDetails? auditDetails;
+
+  const DataModel({this.auditDetails});
+}
+
+@MappableClass()
+class AuditDetails {
+  final String createdBy;
+  final String lastModifiedBy;
+  final int createdTime;
+  final int lastModifiedTime;
+
+  const AuditDetails({
+    required this.createdBy,
+    required this.createdTime,
+    String? lastModifiedBy,
+    int? lastModifiedTime,
+  })  : lastModifiedBy = lastModifiedBy ?? createdBy,
+        lastModifiedTime = lastModifiedTime ?? createdTime;
+}
--- lib/models/registration/registration_model.dart
+++ lib/models/registration/registration_model.dart
@@ -1,37 +1,37 @@
-// ignore_for_file: invalid_annotation_target
-
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:health_campaigns_flutter/models/address/address_model.dart';
-import 'package:health_campaigns_flutter/models/audit/audit_model.dart';
-import 'package:health_campaigns_flutter/models/individual/individual_model.dart';
-import 'package:health_campaigns_flutter/models/sync/sync_model.dart';
-
-part 'registration_model.freezed.dart';
-part 'registration_model.g.dart';
-
-@freezed
-class CampaignRegistrationModel with _$CampaignRegistrationModel {
-  @JsonSerializable(explicitToJson: true)
-  const factory CampaignRegistrationModel({
-    String? additionalFields,
-    required AddressModel address,
-    required String administrativeUnit,
-    required ApiMode apiMode,
-    AuditDetailsModel? auditDetails,
-    required String campaignId,
-    required String clientReferenceId,
-
-    /// [dateOfRegistration] in millisecondsSinceEpoch
-    required int dateOfRegistration,
-    String? beneficiaryId,
-    String? beneficiaryType,
-    required List<IndividualModel> individuals,
-    LatLngModel? location,
-    required int numberOfIndividuals,
-    required String tenantId,
-    required SyncObjectModelType type,
-  }) = _CampaignRegistrationModel;
-
-  factory CampaignRegistrationModel.fromJson(Map<String, dynamic> json) =>
-      _$CampaignRegistrationModelFromJson(json);
-}
+// // ignore_for_file: invalid_annotation_target
+//
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:health_campaigns_flutter/models/address/address_model.dart';
+// import 'package:health_campaigns_flutter/models/audit/audit_model.dart';
+// import 'package:health_campaigns_flutter/models/individual/individual_model.dart';
+// import 'package:health_campaigns_flutter/models/sync/sync_model.dart';
+//
+// part 'registration_model.freezed.dart';
+// part 'registration_model.g.dart';
+//
+// @freezed
+// class CampaignRegistrationModel with _$CampaignRegistrationModel {
+//   @JsonSerializable(explicitToJson: true)
+//   const factory CampaignRegistrationModel({
+//     String? additionalFields,
+//     required AddressModel address,
+//     required String administrativeUnit,
+//     required ApiMode apiMode,
+//     AuditDetailsModel? auditDetails,
+//     required String campaignId,
+//     required String clientReferenceId,
+//
+//     /// [dateOfRegistration] in millisecondsSinceEpoch
+//     required int dateOfRegistration,
+//     String? beneficiaryId,
+//     String? beneficiaryType,
+//     required List<IndividualModel> individuals,
+//     // LatLngModel? location,
+//     required int numberOfIndividuals,
+//     required String tenantId,
+//     required SyncObjectModelType type,
+//   }) = _CampaignRegistrationModel;
+//
+//   factory CampaignRegistrationModel.fromJson(Map<String, dynamic> json) =>
+//       _$CampaignRegistrationModelFromJson(json);
+// }
