c180a6af750108f129eb5993cc55b31260b88a90
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-07 19:58:42 +0530

    [hlm-707]: Remove form components

--- lib/data/repositories/fake_store/fake_store.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-abstract class FakeDataStore<T> {
-  List<T> getFakeDataList([int length = 10]) =>
-      List.generate(length, (index) => fake);
-
-  @protected
-  T get fake;
-}
