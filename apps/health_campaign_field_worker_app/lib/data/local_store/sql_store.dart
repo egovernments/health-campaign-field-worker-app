67795540275ebad4e8e9061a254148050530fad5
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-12 18:30:01 +0530

    [hlm-1426]: Rename library

---
+++ apps/health_campaign_field_worker_app/lib/data/local_store/sql_store.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:health_campaign_field_worker_app/data/local_store/tables/delivery_table.dart';
+import 'package:health_campaign_field_worker_app/data/local_store/tables/campaign_registration_table.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+
+part 'sql_store.g.dart';
+
+@DriftDatabase(tables: [
+  CampaignRegistrationTable,
+  HouseholdTable,
+  IndividualTable,
+  IndividualIdentifierTable,
+  DeliveryTable,
+  DeliveryResourceTable,
+])
+class LocalSqlDataStore extends _$LocalSqlDataStore {
+  LocalSqlDataStore() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  static LazyDatabase _openConnection() {
+    return LazyDatabase(() async {
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(p.join(dbFolder.path, 'db.sqlite'));
+
+      return NativeDatabase(file);
+    });
+  }
+}
