5e2377b25bd9e2ac1bc3dbfed56022e803d3a465
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-15 15:08:38 +0530

    [hlm-707]: Update template

--- mason_templates/freezed_bloc/__brick__/{{{name/snakeCase()}}}/{{{name.snakeCase()}}}.dart
+++ mason_templates/freezed_bloc/__brick__/{{{name/snakeCase()}}}/{{{name.snakeCase()}}}.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part '{{{name}}}.freezed.dart';
 // Uncomment the following line if the Bloc needs to handle JSON Serialization
 // part '{{{name}}}.g.dart';
+part '../../{{{name.snakeCase()}}}/{{{name.snakeCase()}}}.freezed.dart';
 
 
 typedef {{name.pascalCase()}}Emitter = Emitter<{{name.pascalCase()}}State>;
@@ -12,30 +12,14 @@ class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.
   {{name.pascalCase()}}Bloc(super.initialState) {
 
   }
-
 }
 
 @freezed
 class {{name.pascalCase()}}Event with _${{name.pascalCase()}}Event {
   const {{name.pascalCase()}}._();
-  // const factory AuthEvent.login({
-  //   required String userId,
-  //   required String password,
-  // }) = AuthLoginEvent;
-  //
-  // const factory AuthEvent.logout() = AuthLogoutEvent;
 }
 
 @freezed
 class {{name.pascalCase()}}State with _${{name.pascalCase()}}State {
   const {{name.pascalCase()}}State._();
-
-  // const factory AuthState({
-  //   @Default(false) bool initialized,
-  //   @Default(false) bool loading,
-  //   String? accessToken,
-  //   String? refreshToken,
-  // }) = _AuthState;
-  //
-  // bool get isAuthenticated => accessToken == null ? false : true;
 }
