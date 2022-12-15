940207059c864efda6b4e5ae466dd61cd40c58ba
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-15 23:06:42 +0530

    [hlm-707]: Freezed bloc

--- mason_templates/freezed_bloc/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
+++ mason_templates/freezed_bloc/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -10,17 +10,29 @@ typedef {{name.pascalCase()}}Emitter = Emitter<{{name.pascalCase()}}State>;
 
 class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.pascalCase()}}State> {
   {{name.pascalCase()}}Bloc(super.initialState) {
-
+    {{#events}}on(_handle{{..pascalCase()}});
+    {{/events}}
   }
-
+  {{#events}}
+  FutureOr<void> _handle{{..pascalCase()}}(
+    {{name.pascalCase()}}{{..pascalCase()}}Event event,
+    {{name.pascalCase()}}Emitter emit,
+  ) async {
+    // handle logic for {{..camelCase()}} here
+  }
+  {{/events}}
 }
 
 @freezed
 class {{name.pascalCase()}}Event with _${{name.pascalCase()}}Event {
-  const {{name.pascalCase()}}State._();
+  {{^events}}const {{name.pascalCase()}}Event._();{{/events}}
+  {{#events}}const {{name.pascalCase()}}Event.{{..camelCase()}}() = {{name.pascalCase()}}{{..pascalCase()}}Event;
+  {{/events}}
 }
 
 @freezed
 class {{name.pascalCase()}}State with _${{name.pascalCase()}}State {
-  const {{name.pascalCase()}}State._();
+  {{^states}}const {{name.pascalCase()}}State._();{{/states}}
+  {{#states}}const {{name.pascalCase()}}State.{{..camelCase()}}() = {{name.pascalCase()}}{{..pascalCase()}}State;
+  {{/states}}
 }
