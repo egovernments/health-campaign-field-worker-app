228c60360ad4849d2cedde4cd4b7ee1edd5dc74c
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-15 17:32:50 +0530

    [hlm-707]: Update {{name.snakeCase()}}.dart

--- mason_templates/freezed_bloc/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
+++ mason_templates/freezed_bloc/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -1,1 +1,26 @@
 // GENERATED using mason_cli
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '{{name.snakeCase()}}.freezed.dart';
+
+typedef {{name.pascalCase()}}Emitter = Emitter<{{name.pascalCase()}}State>;
+
+class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.pascalCase()}}State> {
+  {{name.pascalCase()}}Bloc(super.initialState) {
+
+  }
+
+}
+
+@freezed
+class {{name.pascalCase()}}Event with _${{name.pascalCase()}}Event {
+  const {{name.pascalCase()}}State._();
+}
+
+@freezed
+class {{name.pascalCase()}}State with _${{name.pascalCase()}}State {
+  const {{name.pascalCase()}}State._();
+}
