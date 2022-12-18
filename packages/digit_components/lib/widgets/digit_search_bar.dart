0f54fcb2274928429b67741f23af293781175265
Naveen <naveen.j@egovernments.org>
2022-12-19 01:15:17 +0530

    added trailing -commas

--- packages/digit_components/lib/widgets/digit_search_bar.dart
+++ packages/digit_components/lib/widgets/digit_search_bar.dart
@@ -18,31 +18,32 @@ class DigitSearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Card(
-        shape: RoundedRectangleBorder(
-          side: BorderSide(color: theme.scaffoldBackgroundColor, width: 1),
-          borderRadius: BorderRadius.circular(
-              borderRadious != null ? (borderRadious! * 3) : 30),
-        ),
-        margin: margin,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: TextField(
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: hintText ?? 'Enter the field details',
-              filled: true,
-              fillColor: theme.cardColor,
-              contentPadding: contentPadding ??
-                  const EdgeInsets.only(left: 14.0, bottom: 6.0, top: 8.0),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(borderRadious ?? 10.0),
-                  borderSide: BorderSide(color: theme.cardColor)),
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: theme.scaffoldBackgroundColor),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: theme.scaffoldBackgroundColor, width: 1),
+        borderRadius: BorderRadius.circular(
+            borderRadious != null ? (borderRadious! * 3) : 30),
+      ),
+      margin: margin,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: hintText ?? 'Enter the field details',
+            filled: true,
+            fillColor: theme.cardColor,
+            contentPadding: contentPadding ??
+                const EdgeInsets.only(left: 14.0, bottom: 6.0, top: 8.0),
+            focusedBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(borderRadious ?? 10.0),
-              ),
+                borderSide: BorderSide(color: theme.cardColor)),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: theme.scaffoldBackgroundColor),
+              borderRadius: BorderRadius.circular(borderRadious ?? 10.0),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
