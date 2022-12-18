224d45f8f25bf3730543c9c6061350747a71aa5c
Naveen <naveen.j@egovernments.org>
2022-12-19 00:45:49 +0530

    added test cases

--- packages/digit_components/lib/widgets/digit_outline_button.dart
+++ packages/digit_components/lib/widgets/digit_outline_button.dart
@@ -1,25 +1,25 @@
-import 'package:digit_components/theme/digit_theme.dart';
 import 'package:flutter/material.dart';
 
-class OutLineButton extends StatelessWidget {
+class DigitOutLineButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
 
-  const OutLineButton(
+  const DigitOutLineButton(
       {super.key, required this.label, required this.onPressed});
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return OutlinedButton(
         onPressed: onPressed,
         style: OutlinedButton.styleFrom(
-          side: BorderSide(color: DigitTheme.instance.colorScheme.secondary),
+          side: BorderSide(color: theme.colorScheme.secondary),
         ),
         child: Padding(
             padding: const EdgeInsets.only(left: 15, right: 15),
             child: Text(
               label,
               style: TextStyle(
-                color: DigitTheme.instance.colorScheme.secondary,
+                color: theme.colorScheme.secondary,
               ),
             )));
   }
