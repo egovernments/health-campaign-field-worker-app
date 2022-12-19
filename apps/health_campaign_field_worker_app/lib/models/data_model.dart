40d2d82769d71f0bc91a0c1e6078e1812fe5e64c
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-20 01:32:31 +0530

    [hlm-707]: Export and generate models

--- apps/health_campaign_field_worker_app/lib/models/data_model.dart
+++ apps/health_campaign_field_worker_app/lib/models/data_model.dart
@@ -2,13 +2,31 @@ library models;
 
 import 'package:dart_mappable/dart_mappable.dart';
 
-export 'project/project.dart';
 export 'oplog/oplog_entry.dart';
-export 'individual.dart';
+
+export 'address.dart';
+export 'address_type.dart';
+export 'boundary.dart';
+export 'document.dart';
+export 'facility.dart';
+export 'gender.dart';
 export 'household.dart';
+export 'household_member.dart';
+export 'identifier.dart';
+export 'individual.dart';
 export 'product.dart';
 export 'product_variant.dart';
-export 'household_member.dart';
+export 'project.dart';
+export 'project_beneficiary.dart';
+export 'project_facility.dart';
+export 'project_product_variant.dart';
+export 'project_resource.dart';
+export 'project_staff.dart';
+export 'project_type.dart';
+export 'target.dart';
+export 'task.dart';
+export 'task_resource.dart';
+
 export 'data_model.mapper.g.dart';
 
 @MappableClass()
