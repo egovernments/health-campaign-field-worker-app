9922ae06b0682e781fbaf52a0541af53490830e0
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-20 21:38:19 +0530

    [hlm-707]: nullables

--- apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/boundary.dart
+++ apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/boundary.dart
@@ -3,10 +3,10 @@ import 'package:drift/drift.dart';
 class BoundaryTable extends Table {
   TextColumn get code => text()();
   TextColumn get name => text()();
-  TextColumn get label => text()();
-  TextColumn get latitude => text()();
-  TextColumn get longitude => text()();
-  TextColumn get materializedPath => text()();
+  TextColumn get label => text().nullable()();
+  TextColumn get latitude => text().nullable()();
+  TextColumn get longitude => text().nullable()();
+  TextColumn get materializedPath => text().nullable()();
   
 
   @override
