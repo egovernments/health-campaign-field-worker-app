1f4b18d910d24fb3888dece1fb198383efdbce0d
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-20 21:39:47 +0530

    [hlm-707]: Move models to /tables

--- mason_templates/digit_entity/__brick__/data/local_store/sql_store/tables/{{name.snakeCase()}}.dart
+++ mason_templates/digit_entity/__brick__/data/local_store/sql_store/tables/{{name.snakeCase()}}.dart
@@ -1,7 +1,7 @@
 import 'package:drift/drift.dart';
 
 class {{name.pascalCase()}}Table extends Table {
-  {{#sqlAttributes}}{{columnType.pascalCase()}}Column get {{name}} => {{type.camelCase()}}()();
+  {{#sqlAttributes}}{{columnType.pascalCase()}}Column get {{name}} => {{type.camelCase()}}(){{#nullable}}.nullable(){{/nullable}}{{#isPk}}.unique(){{/isPk}}();
   {{/sqlAttributes}}
 
   @override
