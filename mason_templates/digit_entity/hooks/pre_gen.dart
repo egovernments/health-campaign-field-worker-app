4fb2e88e77e0147d3b69a8202b72ef33e39a4df2
Ajil Oommen <ajil.oommen@egovernments.org>
2022-12-20 21:21:12 +0530

    [hlm-707]: Update template

--- mason_templates/digit_entity/hooks/pre_gen.dart
+++ mason_templates/digit_entity/hooks/pre_gen.dart
@@ -8,4 +8,33 @@ void run(HookContext context) {
   final model = Mapper.fromMap<ConfigModel>(variables);
   logger.info(model.toJson());
   context.vars = model.toMap();
+
+  final sqlAttributes = <AttributeModel>[
+    ...model.attributes.map((e) {
+      final type = _getSqlType(e.type);
+      return e.copyWith(type: type);
+    }),
+  ];
+}
+
+String _getSqlType(String dartType) {
+  String type;
+  switch (dartType) {
+    case 'String':
+      type = 'Text';
+      break;
+    case 'int':
+      type = 'Integer';
+      break;
+    case 'double':
+      type = 'Real';
+      break;
+    case 'bool':
+      type = 'Boolean';
+      break;
+    default:
+      type = 'Text';
+  }
+
+  return type;
 }
