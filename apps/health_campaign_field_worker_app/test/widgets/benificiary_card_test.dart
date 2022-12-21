f8c879276693183e6d77e1004a4d8b06d0a975f2
Naveen <naveen.j@egovernments.org>
2022-12-21 11:16:50 +0530

    bug fixes

--- apps/health_campaign_field_worker_app/test/widgets/benificiary_card_test.dart
+++ apps/health_campaign_field_worker_app/test/widgets/benificiary_card_test.dart
@@ -1,18 +1,18 @@
-import 'package:health_campaign_field_worker_app/widgets/benificiary/benificiary_card.dart';
+import 'package:health_campaign_field_worker_app/widgets/beneficiary/beneficiary_card.dart';
 
 import '../widget_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group(
-    'Benificiary Card Test',
+    'Beneficiary Card Test',
     () {
       testWidgets(
         'Widget is created without errors',
         (widgetTester) async {
           await widgetTester.pumpWidget(
             const WidgetApp(
-              child: BenificiaryCard(
+              child: BeneficiaryCard(
                 description: '219 / Pemba (200mts)',
                 subtitle: '4 Members',
                 status: 'Delivered',
@@ -21,7 +21,7 @@ void main() {
             ),
           );
           expect(
-            find.widgetWithText(BenificiaryCard, '219 / Pemba (200mts)'),
+            find.widgetWithText(BeneficiaryCard, '219 / Pemba (200mts)'),
             findsOneWidget,
           );
         },
