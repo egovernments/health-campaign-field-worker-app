7fd137d81e69dffec579b2511dad025afdf6863d
Naveen <naveen.j@egovernments.org>
2022-12-22 12:50:12 +0530

    fixed typo

--- apps/health_campaign_field_worker_app/test/widgets/beneficary_inbox_card.test.dart
+++ apps/health_campaign_field_worker_app/test/widgets/beneficary_inbox_card.test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group(
-    'BenificiaryInboxCard Test',
+    'BeneficiaryInboxCard Test',
     () {
       testWidgets(
         'Widget is created without errors',
