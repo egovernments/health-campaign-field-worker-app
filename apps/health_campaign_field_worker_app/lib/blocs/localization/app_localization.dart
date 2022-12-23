df9e61990b2ffc9f07b1b5044b24191008439b3f
Naveen <naveen.j@egovernments.org>
2022-12-23 10:55:06 +0530

    added localization support

--- apps/health_campaign_field_worker_app/lib/blocs/localization/app_localization.dart
+++ apps/health_campaign_field_worker_app/lib/blocs/localization/app_localization.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../models/localization/localization_model.dart';
+import '../../utils/constants.dart';
 import 'app_localizations_delegate.dart';
 import 'localization.dart';
 
@@ -17,8 +19,19 @@ class AppLocalizations {
       AppLocalizationsDelegate();
 
   Future<bool> load() async {
-    // Read the Data from Local DB
-    _localizedStrings = [];
+    // Make API call to local DB and fetch the stored Localizations. and assign the values to _localizedStrings
+    if (scaffoldMessengerKey.currentContext != null) {
+      _localizedStrings = (BlocProvider.of<LocalizationBloc>(
+                scaffoldMessengerKey.currentContext!,
+              ).state.locaization !=
+              null
+          ? BlocProvider.of<LocalizationBloc>(
+              scaffoldMessengerKey.currentContext!,
+            ).state.locaization?.messages
+          : [])!;
+
+      return false;
+    }
 
     return true;
   }
--- apps/health_campaign_field_worker_app/lib/utils/constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/constants.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants {
-  static const String baseURL = 'https://health-dev.digit.org/';
+  static const String baseURL = 'https://mgramseva-qa.egov.org.in/';
   static const String mdmdsApiEndPoint = 'egov-mdms-service/v1/_search';
 }
+
+/// By using this key, we can push pages without context
+final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
