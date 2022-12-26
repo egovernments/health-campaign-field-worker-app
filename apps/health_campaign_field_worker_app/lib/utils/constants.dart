557ab5d0e48b24db4804e43e4b32a181194beff2
Naveen <naveen.j@egovernments.org>
2022-12-26 16:23:43 +0530

    added custom method which returns apiendpoint

--- apps/health_campaign_field_worker_app/lib/utils/constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/constants.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/app_initilization/app_initilization.dart';
 
 class Constants {
   static const String baseURL = 'https://health-dev.digit.org/';
   static const String mdmsApiEndPoint = 'egov-mdms-service/v1/_search';
+  static getEndPoint(BuildContext context, service, String action) {
+    final actionResult = context
+        .read<AppInitilizationBloc>()
+        .state
+        .localizationList
+        .firstWhere((element) => element.service == service)
+        .actions
+        ?.first;
+
+    switch (action) {
+      case 'CREATE':
+        return actionResult?.create;
+      case 'SEARCH':
+        return actionResult?.search;
+      case 'UPDATE':
+        return actionResult?.update;
+      case 'LOGIN':
+        return actionResult?.login;
+    }
+  }
 }
 
 /// By using this key, we can push pages without context
