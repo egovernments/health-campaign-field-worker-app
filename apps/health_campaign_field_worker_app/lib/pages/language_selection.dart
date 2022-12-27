52e3f15f1b15c9d0278028af3089cbe74981fdca
Naveen <naveen.j@egovernments.org>
2022-12-27 16:13:42 +0530

    added localization key

--- apps/health_campaign_field_worker_app/lib/pages/language_selection.dart
+++ apps/health_campaign_field_worker_app/lib/pages/language_selection.dart
@@ -6,6 +6,7 @@ import 'package:digit_components/digit_components.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../blocs/app_config/app_config.dart';
 import '../router/app_router.dart';
+import '../utils/18KeyConstants.dart';
 
 class LanguageSelectionPage extends StatelessWidget {
   const LanguageSelectionPage({Key? key}) : super(key: key);
@@ -36,7 +37,7 @@ class LanguageSelectionPage extends StatelessWidget {
                   onLanguageSubmit: () =>
                       context.router.push(const LoginRoute()),
                   onLanguageChange: (val) {},
-                  languageSubmitLabel: 'Continue',
+                  languageSubmitLabel: i18.common.coreCommonContinue,
                 );
               },
             ),
