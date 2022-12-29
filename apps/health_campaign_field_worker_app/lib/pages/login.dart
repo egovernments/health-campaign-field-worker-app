c67910a858c281dc4b9eeedca32d799cb33fa199
Naveen <naveen.j@egovernments.org>
2022-12-29 12:40:23 +0530

    added props to text-field

--- apps/health_campaign_field_worker_app/lib/pages/login.dart
+++ apps/health_campaign_field_worker_app/lib/pages/login.dart
@@ -23,8 +23,14 @@ class LoginPage extends StatelessWidget {
                   'Login',
                   style: theme.textTheme.displayMedium,
                 ),
-                const DigitTextField(label: 'User ID'),
-                const DigitTextField(label: 'Password'),
+                const DigitTextField(
+                  label: 'User ID',
+                  message: "",
+                ),
+                const DigitTextField(
+                  label: 'Password',
+                  message: "",
+                ),
                 const SizedBox(height: 16),
                 BlocBuilder<AuthBloc, AuthState>(
                   builder: (context, state) => DigitElevatedButton(
