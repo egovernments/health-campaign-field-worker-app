8c4f5dd07c1569131120c12a901c3f3b08beb5fa
Naveen <naveen.j@egovernments.org>
2023-01-04 13:10:04 +0530

    moved RequestInfo to constants

--- apps/health_campaign_field_worker_app/lib/data/repositories/api_interceptors.dart
+++ apps/health_campaign_field_worker_app/lib/data/repositories/api_interceptors.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 
 import '../../models/request_info/request_info_model.dart';
+import '../../utils/constants.dart';
 
 class ApiInterceptors extends Interceptor {
   @override
@@ -10,15 +11,14 @@ class ApiInterceptors extends Interceptor {
   ) async {
     options.data = {
       ...options.data,
-      "RequestInfo": const RequestInfoModel(
-        apiId: 'hcm',
-        ver: ".01",
-        ts: "",
-        action: "_search",
-        did: "1",
-        key: "",
-        msgId: "20170310130900|en_IN",
-        authToken: "a9679414-55dc-497c-9879-47f13069ba4a",
+      "RequestInfo": RequestInfoModel(
+        apiId: RequestInfoData.apiId,
+        ver: RequestInfoData.ver,
+        ts: RequestInfoData.ts,
+        action: options.path.split('/').last,
+        did: RequestInfoData.did,
+        key: RequestInfoData.key,
+        authToken: RequestInfoData.authToken,
       ).toJson(),
     };
     super.onRequest(options, handler);
