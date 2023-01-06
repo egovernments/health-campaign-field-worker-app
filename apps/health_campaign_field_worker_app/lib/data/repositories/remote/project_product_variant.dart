d2315e142b436deded1667650ece95c9664ad802
Ajil Oommen <ajil.oommen@egovernments.org>
2023-01-06 09:18:42 +0530

    [hlm-660]: Recreate repositories

---
+++ apps/health_campaign_field_worker_app/lib/data/repositories/remote/project_product_variant.dart
@@ -0,0 +1,12 @@
+// Generated using mason. Do not modify by hand
+
+import '../../../models/data_model.dart';
+import '../../data_repository.dart';
+
+class ProjectProductVariantRepository extends RemoteRepository<ProjectProductVariantModel, ProjectProductVariantSearchModel> {
+  ProjectProductVariantRepository(
+    super.dio, {
+    required super.path,
+    super.entityName = 'ProjectProductVariant',
+  });
+}
