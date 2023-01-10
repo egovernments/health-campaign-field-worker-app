c5b2680ce70920938c533eb966a1a811eb1e8dde
Shubhranshu Shekhar <shubhranshu.shekhar@thoughtworks.com>
2023-01-10 17:03:25 +0530

    Created DigitCheckBox & DigitDropDown widgets

---
+++ packages/digit_components/lib/widgets/atoms/check_box.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class DigitCheckBox extends StatelessWidget {
+  final bool isChecked;
+  final String checkBoxText;
+  final Function() onChange;
+
+  const DigitCheckBox(
+      {super.key,
+      required this.checkBoxText,
+      required this.onChange,
+      this.isChecked = false});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return InkWell(
+      onTap: onChange,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 24,
+            width: 24,
+            child: Checkbox(
+                value: isChecked,
+                side: MaterialStateBorderSide.resolveWith((states) {
+                  if (states.contains(MaterialState.selected)) {
+                    return BorderSide(
+                        width: 2.0, color: theme.colorScheme.secondary);
+                  }
+                }),
+                fillColor: MaterialStateProperty.resolveWith((states) {
+                  if (states.contains(MaterialState.selected)) {
+                    return theme.colorScheme.background;
+                  }
+                  return theme.colorScheme.secondary;
+                }),
+                checkColor: theme.colorScheme.secondary,
+                onChanged: (bool? value) {
+                  onChange;
+                }),
+          ),
+          const SizedBox(width: 16,),
+          Text(
+            checkBoxText,
+            style: Theme.of(context).textTheme.bodySmall,
+          )
+        ],
+      ),
+    );
+  }
+}
