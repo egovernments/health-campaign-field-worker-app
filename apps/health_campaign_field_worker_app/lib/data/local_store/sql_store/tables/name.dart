e595c5bccbf074f4b8e7c44cb2fd39feaaa832cd
Ajil Oommen <ajil.oommen@egovernments.org>
2023-01-11 22:26:10 +0530

    [hlm-660]: Update models

--- apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/tables/name.dart
+++ apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/tables/name.dart
@@ -4,10 +4,10 @@ import 'package:drift/drift.dart';
 
 
 class Name extends Table {
+  TextColumn get clientReferenceId => text()();
   TextColumn get givenName => text()();
   TextColumn get familyName => text()();
   TextColumn get otherNames => text().nullable()();
-  TextColumn get clientReferenceId => text()();
   
 
   @override
