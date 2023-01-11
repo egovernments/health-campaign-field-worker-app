4baaaff45a0d0c19f6277e671b7ca0091ab92047
Ajil Oommen <ajil.oommen@egovernments.org>
2023-01-11 23:28:43 +0530

    [hlm-660]: Individual address

---
+++ apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/tables/individual_address.dart
@@ -0,0 +1,16 @@
+// Generated using mason. Do not modify by hand
+
+import 'package:drift/drift.dart';
+
+import 'individual.dart';
+import 'address.dart';
+
+class IndividualAddress extends Table {
+  TextColumn get clientReferenceId => text()();
+  
+  TextColumn get individual => text().references(Individual, #clientReferenceId)();
+  TextColumn get address => text().references(Address, #clientReferenceId)();
+
+  @override
+  Set<Column>? get primaryKey => { clientReferenceId,  };
+}
