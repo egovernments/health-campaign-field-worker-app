62bcb6e0edcd75516748adc4668529cb394b3c5b
Shubhranshu Shekhar <shubhranshu.shekhar@thoughtworks.com>
2023-01-12 15:19:07 +0530

    Created Individual Details Screen

---
+++ apps/health_campaign_field_worker_app/lib/pages/individual_details.dart
@@ -0,0 +1,133 @@
+import 'package:digit_components/digit_components.dart';
+import 'package:digit_components/widgets/atoms/digit_check_box.dart';
+import 'package:digit_components/widgets/atoms/digit_dropdown.dart';
+import 'package:digit_components/widgets/atoms/digit_text_form_field.dart';
+import 'package:digit_components/widgets/digit_card.dart';
+import 'package:digit_components/widgets/digit_dob_picker.dart';
+import 'package:digit_components/widgets/digit_elevated_button.dart';
+import 'package:digit_components/widgets/scrollable_content.dart';
+import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+import '../utils/i18_key_constants.dart' as i18;
+import '../widgets/header/back_navigation_help_header.dart';
+
+class IndividualDetails extends StatelessWidget {
+  const IndividualDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: ReactiveFormBuilder(
+        form: buildForm,
+        builder: (context, form, child) {
+          return ScrollableContent(
+            header: Column(children: const [
+              BackNavigationHelpHeaderWidget(),
+            ]),
+            footer: Offstage(
+              offstage: false,
+              child: SizedBox(
+                height: 90,
+                child: DigitCard(
+                  child: DigitElevatedButton(
+                    onPressed: () {
+                      if (form.valid) {
+                        print(form.value);
+                      } else {
+                        form.markAllAsTouched();
+                      }
+                    },
+                    child: Center(
+                      child: Text(i18.individualDetails.submitButtonLabelText),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            children: [
+              DigitCard(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Individual Details Text',
+                      style: theme.textTheme.displayMedium,
+                    ),
+                    Column(children: [
+                      DigitTextFormField(
+                        formControlName: 'individualName',
+                        label: i18.individualDetails.nameLabelText,
+                      ),
+                      DigitCheckBox(
+                        checkBoxText: i18.individualDetails.checkboxLabelText,
+                        onChange: () {},
+                        isChecked: true,
+                      ),
+                      DigitTextFormField(
+                        formControlName: 'idType',
+                        label: i18.individualDetails.idTypeLabelText,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          DigitTextFormField(
+                            formControlName: 'idNumber',
+                            label: i18.individualDetails.idNumberLabelText,
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            i18.individualDetails.idNumberSuggestionText,
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+
+                      /// implement date picker over here
+                      DigitDOBPicker(
+                        datePickerFormControl: 'dob',
+                        ageInputFormControl: 'age',
+                        onChangeDate: (DateTime? dateTime) {},
+                        datePickerLabel: i18.individualDetails.dobLabelText,
+                        ageFieldLabel: i18.individualDetails.ageLabelText,
+                        separatorLabel:
+                            i18.individualDetails.separatorLabelText,
+                      ),
+                      DigitDropDown(
+                        label: i18.individualDetails.genderLabelText,
+                        initialValue: 'Male',
+                        menuItems: const ['Male', 'Female'],
+                        onChanged: (String? newValue) {},
+                        formControlName: 'gender',
+                      ),
+                      DigitTextFormField(
+                        formControlName: 'mobileNumber',
+                        label: i18.individualDetails.mobileNumberLabelText,
+                      ),
+                    ]),
+                    const SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  FormGroup buildForm() => fb.group(<String, Object>{
+        'individualName': FormControl<String>(value: ''),
+        'idType': FormControl<String>(value: ''),
+        'idNumber': FormControl<String>(value: ''),
+        'dob': FormControl<String>(value: ''),
+        'age': FormControl<String>(value: ''),
+        'gender': FormControl<String>(value: ''),
+        'mobileNumber': FormControl<String>(value: ''),
+      });
+}
---
+++ packages/digit_components/lib/widgets/atoms/digit_dropdown.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+class DigitDropDown extends StatelessWidget {
+  final String label;
+  final String initialValue;
+  final List<String> menuItems;
+  final void Function(String? value) onChanged;
+  final String formControlName;
+
+  const DigitDropDown(
+      {super.key,
+      required this.label,
+      required this.initialValue,
+      required this.menuItems,
+      required this.formControlName,
+      required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          const SizedBox(height: 8),
+          ReactiveDropdownField(
+            formControlName: formControlName,
+            items: menuItems
+                .map((e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    ))
+                .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
