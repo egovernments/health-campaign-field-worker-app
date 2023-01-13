e242772a63c53286e849548efb17cbd3d71dc8d3
Ajil Oommen <ajil.oommen@egovernments.org>
2023-01-13 10:37:52 +0530

    [hlm-660]: Intermittent

--- apps/health_campaign_field_worker_app/lib/utils/utils.dart
+++ apps/health_campaign_field_worker_app/lib/utils/utils.dart
@@ -1,3 +1,20 @@
 library app_utils;
 
+import 'package:drift/drift.dart';
+
 export 'app_exception.dart';
+
+
+Expression<bool> buildAnd(Iterable<Expression<bool?>> iterable) {
+  if (iterable.isEmpty) return const Constant(true);
+  final result =  iterable.reduce((value, element) => value & element);
+
+  return result.equals(true);
+}
+
+Expression<bool> buildOr(Iterable<Expression<bool?>> iterable) {
+  if (iterable.isEmpty) return const Constant(true);
+  final result = iterable.reduce((value, element) => value | element);
+
+  return result.equals(true);
+}
