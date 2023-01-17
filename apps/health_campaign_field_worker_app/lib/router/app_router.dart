cf7e867c8efa0719779099e1cc1a65b00ab35051
Naveen <naveen.j@egovernments.org>
2023-01-17 10:57:18 +0530

    added reason for deletion page

--- apps/health_campaign_field_worker_app/lib/router/app_router.dart
+++ apps/health_campaign_field_worker_app/lib/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import '../pages/authenticated.dart';
+import '../pages/deletion_reason.dart';
 import '../pages/deliver_intervention.dart';
 import '../pages/home.dart';
 import '../pages/household_overview.dart';
@@ -36,6 +37,7 @@ part 'app_router.gr.dart';
         AutoRoute(page: SearchBeneficiaryPage, path: 'search-beneficiary'),
         AutoRoute(page: HouseholdOverViewPage, path: 'household-overview'),
         AutoRoute(page: DeliverInterventionPage, path: 'deliver-intervention'),
+        AutoRoute(page: DeletionReasonPage, path: 'deletion-reason'),
       ],
     ),
   ],
