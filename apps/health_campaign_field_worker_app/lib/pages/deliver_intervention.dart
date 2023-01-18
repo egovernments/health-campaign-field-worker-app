f433f726842ab721ace92c92e390124039903995
Naveen <naveen.j@egovernments.org>
2023-01-18 15:55:30 +0530

    added transators

--- apps/health_campaign_field_worker_app/lib/pages/deliver_intervention.dart
+++ apps/health_campaign_field_worker_app/lib/pages/deliver_intervention.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import '../utils/i18_key_constants.dart' as i18;
 import '../widgets/header/back_navigation_help_header.dart';
+import '../widgets/localized.dart';
 
-class DeliverInterventionPage extends StatelessWidget {
-  const DeliverInterventionPage({super.key});
+class DeliverInterventionPage extends LocalizedStatefulWidget {
+  const DeliverInterventionPage({
+    super.key,
+    super.appLocalizations,
+  });
+  @override
+  State<DeliverInterventionPage> createState() =>
+      _DeliverInterventionPageState();
+}
 
+class _DeliverInterventionPageState
+    extends LocalizedState<DeliverInterventionPage> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -27,15 +37,17 @@ class DeliverInterventionPage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          'Deliver Intervention',
+                          i18.deliverIntervention.deliverInterventionLabel,
                           style: theme.textTheme.displayMedium,
                         ),
                       ],
                     ),
-                    const DigitTableCard(
+                    DigitTableCard(
                       element: {
-                        "Date of Registration": "Joseph Segio",
-                        "Household Head Name": "Jose (H)",
+                        i18.deliverIntervention.dateOfRegistrationLabel:
+                            "Joseph Segio",
+                        i18.householdOverView
+                            .householdOverViewHouseholdHeadLabel: "Jose (H)",
                       },
                     ),
                     DigitTableCard(
@@ -45,23 +57,23 @@ class DeliverInterventionPage extends StatelessWidget {
                         style: BorderStyle.solid,
                         width: 1.0,
                       ),
-                      element: const {
-                        "ID Type": "National ID",
-                        "ID Number": "JGK87389",
-                        "Age": "40 years",
-                        "Gender": "Male",
-                        "Mobile Number": "+258 576478",
+                      element: {
+                        i18.deliverIntervention.idTypeText: "National ID",
+                        i18.deliverIntervention.idNumberText: "JGK87389",
+                        i18.common.coreCommonAge: "40 years",
+                        i18.common.coreCommonGender: "Male",
+                        i18.common.coreCommonMobileNumber: "+258 576478",
                       },
                     ),
-                    const DigitTableCard(
+                    DigitTableCard(
                       element: {
-                        "Member Count": 4,
-                        "Number Of Resources For Delivery": "03",
+                        i18.deliverIntervention.memberCountText: 4,
+                        i18.deliverIntervention.noOfResourcesForDelivery: "03",
                       },
                     ),
                     DigitDropdown(
-                      label: 'Resource Delivered',
-                      initialValue: 'Betnets',
+                      label: i18.deliverIntervention.resourceDeliveredLabel,
+                      initialValue: 'BEDNETS',
                       menuItems: [
                         MenuItemModel(
                           "DOLO",
@@ -78,11 +90,11 @@ class DeliverInterventionPage extends StatelessWidget {
                     DigitIntegerFormPicker(
                       form: form,
                       formControlName: 'quantityDistributed',
-                      label: "Resource Delivered",
+                      label: i18.deliverIntervention.quantityDistributedLabel,
                       incrementer: true,
                     ),
                     DigitDropdown(
-                      label: 'Delivery Comment',
+                      label: i18.deliverIntervention.deliveryCommentLabel,
                       initialValue: 'Betnets',
                       menuItems: [
                         MenuItemModel(
@@ -114,19 +126,18 @@ class DeliverInterventionPage extends StatelessWidget {
           child: DigitElevatedButton(
             onPressed: () => DigitDialog.show(
               context,
-              title: "Ready to Submit?",
-              content:
-                  "Make sure you review all details before clicking on the Submit button. Click on the Cancel button to go back to the previous page.",
-              primaryActionLabel: "Submit",
+              title: i18.deliverIntervention.dialogTitle,
+              content: i18.deliverIntervention.dialogContent,
+              primaryActionLabel: i18.common.coreCommonSubmit,
               primaryAction: () =>
                   Navigator.of(context, rootNavigator: true).pop(),
-              secondaryActionLabel: "Cancel",
+              secondaryActionLabel: i18.common.coreCommonCancel,
               secondaryAction: () =>
                   Navigator.of(context, rootNavigator: true).pop(),
             ),
-            child: const Center(
+            child: Center(
               child: Text(
-                "Submit",
+                i18.common.coreCommonSubmit,
               ),
             ),
           ),
