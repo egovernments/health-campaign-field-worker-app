fc27d083b4db230cd2e1fc201911bd4aa711c16a
Ajil Oommen <ajil.oommen@egovernments.org>
2023-01-23 13:01:30 +0530

    [hlm-1234]: Fix individual details

--- apps/health_campaign_field_worker_app/test/router/router.dart
+++ apps/health_campaign_field_worker_app/test/router/router.dart
@@ -1,15 +1,18 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:health_campaign_field_worker_app/blocs/localization/app_localization.dart';
 import 'package:health_campaign_field_worker_app/pages/household_details.dart';
+import 'package:health_campaign_field_worker_app/pages/individual_details.dart';
 
 export 'package:auto_route/auto_route.dart';
-import 'package:health_campaign_field_worker_app/blocs/localization/app_localization.dart';
+
 part 'router.gr.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: [
     AutoRoute(page: HouseHoldDetailsPage, path: 'household-details'),
+    AutoRoute(page: IndividualDetailsPage, path: 'individual-details'),
   ],
 )
 class AppRouter extends _$AppRouter {}
