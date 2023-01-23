202b168754788325907fbfeb0e6994fb572b99c9
Ajil Oommen <ajil.oommen@egovernments.org>
2023-01-23 21:17:32 +0530

    [HLM-1140]: Update DigitDialog
    [HLM-1140]: Update digit_dialog.dart
    
    [HLM-1140]: Update digit_dialog.dart

--- packages/digit_components/lib/widgets/digit_dialog.dart
+++ packages/digit_components/lib/widgets/digit_dialog.dart
@@ -2,11 +2,11 @@ import 'package:digit_components/digit_components.dart';
 import 'package:flutter/material.dart';
 
 class DigitDialog extends StatelessWidget {
-  final Widget title;
+  final Widget? title;
   final Widget? content;
   final EdgeInsets titlePadding;
   final EdgeInsets contentPadding;
-  final String primaryActionLabel;
+  final String? primaryActionLabel;
   final VoidCallback? primaryAction;
   final String? secondaryActionLabel;
   final VoidCallback? secondaryAction;
@@ -14,36 +14,50 @@ class DigitDialog extends StatelessWidget {
   const DigitDialog({
     super.key,
     this.content,
-    required this.title,
-    required this.primaryActionLabel,
+    this.title,
+    this.primaryActionLabel,
     this.primaryAction,
-    this.titlePadding = const EdgeInsets.fromLTRB(8, 16, 8, 8),
-    this.contentPadding = const EdgeInsets.fromLTRB(8, 8, 8, 8),
+    this.titlePadding = const EdgeInsets.fromLTRB(
+      kPadding,
+      kPadding * 2,
+      kPadding,
+      kPadding,
+    ),
+    this.contentPadding = const EdgeInsets.all(kPadding),
     this.secondaryActionLabel,
     this.secondaryAction,
   });
 
   static Future<T?> show<T>(
     BuildContext context, {
-    required String title,
-    required String content,
-    required String primaryActionLabel,
-    String? secondaryActionLabel,
-    VoidCallback? primaryAction,
-    VoidCallback? secondaryAction,
-  }) =>
-      showDialog<T>(
-        context: context,
-        barrierColor: DigitTheme.instance.colors.black.withOpacity(0.7),
-        builder: (context) => DigitDialog(
-          title: Text(title, textAlign: TextAlign.left),
-          content: Text(content, textAlign: TextAlign.left),
-          primaryActionLabel: primaryActionLabel,
-          primaryAction: primaryAction,
-          secondaryActionLabel: secondaryActionLabel,
-          secondaryAction: secondaryAction,
-        ),
-      );
+    required DigitDialogOptions options,
+  }) {
+    Widget? title = options.title;
+    Widget? content = options.content;
+
+    if (options.titleText != null) {
+      title ??= Text(options.titleText!, textAlign: TextAlign.left);
+    }
+
+    if (options.contentText != null) {
+      content ??= Text(options.contentText!, textAlign: TextAlign.left);
+    }
+
+    return showDialog<T>(
+      context: context,
+      barrierDismissible: options.barrierDismissible,
+      barrierColor: options.barrierColor ??
+          DigitTheme.instance.colors.black.withOpacity(0.7),
+      builder: (context) => DigitDialog(
+        title: title,
+        content: content,
+        primaryActionLabel: options.primaryAction?.label,
+        primaryAction: options.primaryAction?.action,
+        secondaryActionLabel: options.secondaryAction?.label,
+        secondaryAction: options.secondaryAction?.action,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) => AlertDialog(
@@ -51,10 +65,11 @@ class DigitDialog extends StatelessWidget {
         content: content,
         actionsAlignment: MainAxisAlignment.spaceBetween,
         actions: <Widget>[
-          DigitElevatedButton(
-            onPressed: primaryAction,
-            child: Center(child: Text(primaryActionLabel)),
-          ),
+          if (primaryActionLabel != null)
+            DigitElevatedButton(
+              onPressed: primaryAction,
+              child: Center(child: Text(primaryActionLabel!)),
+            ),
           if (secondaryActionLabel != null)
             TextButton(
               onPressed: secondaryAction,
@@ -64,4 +79,40 @@ class DigitDialog extends StatelessWidget {
         titlePadding: titlePadding,
         contentPadding: contentPadding,
       );
+}
+
+class DigitDialogOptions {
+  final EdgeInsets titlePadding;
+  final EdgeInsets contentPadding;
+  final String? titleText;
+  final String? contentText;
+  final Widget? title;
+  final Widget? content;
+  final DigitActionOptions? primaryAction;
+  final DigitActionOptions? secondaryAction;
+  final bool barrierDismissible;
+  final Color? barrierColor;
+
+  const DigitDialogOptions({
+    this.titleText,
+    this.contentText,
+    this.title,
+    this.content,
+    this.primaryAction,
+    this.secondaryAction,
+    this.barrierDismissible = false,
+    this.titlePadding = const EdgeInsets.all(kPadding),
+    this.contentPadding = const EdgeInsets.all(kPadding),
+    this.barrierColor,
+  });
+}
+
+class DigitActionOptions {
+  final String label;
+  final VoidCallback? action;
+
+  const DigitActionOptions({
+    required this.label,
+    this.action,
+  });
 }
