68b91e382aff0a696aa215d765e73b57575ba543
Ajil Oommen <ajil.oommen@egovernments.org>
2023-02-10 23:35:17 +0530

    [hlm-1145]: Project beneficiary

--- apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/tables/task.dart
+++ apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/tables/task.dart
@@ -12,11 +12,11 @@ class Task extends Table {
   TextColumn get clientReferenceId => text()();
   TextColumn get tenantId => text()();
   IntColumn get rowVersion => integer()();
-  DateTimeColumn get plannedStartDate => dateTime().nullable()();
-  DateTimeColumn get plannedEndDate => dateTime().nullable()();
-  DateTimeColumn get actualStartDate => dateTime().nullable()();
-  DateTimeColumn get actualEndDate => dateTime().nullable()();
-  DateTimeColumn get createdDate => dateTime().nullable()();
+  IntColumn get plannedStartDate => integer().nullable()();
+  IntColumn get plannedEndDate => integer().nullable()();
+  IntColumn get actualStartDate => integer().nullable()();
+  IntColumn get actualEndDate => integer().nullable()();
+  IntColumn get createdDate => integer().nullable()();
   
 
   @override
