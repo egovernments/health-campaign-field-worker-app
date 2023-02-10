e9bf66e7f3b03b78990170e252f3ad2f6e0b2ffb
Ajil Oommen <ajil.oommen@egovernments.org>
2023-02-11 01:57:43 +0530

    [hlm-1145]: Working add beneficiaries

--- apps/health_campaign_field_worker_app/lib/app.dart
+++ apps/health_campaign_field_worker_app/lib/app.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:isar/isar.dart';
+import 'package:provider/provider.dart';
 
 import 'blocs/app_initialization/app_initialization.dart';
 import 'blocs/auth/auth.dart';
@@ -36,116 +37,114 @@ class MainApplication extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AppInitializationBloc(
-            isar: isar,
-            mdmsRepository: MdmsRepository(client),
-          )..add(const AppInitializationSetupEvent()),
-        ),
-        BlocProvider(
-          create: (context) => AuthBloc(
-            const AuthState(),
-            AuthRepository(client),
+    return Provider.value(
+      value: sql,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AppInitializationBloc(
+              isar: isar,
+              mdmsRepository: MdmsRepository(client),
+            )..add(const AppInitializationSetupEvent()),
           ),
-        ),
-        BlocProvider(
-          create: (context) => TableHideActionBloc(
-            const TableHideActionState(),
+          BlocProvider(create: (context) => AuthBloc(const AuthState())),
+          BlocProvider(
+            create: (context) => TableHideActionBloc(
+              const TableHideActionState(),
+            ),
           ),
-        ),
-      ],
-      child: BlocBuilder<AppInitializationBloc, AppInitializationState>(
-        builder: (context, appConfigState) {
-          const defaultLocale = Locale('en', 'IN');
+        ],
+        child: BlocBuilder<AppInitializationBloc, AppInitializationState>(
+          builder: (context, appConfigState) {
+            const defaultLocale = Locale('en', 'IN');
 
-          return BlocBuilder<AuthBloc, AuthState>(
-            builder: (context, authState) {
-              if (appConfigState is! AppInitialized) {
-                return const MaterialApp(
-                  home: Scaffold(
-                    body: Center(
-                      child: Text('Loading'),
+            return BlocBuilder<AuthBloc, AuthState>(
+              builder: (context, authState) {
+                if (appConfigState is! AppInitialized) {
+                  return const MaterialApp(
+                    home: Scaffold(
+                      body: Center(
+                        child: Text('Loading'),
+                      ),
+                    ),
+                  );
+                }
+
+                final appConfig = appConfigState.appConfiguration;
+
+                final localizationModulesList = appConfig.backendInterface;
+                final firstLanguage = appConfig.languages?.first.value;
+                final languages = appConfig.languages;
+
+                return BlocProvider(
+                  create:
+                      (localizationModulesList != null && firstLanguage != null)
+                          ? (context) => LocalizationBloc(
+                                const LocalizationState(),
+                                LocalizationRepository(client, isar),
+                                isar,
+                              )..add(
+                                  LocalizationEvent.onLoadLocalization(
+                                    module: localizationModulesList.interfaces
+                                        .where((element) =>
+                                            element.type ==
+                                            Modules.localizationModule)
+                                        .map((e) => e.name.toString())
+                                        .join(',')
+                                        .toString(),
+                                    tenantId: appConfig.tenantId.toString(),
+                                    locale: firstLanguage,
+                                    path: Constants.localizationApiPath,
+                                  ),
+                                )
+                          : (context) => LocalizationBloc(
+                                const LocalizationState(),
+                                LocalizationRepository(client, isar),
+                                isar,
+                              ),
+                  child: NetworkManagerProviderWrapper(
+                    isar: isar,
+                    configuration: const NetworkManagerConfiguration(
+                      persistenceConfig: PersistenceConfiguration.offlineFirst,
+                    ),
+                    dio: client,
+                    sql: sql,
+                    child: MaterialApp.router(
+                      supportedLocales: languages != null
+                          ? languages.map((e) {
+                              final results = e.value.split('_');
+
+                              return results.isNotEmpty
+                                  ? Locale(results.first, results.last)
+                                  : defaultLocale;
+                            })
+                          : [defaultLocale],
+                      localizationsDelegates: [
+                        AppLocalizations.getDelegate(appConfig, isar),
+                        GlobalWidgetsLocalizations.delegate,
+                        GlobalCupertinoLocalizations.delegate,
+                        GlobalMaterialLocalizations.delegate,
+                      ],
+                      theme: DigitTheme.instance.mobileTheme,
+                      routeInformationParser: appRouter.defaultRouteParser(),
+                      scaffoldMessengerKey: scaffoldMessengerKey,
+                      routerDelegate: AutoRouterDelegate.declarative(
+                        appRouter,
+                        navigatorObservers: () => [AppRouterObserver()],
+                        routes: (handler) => [
+                          if (authState.isAuthenticated)
+                            const AuthenticatedRouteWrapper()
+                          else
+                            const UnauthenticatedRouteWrapper(),
+                        ],
+                      ),
                     ),
                   ),
                 );
-              }
-
-              final appConfig = appConfigState.appConfiguration;
-
-              final localizationModulesList = appConfig.backendInterface;
-              final firstLanguage = appConfig.languages?.first.value;
-              final languages = appConfig.languages;
-
-              return BlocProvider(
-                create:
-                    (localizationModulesList != null && firstLanguage != null)
-                        ? (context) => LocalizationBloc(
-                              const LocalizationState(),
-                              LocalizationRepository(client, isar),
-                              isar,
-                            )..add(
-                                LocalizationEvent.onLoadLocalization(
-                                  module: localizationModulesList.interfaces
-                                      .where((element) =>
-                                          element.type ==
-                                          Modules.localizationModule)
-                                      .map((e) => e.name.toString())
-                                      .join(',')
-                                      .toString(),
-                                  tenantId: appConfig.tenantId.toString(),
-                                  locale: firstLanguage,
-                                  path: Constants.localizationApiPath,
-                                ),
-                              )
-                        : (context) => LocalizationBloc(
-                              const LocalizationState(),
-                              LocalizationRepository(client, isar),
-                              isar,
-                            ),
-                child: NetworkManagerProviderWrapper(
-                  isar: isar,
-                  configuration: const NetworkManagerConfiguration(
-                    persistenceConfig: PersistenceConfiguration.offlineFirst,
-                  ),
-                  dio: client,
-                  sql: sql,
-                  child: MaterialApp.router(
-                    supportedLocales: languages != null
-                        ? languages.map((e) {
-                            final results = e.value.split('_');
-
-                            return results.isNotEmpty
-                                ? Locale(results.first, results.last)
-                                : defaultLocale;
-                          })
-                        : [defaultLocale],
-                    localizationsDelegates: [
-                      AppLocalizations.getDelegate(appConfig, isar),
-                      GlobalWidgetsLocalizations.delegate,
-                      GlobalCupertinoLocalizations.delegate,
-                      GlobalMaterialLocalizations.delegate,
-                    ],
-                    theme: DigitTheme.instance.mobileTheme,
-                    routeInformationParser: appRouter.defaultRouteParser(),
-                    scaffoldMessengerKey: scaffoldMessengerKey,
-                    routerDelegate: AutoRouterDelegate.declarative(
-                      appRouter,
-                      navigatorObservers: () => [AppRouterObserver()],
-                      routes: (handler) => [
-                        if (authState.isAuthenticated)
-                          const AuthenticatedRouteWrapper()
-                        else
-                          const UnauthenticatedRouteWrapper(),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          );
-        },
+              },
+            );
+          },
+        ),
       ),
     );
   }
