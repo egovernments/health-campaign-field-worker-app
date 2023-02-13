2c17a377f8c60870822aa99e09eebddc76bb115e
Naveen <naveen.j@egovernments.org>
2023-02-13 12:17:23 +0530

    added bloc for selection

--- apps/health_campaign_field_worker_app/lib/pages/household_overview.dart
+++ apps/health_campaign_field_worker_app/lib/pages/household_overview.dart
@@ -1,5 +1,8 @@
 import 'package:digit_components/digit_components.dart';
 import 'package:flutter/material.dart';
+import 'package:digit_components/digit_components.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../blocs/selected_households/selected_households.dart';
 import '../utils/i18_key_constants.dart' as i18;
 import '../router/app_router.dart';
 import '../widgets/action_card/action_card.dart';
@@ -29,128 +32,173 @@ class _HouseholdOverViewPageState
           BackNavigationHelpHeaderWidget(),
         ]),
         children: [
-          DigitCard(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          BlocBuilder<SelectedHouseHoldsBloc, SelectedHouseHoldsState>(
+            builder: (context, state) {
+              print(
+                state.individual?.name?.givenName,
+              );
+
+              return DigitCard(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Flexible(
-                      child: Text(
-                        localizations.translate(
-                          i18.householdOverView.householdOverViewLabel,
-                        ),
-                        style: theme.textTheme.displayMedium,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    DigitIconButton(
-                      onPressed: () => DigitActionDialog.show(
-                        context,
-                        widget: ActionCard(
-                          items: [
-                            ActionCardModel(
-                              icon: Icons.edit,
-                              label: localizations.translate(
-                                i18.householdOverView
-                                    .householdOverViewEditLabel,
-                              ),
-                              action: () {
-                                // TODO: Complete implementation
-                              },
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            localizations.translate(
+                              i18.householdOverView.householdOverViewLabel,
                             ),
-                            ActionCardModel(
-                              icon: Icons.delete,
-                              label: localizations.translate(i18
-                                  .householdOverView
-                                  .householdOverViewDeleteLabel),
-                              action: () => DigitDialog.show(
-                                context,
-                                options: DigitDialogOptions(
-                                  titleText: localizations.translate(i18
-                                      .householdOverView
-                                      .householdOverViewActionCardTitle),
-                                  primaryAction: DigitDialogActions(
-                                    label: localizations.translate(i18
-                                        .householdOverView
-                                        .householdOverViewPrimaryActionLabel),
-                                    action: (context) {
-                                      Navigator.of(context, rootNavigator: true)
-                                          .pop();
-                                      Navigator.of(context, rootNavigator: true)
-                                          .pop();
-                                    },
+                            style: theme.textTheme.displayMedium,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        DigitIconButton(
+                          onPressed: () => DigitActionDialog.show(
+                            context,
+                            widget: ActionCard(
+                              items: [
+                                ActionCardModel(
+                                  icon: Icons.edit,
+                                  label: localizations.translate(
+                                    i18.householdOverView
+                                        .householdOverViewEditLabel,
                                   ),
-                                  secondaryAction: DigitDialogActions(
-                                    label: localizations.translate(i18
-                                        .householdOverView
-                                        .householdOverViewSecondaryActionLabel),
-                                    action: (context) {
-                                      Navigator.of(context, rootNavigator: true)
-                                          .pop();
-                                    },
+                                  action: () {
+                                    // TODO: Complete implementation
+                                  },
+                                ),
+                                ActionCardModel(
+                                  icon: Icons.delete,
+                                  label: localizations.translate(i18
+                                      .householdOverView
+                                      .householdOverViewDeleteLabel),
+                                  action: () => DigitDialog.show(
+                                    context,
+                                    options: DigitDialogOptions(
+                                      titleText: localizations.translate(i18
+                                          .householdOverView
+                                          .householdOverViewActionCardTitle),
+                                      primaryAction: DigitDialogActions(
+                                        label: localizations.translate(i18
+                                            .householdOverView
+                                            .householdOverViewPrimaryActionLabel),
+                                        action: (context) {
+                                          Navigator.of(context,
+                                                  rootNavigator: true)
+                                              .pop();
+                                          Navigator.of(context,
+                                                  rootNavigator: true)
+                                              .pop();
+                                        },
+                                      ),
+                                      secondaryAction: DigitDialogActions(
+                                        label: localizations.translate(i18
+                                            .householdOverView
+                                            .householdOverViewSecondaryActionLabel),
+                                        action: (context) {
+                                          Navigator.of(context,
+                                                  rootNavigator: true)
+                                              .pop();
+                                        },
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
+                              ],
                             ),
-                          ],
+                          ),
+                          iconText: localizations.translate(
+                            i18.householdOverView.householdOverViewEditIconText,
+                          ),
+                          icon: Icons.edit,
                         ),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: DigitIconButton(
+                        icon: Icons.check_circle,
+                        iconText: localizations.translate(
+                          i18.householdOverView
+                              .householdOverViewDeliveredIconLabel,
+                        ),
+                        iconTextColor:
+                            DigitTheme.instance.colorScheme.onSurfaceVariant,
+                        iconColor:
+                            DigitTheme.instance.colorScheme.onSurfaceVariant,
                       ),
-                      iconText: localizations.translate(
-                        i18.householdOverView.householdOverViewEditIconText,
+                    ),
+                    DigitTableCard(
+                      element: {
+                        localizations.translate(i18.householdOverView
+                                .householdOverViewHouseholdHeadNameLabel):
+                            state.individual?.name?.givenName,
+                        localizations.translate(
+                          i18.householdLocation.administrationAreaFormLabel,
+                        ): 'Solimbo',
+                        localizations.translate(
+                          i18.deliverIntervention.memberCountText,
+                        ): state.household?.memberCount,
+                      },
+                    ),
+                    MemberCard(
+                      name: state.individual!.name!.givenName.toString(),
+                      age: int.parse((DateTime.now()
+                                  .difference(DateTime.parse(
+                                    state.individual!.dateOfBirth!,
+                                  ))
+                                  .inDays /
+                              365)
+                          .round()
+                          .toStringAsFixed(0)),
+                      gender: state.individual!.gender!.name,
+                      isDelivered: false,
+                      localizations: localizations,
+                    ),
+                    Column(
+                      children: state.householdMembers!
+                          .map(
+                            (ele) => MemberCard(
+                              name: 'Joseph Sergio (H)',
+                              age: int.parse((DateTime.now()
+                                          .difference(DateTime.parse(
+                                            state.individual!.dateOfBirth!,
+                                          ))
+                                          .inDays /
+                                      365)
+                                  .round()
+                                  .toStringAsFixed(0)),
+                              gender: 'Male',
+                              isDelivered: false,
+                              localizations: localizations,
+                            ),
+                          )
+                          .toList(),
+                    ),
+                    // MemberCard(
+                    //   name: 'Maria',
+                    //   age: 40,
+                    //   gender: 'Female',
+                    //   isDelivered: true,
+                    //   localizations: localizations,
+                    // ),
+                    Center(
+                      child: DigitIconButton(
+                        onPressed: () {
+                          // TODO: Complete implementation
+                        },
+                        iconText: localizations.translate(
+                          i18.householdOverView.householdOverViewAddActionText,
+                        ),
+                        icon: Icons.add,
                       ),
-                      icon: Icons.edit,
                     ),
                   ],
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: DigitIconButton(
-                    icon: Icons.check_circle,
-                    iconText: localizations.translate(
-                      i18.householdOverView.householdOverViewDeliveredIconLabel,
-                    ),
-                    iconTextColor:
-                        DigitTheme.instance.colorScheme.onSurfaceVariant,
-                    iconColor: DigitTheme.instance.colorScheme.onSurfaceVariant,
-                  ),
-                ),
-                DigitTableCard(
-                  element: {
-                    localizations.translate(i18.householdOverView
-                        .householdOverViewHouseholdHeadLabel): "Joseph Sergio",
-                    localizations.translate(i18.householdOverView
-                        .householdOverViewHouseholdHeadNameLabel): "Jose (H)",
-                  },
-                ),
-                MemberCard(
-                  name: 'Joseph Sergio (H)',
-                  age: 40,
-                  gender: 'Male',
-                  isDelivered: false,
-                  localizations: localizations,
-                ),
-                MemberCard(
-                  name: 'Maria',
-                  age: 40,
-                  gender: 'Female',
-                  isDelivered: true,
-                  localizations: localizations,
-                ),
-                Center(
-                  child: DigitIconButton(
-                    onPressed: () {
-                      // TODO: Complete implementation
-                    },
-                    iconText: localizations.translate(
-                      i18.householdOverView.householdOverViewAddActionText,
-                    ),
-                    icon: Icons.add,
-                  ),
-                ),
-              ],
-            ),
+              );
+            },
           ),
         ],
       ),
