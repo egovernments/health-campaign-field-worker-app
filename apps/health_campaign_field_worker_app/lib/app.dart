25696028ede5728e9988abbd18c2e26087177ba1
Ajil Oommen <ajil.oommen@egovernments.org>
2023-02-15 16:06:27 +0530

    Intermittent fix

--- apps/health_campaign_field_worker_app/lib/app.dart
+++ apps/health_campaign_field_worker_app/lib/app.dart
@@ -11,7 +11,6 @@ import 'blocs/localization/app_localization.dart';
 import 'blocs/localization/localization.dart';
 import 'data/local_store/sql_store/sql_store.dart';
 import 'data/network_manager.dart';
-import 'data/repositories/remote/auth.dart';
 import 'data/repositories/remote/localization.dart';
 import 'data/repositories/remote/mdms.dart';
 import 'router/app_navigator_observer.dart';
@@ -40,114 +39,117 @@ class MainApplication extends StatelessWidget {
         RepositoryProvider<LocalSqlDataStore>.value(value: sql),
         RepositoryProvider<Isar>.value(value: isar),
       ],
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => AppInitializationBloc(
-              isar: isar,
-              mdmsRepository: MdmsRepository(client),
-            )..add(const AppInitializationSetupEvent()),
+      child: BlocProvider(
+        create: (context) => AppInitializationBloc(
+          isar: isar,
+          mdmsRepository: MdmsRepository(client),
+        )..add(const AppInitializationSetupEvent()),
+        child: NetworkManagerProviderWrapper(
+          isar: isar,
+          configuration: const NetworkManagerConfiguration(
+            persistenceConfig: PersistenceConfiguration.offlineFirst,
           ),
-          BlocProvider(
-            create: (context) => AuthBloc(
-              const AuthState.initial(),
-              AuthRepository(client),
-            ),
-          ),
-        ],
-        child: BlocBuilder<AppInitializationBloc, AppInitializationState>(
-          builder: (context, appConfigState) {
-            const defaultLocale = Locale('en', 'IN');
+          dio: client,
+          sql: sql,
+          child: MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (ctx) => AuthBloc(
+                  authRepository: ctx.read(),
+                ),
+              ),
+            ],
+            child: BlocBuilder<AppInitializationBloc, AppInitializationState>(
+              builder: (context, appConfigState) {
+                const defaultLocale = Locale('en', 'IN');
 
-            return BlocBuilder<AuthBloc, AuthState>(
-              builder: (context, authState) {
-                if (appConfigState is! AppInitialized) {
-                  return const MaterialApp(
-                    home: Scaffold(
-                      body: Center(
-                        child: Text('Loading'),
-                      ),
-                    ),
-                  );
-                }
-
-                final appConfig = appConfigState.appConfiguration;
-
-                final localizationModulesList = appConfig.backendInterface;
-                final firstLanguage = appConfig.languages?.first.value;
-                final languages = appConfig.languages;
-
-                return BlocProvider(
-                  create:
-                      (localizationModulesList != null && firstLanguage != null)
-                          ? (context) => LocalizationBloc(
-                                const LocalizationState(),
-                                LocalizationRepository(client, isar),
-                                isar,
-                              )..add(
-                                  LocalizationEvent.onLoadLocalization(
-                                    module: localizationModulesList.interfaces
-                                        .where((element) =>
-                                            element.type ==
-                                            Modules.localizationModule)
-                                        .map((e) => e.name.toString())
-                                        .join(',')
-                                        .toString(),
-                                    tenantId: appConfig.tenantId.toString(),
-                                    locale: firstLanguage,
-                                    path: Constants.localizationApiPath,
-                                  ),
-                                )
-                          : (context) => LocalizationBloc(
-                                const LocalizationState(),
-                                LocalizationRepository(client, isar),
-                                isar,
-                              ),
-                  child: NetworkManagerProviderWrapper(
-                    isar: isar,
-                    configuration: const NetworkManagerConfiguration(
-                      persistenceConfig: PersistenceConfiguration.offlineFirst,
-                    ),
-                    dio: client,
-                    sql: sql,
-                    child: MaterialApp.router(
-                      supportedLocales: languages != null
-                          ? languages.map((e) {
-                              final results = e.value.split('_');
-
-                              return results.isNotEmpty
-                                  ? Locale(results.first, results.last)
-                                  : defaultLocale;
-                            })
-                          : [defaultLocale],
-                      localizationsDelegates: [
-                        AppLocalizations.getDelegate(appConfig, isar),
-                        GlobalWidgetsLocalizations.delegate,
-                        GlobalCupertinoLocalizations.delegate,
-                        GlobalMaterialLocalizations.delegate,
-                      ],
-                      theme: DigitTheme.instance.mobileTheme,
-                      routeInformationParser: appRouter.defaultRouteParser(),
-                      scaffoldMessengerKey: scaffoldMessengerKey,
-                      routerDelegate: AutoRouterDelegate.declarative(
-                        appRouter,
-                        navigatorObservers: () => [AppRouterObserver()],
-                        routes: (handler) => [
-                          authState.maybeWhen(
-                            initial: () => const UnauthenticatedRouteWrapper(),
-                            loaded:
-                                (String? accessToken, String? refreshToken) =>
-                                    const AuthenticatedRouteWrapper(),
-                            orElse: () => const UnauthenticatedRouteWrapper(),
+                return BlocBuilder<AuthBloc, AuthState>(
+                  builder: (context, authState) {
+                    if (appConfigState is! AppInitialized) {
+                      return const MaterialApp(
+                        home: Scaffold(
+                          body: Center(
+                            child: Text('Loading'),
                           ),
+                        ),
+                      );
+                    }
+
+                    final appConfig = appConfigState.appConfiguration;
+
+                    final localizationModulesList = appConfig.backendInterface;
+                    final firstLanguage = appConfig.languages?.first.value;
+                    final languages = appConfig.languages;
+
+                    return MultiBlocProvider(
+                      providers: [
+                        BlocProvider(
+                          create: (localizationModulesList != null &&
+                                  firstLanguage != null)
+                              ? (context) => LocalizationBloc(
+                                    const LocalizationState(),
+                                    LocalizationRepository(client, isar),
+                                    isar,
+                                  )..add(
+                                      LocalizationEvent.onLoadLocalization(
+                                        module: localizationModulesList
+                                            .interfaces
+                                            .where((element) =>
+                                                element.type ==
+                                                Modules.localizationModule)
+                                            .map((e) => e.name.toString())
+                                            .join(',')
+                                            .toString(),
+                                        tenantId: appConfig.tenantId.toString(),
+                                        locale: firstLanguage,
+                                        path: Constants.localizationApiPath,
+                                      ),
+                                    )
+                              : (context) => LocalizationBloc(
+                                    const LocalizationState(),
+                                    LocalizationRepository(client, isar),
+                                    isar,
+                                  ),
+                        ),
+                      ],
+                      child: MaterialApp.router(
+                        supportedLocales: languages != null
+                            ? languages.map((e) {
+                                final results = e.value.split('_');
+
+                                return results.isNotEmpty
+                                    ? Locale(results.first, results.last)
+                                    : defaultLocale;
+                              })
+                            : [defaultLocale],
+                        localizationsDelegates: [
+                          AppLocalizations.getDelegate(appConfig, isar),
+                          GlobalWidgetsLocalizations.delegate,
+                          GlobalCupertinoLocalizations.delegate,
+                          GlobalMaterialLocalizations.delegate,
                         ],
+                        theme: DigitTheme.instance.mobileTheme,
+                        routeInformationParser: appRouter.defaultRouteParser(),
+                        scaffoldMessengerKey: scaffoldMessengerKey,
+                        routerDelegate: AutoRouterDelegate.declarative(
+                          appRouter,
+                          navigatorObservers: () => [AppRouterObserver()],
+                          routes: (handler) => authState.maybeWhen(
+                            orElse: () => [
+                              const UnauthenticatedRouteWrapper(),
+                            ],
+                            authenticated: (_, __) => [
+                              const AuthenticatedRouteWrapper(),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 );
               },
-            );
-          },
+            ),
+          ),
         ),
       ),
     );
