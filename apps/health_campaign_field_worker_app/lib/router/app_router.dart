52eb18b48237d2ebcb3da866af29f0aada6612ac
Shubhranshu Shekhar <shubhranshu.shekhar@thoughtworks.com>
2023-02-15 15:24:02 +0530

    created screens

--- apps/health_campaign_field_worker_app/lib/router/app_router.dart
+++ apps/health_campaign_field_worker_app/lib/router/app_router.dart
@@ -11,6 +11,9 @@ import '../pages/beneficiary_registration/individual_details.dart';
 import '../pages/deliver_intervention.dart';
 import '../pages/home.dart';
 import '../pages/household_overview.dart';
+import '../pages/inventory/stock_damaged_details.dart';
+import '../pages/inventory/stock_lost_details.dart';
+import '../pages/inventory/stock_returned_details.dart';
 import '../pages/language_selection.dart';
 import '../pages/login.dart';
 import '../pages/project_selection.dart';
@@ -18,7 +21,8 @@ import '../pages/search_beneficiary.dart';
 import '../pages/unauthenticated.dart';
 import '../pages/manage_stocks.dart';
 import '../pages/warehouse_details.dart';
-import '../pages/stock_receipt_details.dart';
+import '../pages/inventory/stock_received_details.dart';
+import '../pages/inventory/stock_issued_details.dart';
 export 'package:auto_route/auto_route.dart';
 
 part 'app_router.gr.dart';
@@ -61,9 +65,14 @@ part 'app_router.gr.dart';
         ),
         AutoRoute(page: AcknowledgementPage, path: 'acknowledgement'),
         AutoRoute(page: ProjectSelectionPage, path: 'project-selection'),
+        /// Inventory Routes
         AutoRoute(page: ManageStocksPage, path: 'manage-stocks'),
         AutoRoute(page: WarehouseDetailsPage, path: 'warehouse-details'),
-        AutoRoute(page: StockReceiptDetailsPage, path: 'stock-receipt-details'),
+        AutoRoute(page: StockReceivedDetailsPage, path: 'stock-receipt-details'),
+        AutoRoute(page: StockIssuedDetailsPage, path: 'stock-issued-details'),
+        AutoRoute(page: StockReturnedDetailsPage, path: 'stock-returned-details'),
+        AutoRoute(page: StockDamagedDetailsPage, path: 'stock-damaged-details'),
+        AutoRoute(page: StockLostDetailsPage, path: 'stock-lost-details'),
       ],
     ),
   ],
