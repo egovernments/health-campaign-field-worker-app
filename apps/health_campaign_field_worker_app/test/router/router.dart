054e9c90e9dcce977ac093bdcadf3c1b08b4faee
Ajil Oommen <ajil.oommen@egovernments.org>
2023-02-17 14:07:32 +0530

    Fix household overview

--- apps/health_campaign_field_worker_app/test/router/router.dart
+++ apps/health_campaign_field_worker_app/test/router/router.dart
@@ -1,18 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:health_campaign_field_worker_app/blocs/localization/app_localization.dart';
+import 'package:health_campaign_field_worker_app/pages/beneficiary/deliver_intervention.dart';
+import 'package:health_campaign_field_worker_app/pages/beneficiary/household_overview.dart';
 import 'package:health_campaign_field_worker_app/pages/beneficiary_registration/household_details.dart';
 import 'package:health_campaign_field_worker_app/pages/beneficiary_registration/individual_details.dart';
-import 'package:health_campaign_field_worker_app/pages/deliver_intervention.dart';
-import 'package:health_campaign_field_worker_app/pages/household_overview.dart';
-import 'package:health_campaign_field_worker_app/pages/inventory/stock_damaged_details.dart';
-import 'package:health_campaign_field_worker_app/pages/inventory/stock_issued_details.dart';
-import 'package:health_campaign_field_worker_app/pages/inventory/stock_lost_details.dart';
-import 'package:health_campaign_field_worker_app/pages/inventory/stock_received_details.dart';
-import 'package:health_campaign_field_worker_app/pages/inventory/stock_returned_details.dart';
-import 'package:health_campaign_field_worker_app/pages/manage_stocks.dart';
-import 'package:health_campaign_field_worker_app/pages/stock_reconciliation.dart';
-import 'package:health_campaign_field_worker_app/pages/warehouse_details.dart';
 
 import '';
 
