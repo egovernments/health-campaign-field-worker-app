640d75db782c2e016f9384890c82374daf86ad70
Shubhranshu Shekhar <shubhranshu.shekhar@thoughtworks.com>
2023-02-20 10:34:21 +0530

    Added validations

--- apps/health_campaign_field_worker_app/lib/pages/inventory/stock_lost_details.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory/stock_lost_details.dart
@@ -102,18 +102,34 @@ class _StockReceiptDetailsPageState
                         label: localizations.translate(
                           i18.stockDetails.quantityLost,
                         ),
+                        maxLength: 200,
+                        isRequired: true,
+                        validationMessages: {
+                          'required': (object) => 'Quantity lost is required',
+                        },
                       ),
                       DigitTextFormField(
                         formControlName: 'waybillNumber',
                         label: localizations.translate(
                           i18.stockDetails.waybillNumber,
                         ),
+                        maxLength: 200,
+                        isRequired: true,
+                        validationMessages: {
+                          'required': (object) => 'Waybill number is required',
+                        },
                       ),
                       DigitTextFormField(
                         formControlName: 'noIndicatedOnWaybill',
                         label: localizations.translate(
                           i18.stockDetails.noIndicatedOnWaybill,
                         ),
+                        maxLength: 200,
+                        isRequired: true,
+                        validationMessages: {
+                          'required': (object) =>
+                              'Number Indicated on waybill is required',
+                        },
                       ),
                     ]),
                     const SizedBox(height: 16),
@@ -133,12 +149,22 @@ class _StockReceiptDetailsPageState
                       label: localizations.translate(
                         i18.stockDetails.vehicleNumber,
                       ),
+                      maxLength: 200,
+                      isRequired: true,
+                      validationMessages: {
+                        'required': (object) => 'Vehicle number is required',
+                      },
                     ),
                     DigitTextFormField(
                       formControlName: 'comments',
                       label: localizations.translate(
                         i18.stockDetails.comments,
                       ),
+                      maxLength: 200,
+                      isRequired: true,
+                      validationMessages: {
+                        'required': (object) => 'Comments are required',
+                      },
                     ),
                   ],
                 ),
