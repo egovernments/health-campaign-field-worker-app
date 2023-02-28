179c5a3a9c625efa1ea74f9e1e92db85382b7ab2
Shubhranshu Shekhar <shubhranshu.shekhar@thoughtworks.com>
2023-02-28 10:26:55 +0530

    Stock Reconciliation

---
+++ apps/health_campaign_field_worker_app/lib/models/entities/stock_reconciliation.dart
@@ -0,0 +1,79 @@
+// Generated using mason. Do not modify by hand
+import 'package:dart_mappable/dart_mappable.dart';
+import 'package:drift/drift.dart';
+
+import '../../data/local_store/sql_store/sql_store.dart';
+import '../data_model.dart';
+
+@MappableClass(ignoreNull: true)
+class StockReconciliationSearchModel extends EntitySearchModel {
+  final String? tenantId;
+  final String? facilityId;
+  final String? productVariantId;
+  final List<String>? clientReferenceId;
+  final bool? isDeleted;
+
+  StockReconciliationSearchModel({
+    this.tenantId,
+    this.facilityId,
+    this.productVariantId,
+    this.clientReferenceId,
+    this.isDeleted,
+    super.boundaryCode,
+  }) : super();
+}
+
+@MappableClass(ignoreNull: true)
+class StockReconciliationModel extends EntityModel {
+  final String? tenantId;
+  final String? facilityId;
+  final String? productVariantId;
+  final String? referenceId;
+  final String? referenceIdType;
+  final int? physicalCount;
+  final int? calculatedCount;
+  final String? commentsOnReconciliation;
+  final DateTime? dateOfReconciliation;
+  final String clientReferenceId;
+  final bool? isDeleted;
+  final int? rowVersion;
+
+  StockReconciliationModel({
+    this.tenantId,
+    this.facilityId,
+    this.productVariantId,
+    this.referenceId,
+    this.referenceIdType,
+    this.physicalCount,
+    this.calculatedCount,
+    this.commentsOnReconciliation,
+    int? dateOfReconciliation,
+    required this.clientReferenceId,
+    this.isDeleted,
+    this.rowVersion,
+    super.auditDetails,
+  })  : dateOfReconciliation = dateOfReconciliation == null
+            ? null
+            : DateTime.fromMillisecondsSinceEpoch(dateOfReconciliation),
+        super();
+
+  int? get dateOfReconciliationTime =>
+      dateOfReconciliation?.millisecondsSinceEpoch;
+
+  StockReconciliationCompanion get companion {
+    return StockReconciliationCompanion(
+      tenantId: Value(tenantId),
+      facilityId: Value(facilityId),
+      productVariantId: Value(productVariantId),
+      referenceId: Value(referenceId),
+      referenceIdType: Value(referenceIdType),
+      physicalCount: Value(physicalCount),
+      calculatedCount: Value(calculatedCount),
+      commentsOnReconciliation: Value(commentsOnReconciliation),
+      dateOfReconciliation: Value(dateOfReconciliationTime),
+      clientReferenceId: Value(clientReferenceId),
+      isDeleted: Value(isDeleted),
+      rowVersion: Value(rowVersion),
+    );
+  }
+}
