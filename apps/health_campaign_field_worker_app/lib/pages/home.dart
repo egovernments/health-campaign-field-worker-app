1b2daab2f66e393aedf25c88f9b033c79bcc063a
Ajil Oommen <ajil.oommen@egovernments.org>
2023-03-03 00:37:09 +0530

    [hlm-1301]: Fix project bloc

--- apps/health_campaign_field_worker_app/lib/pages/home.dart
+++ apps/health_campaign_field_worker_app/lib/pages/home.dart
@@ -7,7 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:isar/isar.dart';
 
 import '../blocs/auth/auth.dart';
-import '../blocs/project_selection/project_selection.dart';
+import '../blocs/project/project.dart';
 import '../blocs/sync/sync.dart';
 import '../data/data_repository.dart';
 import '../data/local_store/no_sql/schema/oplog.dart';
@@ -203,8 +203,8 @@ class _HomePageState extends LocalizedState<HomePage> {
           icon: Icons.announcement,
           label: i18.home.fileComplaint,
           onPressed: () {
-            context.read<ProjectSelectionBloc>().add(
-                  const ProjectSelectionProjectInitEvent(),
+            context.read<ProjectBloc>().add(
+                  const ProjectInitializeEvent(),
                 );
           },
         ),
