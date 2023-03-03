145dbd9abcf3620f83aa8298df92ab409418127d
Ajil Oommen <ajil.oommen@egovernments.org>
2023-03-03 18:18:23 +0530

    [hlm-1301]: Added facility

--- apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/tables/project_facility.dart
+++ apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/tables/project_facility.dart
@@ -4,15 +4,14 @@ import 'package:drift/drift.dart';
 
 
 class ProjectFacility extends Table {
-  TextColumn get id => text().nullable()();
-  TextColumn get facilityId => text().nullable()();
-  TextColumn get projectId => text().nullable()();
-  TextColumn get clientReferenceId => text()();
+  TextColumn get id => text()();
+  TextColumn get facilityId => text()();
+  TextColumn get projectId => text()();
   TextColumn get tenantId => text().nullable()();
   BoolColumn get isDeleted => boolean().nullable()();
   IntColumn get rowVersion => integer().nullable()();
   
 
   @override
-  Set<Column> get primaryKey => { clientReferenceId,  };
+  Set<Column> get primaryKey => { id,  };
 }
