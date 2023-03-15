394c3b9227633e4f8741620a19d25e025906c9fc
Ajil Oommen <ajil.oommen@egovernments.org>
2023-03-15 19:19:23 +0530

    Fix sync down

--- apps/health_campaign_field_worker_app/lib/data/repositories/remote/stock_reconciliation.dart
+++ apps/health_campaign_field_worker_app/lib/data/repositories/remote/stock_reconciliation.dart
@@ -3,13 +3,11 @@
 import '../../../models/data_model.dart';
 import '../../data_repository.dart';
 
-class StockReconciliationRemoteRepository extends RemoteRepository<
-    StockReconciliationModel, StockReconciliationSearchModel> {
+class StockReconciliationRemoteRepository extends RemoteRepository<StockReconciliationModel, StockReconciliationSearchModel> {
   StockReconciliationRemoteRepository(
     super.dio, {
     required super.actionMap,
     super.entityName = 'StockReconciliation',
-    super.isPlural = false,
   });
 
   @override
