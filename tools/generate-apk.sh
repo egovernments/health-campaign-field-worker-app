29098d47b71333cbdb8b62a4bcfa67e4886b9037
Ajil Oommen <ajil.oommen@egovernments.org>
2023-03-21 15:56:09 +0530

    [hlm-2098]: Fix apk script and env

---
+++ tools/generate-apk.sh
@@ -0,0 +1,30 @@
+echo "Installing bricks"
+echo "-----------------"
+./tools/install_bricks.sh
+echo
+
+cd apps/health_campaign_field_worker_app || exit
+
+echo "Creating env configs"
+echo "--------------------"
+cp -fr .env-qa .env
+echo
+
+mkdir release-apk
+
+echo "Generating QA APK"
+echo "-----------------"
+flutter build apk
+cp -fr build/app/outputs/flutter-apk/app-release.apk release-apk/apk-qa.apk
+echo
+
+echo "Creating env configs"
+echo "--------------------"
+cp -fr .env-uat .env
+echo
+
+echo "Generating UAT APK"
+echo "------------------"
+flutter build apk
+cp -fr build/app/outputs/flutter-apk/app-release.apk release-apk/apk-uat.apk
+echo
