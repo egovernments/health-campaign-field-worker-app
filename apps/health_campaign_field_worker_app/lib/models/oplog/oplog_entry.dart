152d085776f9cbf95adcd78b0109fe03ff0ab32c
Ajil Oommen <ajil.oommen@egovernments.org>
2023-03-22 16:15:50 +0530

    [hlm-2207]: Fix sync logic

--- apps/health_campaign_field_worker_app/lib/models/oplog/oplog_entry.dart
+++ apps/health_campaign_field_worker_app/lib/models/oplog/oplog_entry.dart
@@ -1,5 +1,6 @@
 import 'package:dart_mappable/dart_mappable.dart';
 
+import '../../data/local_store/no_sql/schema/oplog.dart';
 import '../data_model.dart';
 
 @MappableClass()
@@ -8,21 +9,67 @@ class OpLogEntry<T extends EntityModel> {
   final T entity;
   final DataModelType type;
   final DataOperation operation;
-  final bool isSynced;
-  final DateTime dateCreated;
+  final bool syncedUp;
+  final bool syncedDown;
   final String createdBy;
-  final DateTime? syncedOn;
+  final DateTime createdAt;
+  final DateTime? syncedUpOn;
+  final DateTime? syncedDownOn;
+  final String? serverGeneratedId;
+  final String? clientReferenceId;
 
   const OpLogEntry(
     this.entity,
     this.operation, {
     this.id,
+    required this.createdAt,
     required this.createdBy,
     required this.type,
-    this.isSynced = false,
-    required this.dateCreated,
-    this.syncedOn,
+    this.syncedUp = false,
+    this.syncedDown = false,
+    this.syncedUpOn,
+    this.syncedDownOn,
+    this.serverGeneratedId,
+    this.clientReferenceId,
   });
+
+  static OpLogEntry<T> fromOpLog<T extends EntityModel>(OpLog e) {
+    return OpLogEntry<T>(
+      e.getEntity<T>(),
+      e.operation,
+      createdBy: e.createdBy,
+      createdAt: e.createdAt,
+      type: e.entityType,
+      serverGeneratedId: e.serverGeneratedId,
+      id: e.id,
+      clientReferenceId: e.clientReferenceId,
+      syncedDown: e.syncedDown,
+      syncedDownOn: e.syncedDownOn,
+      syncedUp: e.syncedUp,
+      syncedUpOn: e.syncedUpOn,
+    );
+  }
+
+  OpLog get oplog {
+    OpLog oplog = OpLog()
+      ..entityString = entity.toJson()
+      ..entityType = type
+      ..operation = operation
+      ..serverGeneratedId = serverGeneratedId
+      ..clientReferenceId = clientReferenceId
+      ..syncedUpOn = syncedUpOn
+      ..syncedDownOn = syncedDownOn
+      ..createdBy = createdBy
+      ..createdAt = createdAt
+      ..syncedUp = syncedUp
+      ..syncedDown = syncedDown;
+
+    if (id != null) {
+      oplog.id = id!;
+    }
+
+    return oplog;
+  }
 }
 
 @MappableEnum()
