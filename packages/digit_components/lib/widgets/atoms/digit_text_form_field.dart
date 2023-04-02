8ed4bf4ff00cb04d337b1326b931324aaebfd884
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-04-03 03:10:02 +0530

    DigitTextFormField hintText Added

--- packages/digit_components/lib/widgets/atoms/digit_text_form_field.dart
+++ packages/digit_components/lib/widgets/atoms/digit_text_form_field.dart
@@ -8,6 +8,7 @@ class DigitTextFormField extends StatelessWidget {
   final bool readOnly;
   final String formControlName;
   final String? hint;
+  final String? hintText;
   final Widget? suffix;
   final bool isRequired;
   final int minLines;
@@ -51,104 +52,124 @@ class DigitTextFormField extends StatelessWidget {
       this.minLength,
       this.inputFormatter,
       this.prefixIcon,
-      this.prefixText});
+      this.prefixText,
+      this.hintText});
 
   @override
   Widget build(BuildContext context) => LabeledField(
         label: '$label ${isRequired ? '*' : ''}',
-        child: ReactiveTextField(
-            onChanged: onChanged,
-            readOnly: readOnly,
-            formControlName: formControlName,
-            maxLength: maxLength,
-            validationMessages: validationMessages,
-            autofocus: false,
-            textCapitalization: textCapitalization,
-            minLines: minLines,
-            maxLines: maxLines,
-            obscureText: obscureText,
-            focusNode: focusNode,
-            keyboardType: keyboardType,
-            inputFormatters: inputFormatter,
-            valueAccessor: valueAccessor,
-            decoration: readOnly == true
-                ? InputDecoration(
-                    enabledBorder:
-                        DigitTheme.instance.inputDecorationTheme.disabledBorder,
-                    fillColor: DigitTheme.instance.colors.cloudGray,
-                    focusedBorder:
-                        DigitTheme.instance.inputDecorationTheme.disabledBorder,
-                    focusColor: DigitTheme.instance.colors.cloudGray,
-                    suffixIcon: suffix == null
-                        ? null
-                        : InkWell(
-                            onTap: onTap,
-                            child: suffix,
-                          ),
-                    prefixIconConstraints:
-                        const BoxConstraints(minWidth: 0, minHeight: 0),
-                    prefixStyle: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        color: readOnly
-                            ? const DigitColors().cloudGray
-                            : DigitTheme.instance.colorScheme.onBackground),
-                    prefixIcon: prefixIcon ??
-                        (prefixText == ''
+        child: Column(
+          children: [
+            ReactiveTextField(
+                onChanged: onChanged,
+                readOnly: readOnly,
+                formControlName: formControlName,
+                maxLength: maxLength,
+                validationMessages: validationMessages,
+                autofocus: false,
+                textCapitalization: textCapitalization,
+                minLines: minLines,
+                maxLines: maxLines,
+                obscureText: obscureText,
+                focusNode: focusNode,
+                keyboardType: keyboardType,
+                inputFormatters: inputFormatter,
+                valueAccessor: valueAccessor,
+                decoration: readOnly == true
+                    ? InputDecoration(
+                        enabledBorder: DigitTheme
+                            .instance.inputDecorationTheme.disabledBorder,
+                        fillColor: DigitTheme.instance.colors.cloudGray,
+                        focusedBorder: DigitTheme
+                            .instance.inputDecorationTheme.disabledBorder,
+                        focusColor: DigitTheme.instance.colors.cloudGray,
+                        suffixIcon: suffix == null
                             ? null
-                            : Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 10, left: 10, bottom: 10, right: 0),
-                                child: Text(
-                                  prefixText ?? '',
-                                  style: TextStyle(
-                                      fontSize: kIsWeb ? 15 : 16,
-                                      fontWeight: FontWeight.w400,
-                                      color: readOnly
-                                          ? const DigitColors().cloudGray
-                                          : DigitTheme.instance.colorScheme
-                                              .onBackground),
-                                ),
-                              )),
-                  )
-                : InputDecoration(
-                    labelText: hint,
-                    contentPadding: const EdgeInsets.fromLTRB(16, 12, 0, 12),
-                    suffixIconConstraints: const BoxConstraints(
-                      maxHeight: 40,
-                      maxWidth: 40,
-                    ),
-                    prefixIconConstraints:
-                        const BoxConstraints(minWidth: 0, minHeight: 0),
-                    prefixStyle: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        color: readOnly
-                            ? const DigitColors().cloudGray
-                            : DigitTheme.instance.colorScheme.onBackground),
-                    prefixIcon: prefixIcon ??
-                        (prefixText == ''
+                            : InkWell(
+                                onTap: onTap,
+                                child: suffix,
+                              ),
+                        prefixIconConstraints:
+                            const BoxConstraints(minWidth: 0, minHeight: 0),
+                        prefixStyle: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: readOnly
+                                ? const DigitColors().cloudGray
+                                : DigitTheme.instance.colorScheme.onBackground),
+                        prefixIcon: prefixIcon ??
+                            (prefixText == ''
+                                ? null
+                                : Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 10,
+                                        left: 10,
+                                        bottom: 10,
+                                        right: 0),
+                                    child: Text(
+                                      prefixText ?? '',
+                                      style: TextStyle(
+                                          fontSize: kIsWeb ? 15 : 16,
+                                          fontWeight: FontWeight.w400,
+                                          color: readOnly
+                                              ? const DigitColors().cloudGray
+                                              : DigitTheme.instance.colorScheme
+                                                  .onBackground),
+                                    ),
+                                  )),
+                      )
+                    : InputDecoration(
+                        labelText: hint,
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(16, 12, 0, 12),
+                        suffixIconConstraints: const BoxConstraints(
+                          maxHeight: 40,
+                          maxWidth: 40,
+                        ),
+                        prefixIconConstraints:
+                            const BoxConstraints(minWidth: 0, minHeight: 0),
+                        prefixStyle: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: readOnly
+                                ? const DigitColors().cloudGray
+                                : DigitTheme.instance.colorScheme.onBackground),
+                        prefixIcon: prefixIcon ??
+                            (prefixText == ''
+                                ? null
+                                : Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 10,
+                                        left: 10,
+                                        bottom: 10,
+                                        right: 0),
+                                    child: Text(
+                                      prefixText ?? '',
+                                      style: TextStyle(
+                                          fontSize: kIsWeb ? 15 : 16,
+                                          fontWeight: FontWeight.w400,
+                                          color: readOnly
+                                              ? const DigitColors().cloudGray
+                                              : DigitTheme.instance.colorScheme
+                                                  .onBackground),
+                                    ),
+                                  )),
+                        suffixIcon: suffix == null
                             ? null
-                            : Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 10, left: 10, bottom: 10, right: 0),
-                                child: Text(
-                                  prefixText ?? '',
-                                  style: TextStyle(
-                                      fontSize: kIsWeb ? 15 : 16,
-                                      fontWeight: FontWeight.w400,
-                                      color: readOnly
-                                          ? const DigitColors().cloudGray
-                                          : DigitTheme.instance.colorScheme
-                                              .onBackground),
-                                ),
-                              )),
-                    suffixIcon: suffix == null
-                        ? null
-                        : InkWell(
-                            onTap: onTap,
-                            child: suffix,
-                          ),
-                  )),
+                            : InkWell(
+                                onTap: onTap,
+                                child: suffix,
+                              ),
+                      )),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(hintText ?? '',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: const DigitColors().cloudGray)),
+            )
+          ],
+        ),
       );
 }
