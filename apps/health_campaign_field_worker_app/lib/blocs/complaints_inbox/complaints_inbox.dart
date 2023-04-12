6e3c1fbd80109331d036336b4d8279f2198c8467
Neelakantan S <118709917+neel-tw@users.noreply.github.com>
2023-04-12 12:52:47 +0530

    [hlm-2297]: Revise complaints inbox bloc

--- apps/health_campaign_field_worker_app/lib/blocs/complaints_inbox/complaints_inbox.dart
+++ apps/health_campaign_field_worker_app/lib/blocs/complaints_inbox/complaints_inbox.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../models/complaints/complaints.dart';
+import '../../utils/typedefs.dart';
 
 part 'complaints_inbox.freezed.dart';
 
@@ -12,34 +13,36 @@ typedef ComplaintsInboxEmitter = Emitter<ComplaintInboxState>;
 
 class ComplaintsInboxBloc
     extends Bloc<ComplaintInboxEvent, ComplaintInboxState> {
-  ComplaintsInboxBloc(super.initialState) {
-    on(_handleSaveComplaints);
+  final PgrServiceDataRepository pgrRepository;
+
+  ComplaintsInboxBloc(
+    super.initialState, {
+    required this.pgrRepository,
+  }) {
+    on(_handleLoadComplaints);
   }
 
-  FutureOr<void> _handleSaveComplaints(
-    ComplaintInboxSaveComplaintsEvent event,
+  FutureOr<void> _handleLoadComplaints(
+    ComplaintInboxLoadComplaintsEvent event,
     ComplaintsInboxEmitter emit,
   ) async {
-    state.maybeMap(
-      orElse: () {
-        return;
-      },
-      complaints: (value) {
-        emit(value.copyWith(complaintInboxItems: event.complaintInboxItems));
-      },
-    );
+
   }
 }
 
 @freezed
 class ComplaintInboxState with _$ComplaintInboxState {
   const factory ComplaintInboxState.complaints({
-    List<ComplaintsInboxItem>? complaintInboxItems,
+    @Default(false) bool loading,
+    @Default([]) List<ComplaintsInboxItem> complaintInboxItems,
   }) = _ComplaintInboxState;
 }
 
 @freezed
 class ComplaintInboxEvent with _$ComplaintInboxEvent {
+  const factory ComplaintInboxEvent.loadComplaints() =
+      ComplaintInboxLoadComplaintsEvent;
+
   const factory ComplaintInboxEvent.saveComplaints({
     List<ComplaintsInboxItem>? complaintInboxItems,
   }) = ComplaintInboxSaveComplaintsEvent;
