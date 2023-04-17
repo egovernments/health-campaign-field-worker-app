c1cfd2d55eb7a462b5cf5a98e9dc2293a856da30
Ajil Oommen <ajil.oommen@egovernments.org>
2023-04-17 16:20:37 +0530

    [hlm-2297]: Update boundary_selection.dart

--- apps/health_campaign_field_worker_app/lib/pages/boundary_selection.dart
+++ apps/health_campaign_field_worker_app/lib/pages/boundary_selection.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:collection/collection.dart';
 import 'package:digit_components/digit_components.dart';
 import 'package:flutter/material.dart';
@@ -11,85 +12,105 @@ class BoundarySelectionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => true,
-      child: Scaffold(
-        appBar: AppBar(),
-        body: BlocBuilder<BoundaryBloc, BoundaryState>(
-          builder: (context, state) {
-            if (state.loading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+    return BlocBuilder<BoundaryBloc, BoundaryState>(
+      builder: (context, state) {
+        final selectedBoundary = state.selectedBoundaryMap.entries
+            .lastWhereOrNull((element) => element.value != null);
 
-            final labelList = state.selectedBoundaryMap.keys.toList();
-
-            return ScrollableContent(
-              slivers: [
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, labelIndex) {
-                      final label = labelList.elementAt(labelIndex);
-
-                      final filteredItems = state.boundaryList.where((element) {
-                        if (element.label != label) return false;
-
-                        if (labelIndex == 0) return true;
-                        final parentIndex = labelIndex - 1;
-
-                        final parentBoundaryEntry = state
-                            .selectedBoundaryMap.entries
-                            .elementAtOrNull(parentIndex);
-                        final parentBoundary = parentBoundaryEntry?.value;
-                        if (parentBoundary == null) return false;
-
-                        if (element.materializedPathList
-                            .contains(parentBoundary.code)) {
-                          return true;
-                        }
-
-                        return false;
-                      }).toList();
-
-                      print('$label ${filteredItems.length}');
-
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: kPadding * 2,
-                        ),
-                        child: DigitDropdown<BoundaryModel>(
-                          value: state.selectedBoundaryMap.entries
-                              .firstWhereOrNull(
-                                (element) => element.key == label,
-                              )
-                              ?.value,
-                          label: label,
-                          menuItems: filteredItems,
-                          onChanged: (value) {
-                            if (value == null) return;
-
-                            context.read<BoundaryBloc>().add(
-                                  BoundarySelectEvent(
-                                    label: label,
-                                    selectedBoundary: value,
-                                  ),
-                                );
-                          },
-                          valueMapper: (value) {
-                            return value.name ?? value.code ?? 'No Value';
-                          },
-                        ),
-                      );
-                    },
-                    childCount: labelList.length,
-                  ),
-                ),
-              ],
-            );
+        return WillPopScope(
+          onWillPop: () async {
+            return selectedBoundary != null;
           },
-        ),
-      ),
+          child: Scaffold(
+            body: Builder(
+              builder: (context) {
+                if (state.loading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final labelList = state.selectedBoundaryMap.keys.toList();
+
+                return Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: labelList.length,
+                        itemBuilder: (context, labelIndex) {
+                          final label = labelList.elementAt(labelIndex);
+
+                          final filteredItems =
+                              state.boundaryList.where((element) {
+                            if (element.label != label) return false;
+
+                            if (labelIndex == 0) return true;
+                            final parentIndex = labelIndex - 1;
+
+                            final parentBoundaryEntry = state
+                                .selectedBoundaryMap.entries
+                                .elementAtOrNull(parentIndex);
+                            final parentBoundary = parentBoundaryEntry?.value;
+                            if (parentBoundary == null) return false;
+
+                            if (element.materializedPathList
+                                .contains(parentBoundary.code)) {
+                              return true;
+                            }
+
+                            return false;
+                          }).toList();
+
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: kPadding * 2,
+                            ),
+                            child: DigitDropdown<BoundaryModel>(
+                              value: state.selectedBoundaryMap.entries
+                                  .firstWhereOrNull(
+                                    (element) => element.key == label,
+                                  )
+                                  ?.value,
+                              label: label,
+                              menuItems: filteredItems,
+                              onChanged: (value) {
+                                if (value == null) return;
+
+                                context.read<BoundaryBloc>().add(
+                                      BoundarySelectEvent(
+                                        label: label,
+                                        selectedBoundary: value,
+                                      ),
+                                    );
+                              },
+                              valueMapper: (value) {
+                                return value.name ?? value.code ?? 'No Value';
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    DigitCard(
+                      margin: const EdgeInsets.only(left: 0, right: 0, top: 10),
+                      child: SafeArea(
+                        child: DigitElevatedButton(
+                          onPressed: selectedBoundary == null
+                              ? null
+                              : () {
+                                  context.router.pop();
+                                },
+                          child: const Text('Submit'),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
