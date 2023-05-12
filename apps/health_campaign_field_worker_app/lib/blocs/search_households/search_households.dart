2025a510244ac5895ca58fa4cb7184513e584d16
Ajil Oommen <ajil.oommen@egovernments.org>
2023-05-12 17:12:51 +0530

    [hlm-2054]: Decrease debounce duration

--- apps/health_campaign_field_worker_app/lib/blocs/search_households/search_households.dart
+++ apps/health_campaign_field_worker_app/lib/blocs/search_households/search_households.dart
@@ -41,7 +41,7 @@ class SearchHouseholdsBloc
     on(
       _handleSearchByHouseholdHead,
       transformer: debounce<SearchHouseholdsSearchByHouseholdHeadEvent>(
-        const Duration(milliseconds: 500),
+        const Duration(milliseconds: 100),
       ),
     );
     on(_handleClear);
