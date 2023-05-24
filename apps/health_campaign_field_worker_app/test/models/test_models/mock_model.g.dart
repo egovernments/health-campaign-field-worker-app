77fcb9b68ebe293eb62368d0fed35099ae23d4d5
Ajil Oommen <ajil.oommen@egovernments.org>
2023-05-24 18:39:43 +0530

    [hlm-000]: Add generated files

---
+++ apps/health_campaign_field_worker_app/test/models/test_models/mock_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mock_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_MockDataModel _$$_MockDataModelFromJson(Map<String, dynamic> json) =>
+    _$_MockDataModel(
+      id: json['id'] as String,
+    );
+
+Map<String, dynamic> _$$_MockDataModelToJson(_$_MockDataModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+    };
