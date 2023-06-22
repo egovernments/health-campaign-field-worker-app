eac4d998b39e96932b4953630a4b9fabfe7d98c8
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-06-22 11:20:07 +0530

    Updated example file with a form view

--- packages/digit_components/example/example.dart
+++ packages/digit_components/example/example.dart
@@ -31,7 +31,10 @@ class MyHomePage extends StatefulWidget {
 class MyHomePageState extends State<MyHomePage> {
   String nameKey = 'name';
   String fatherNameKey = 'fatherName';
+  String relationshipKey = 'relationship';
   String mobileNumberKey = 'mobileNumber';
+  String genderKey = 'gender';
+  String dobKey = 'dateOfBirth';
 
   @override
   void initState() {
@@ -44,51 +47,110 @@ class MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        child: ReactiveFormBuilder(
-            form: buildForm,
-            builder: (context, form, child) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    DigitTextFormField(
-                      label: 'Name',
-                      formControlName: nameKey,
-                      isRequired: true,
-                    ),
-                    const DigitDateFormPicker(
+      body: ScrollableContent(
+        header: const Text(
+          'User Details',
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+        ),
+        children: [
+          ReactiveFormBuilder(
+              form: buildForm,
+              builder: (context, form, child) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      DigitTextFormField(
+                        label: 'Name',
+                        formControlName: nameKey,
+                        isRequired: true,
+                        validationMessages: {
+                          'required': (_) => 'Name is required',
+                          'minLength': (_) =>
+                              'Name should be minimum of 2 characters',
+                          'maxLength': (_) =>
+                              'Name should be maximum of 128 characters',
+                        },
+                      ),
+                      DigitTextFormField(
+                        formControlName: fatherNameKey,
+                        isRequired: true,
+                        label: 'Guardian Name',
+                        validationMessages: {
+                          'required': (_) => 'Guardian name is required',
+                          'minLength': (_) =>
+                              'Guardian name should be minimum of 2 characters',
+                          'maxLength': (_) =>
+                              'Guardian name should be maximum of 128 characters',
+                        },
+                      ),
+                      DigitReactiveDropdown<String>(
+                        label: 'Relationship',
+                        menuItems: const ['father', 'husband'],
+                        isRequired: true,
+                        formControlName: relationshipKey,
+                        valueMapper: (value) => value.toUpperCase(),
+                        onChanged: (value) {},
+                        validationMessages: {
+                          'required': (_) =>
+                              'Please mention the relationship with the guardian',
+                        },
+                      ),
+                      DigitTextFormField(
+                        label: 'Mobile Number',
+                        formControlName: mobileNumberKey,
+                        isRequired: true,
+                        minLength: 10,
+                        maxLength: 10,
+                        keyboardType: TextInputType.phone,
+                        validationMessages: {
+                          'required': (_) => 'Mobile number is required',
+                          'minLength': (_) =>
+                              'Mobile number should be of 10 digits',
+                          'maxLength': (_) =>
+                              'Mobile number should be of 10 digits',
+                          'min': (_) => 'Mobile number should start from 6-9',
+                          'max': (_) => 'Mobile number should start from 6-9',
+                        },
+                      ),
+                      DigitDateFormPicker(
                         label: 'Date of birth',
                         isRequired: true,
                         icon: Icons.info_outline_rounded,
-                        formControlName: 'dateOfBirth',
+                        formControlName: dobKey,
                         autoValidation: AutovalidateMode.always,
-                        requiredMessage: 'Date of birth is required'),
-                    DigitReactiveDropdown<String>(
-                      label: 'Gender',
-                      menuItems: const ['Male', 'Female', 'Transgender'],
-                      formControlName: 'socialCategory',
-                      valueMapper: (value) => value,
-                      onChanged: (value) {},
-                    ),
-                    DigitElevatedButton(
-                        onPressed: () {
-                          if (form.valid) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text('Yay ! Details Submitted')));
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text(
-                                        'Oops ! Please fill the mandatory details')));
-                          }
-                        },
-                        child: const Text('Submit'))
-                  ],
-                ),
-              );
-            }),
+                        requiredMessage: 'Date of birth is required',
+                      ),
+                      DigitReactiveDropdown<String>(
+                        label: 'Gender',
+                        menuItems: const ['male', 'female', 'transgender'],
+                        formControlName: genderKey,
+                        valueMapper: (value) => value.toUpperCase(),
+                        onChanged: (value) {},
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: DigitElevatedButton(
+                            onPressed: () {
+                              if (form.valid) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content:
+                                            Text('Yay ! Details Submitted')));
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            'Oops ! Please fill the mandatory details')));
+                              }
+                            },
+                            child: const Text('Submit')),
+                      )
+                    ],
+                  ),
+                );
+              })
+        ],
       ), // This trailing comma makes auto-formattig nicer for build methods.
     );
   }
@@ -99,12 +161,22 @@ class MyHomePageState extends State<MyHomePage> {
           Validators.minLength(2),
           Validators.maxLength(128)
         ]),
-        fatherNameKey:
+        fatherNameKey: FormControl<String>(value: '', validators: [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(128)
+        ]),
+        relationshipKey:
             FormControl<String>(value: null, validators: [Validators.required]),
+        dobKey: FormControl<DateTime>(
+            value: null, validators: [Validators.required]),
+        genderKey: FormControl<String>(value: null),
         mobileNumberKey: FormControl<String>(value: '', validators: [
           Validators.required,
           Validators.minLength(10),
-          Validators.maxLength(10)
+          Validators.maxLength(10),
+          Validators.min('5999999999'),
+          Validators.max('9999999999'),
         ])
       });
 }
