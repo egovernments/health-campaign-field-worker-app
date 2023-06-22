cef5215e36dcd3d4ab9a837b899c71a205f2c39f
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-06-22 23:45:10 +0530

    Updated example file

--- packages/digit_components/example/example.dart
+++ packages/digit_components/example/example.dart
@@ -47,110 +47,140 @@ class MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ScrollableContent(
-        header: const Text(
-          'User Details',
-          style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ScrollableContent(
+          header: const Text(
+            'User Details',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+          ),
+          children: [
+            ReactiveFormBuilder(
+                form: buildForm,
+                builder: (context, form, child) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        DigitTextFormField(
+                          label: 'Name',
+                          formControlName: nameKey,
+                          isRequired: true,
+                          validationMessages: {
+                            'required': (_) => 'Name is required',
+                            'minLength': (_) =>
+                                'Name should be minimum of 2 characters',
+                            'maxLength': (_) =>
+                                'Name should be maximum of 128 characters',
+                          },
+                        ),
+                        DigitTextFormField(
+                          formControlName: fatherNameKey,
+                          isRequired: true,
+                          label: 'Guardian Name',
+                          validationMessages: {
+                            'required': (_) => 'Guardian name is required',
+                            'minLength': (_) =>
+                                'Guardian name should be minimum of 2 characters',
+                            'maxLength': (_) =>
+                                'Guardian name should be maximum of 128 characters',
+                          },
+                        ),
+                        DigitReactiveDropdown<String>(
+                          label: 'Relationship',
+                          menuItems: const ['father', 'husband'],
+                          isRequired: true,
+                          formControlName: relationshipKey,
+                          valueMapper: (value) => value.toUpperCase(),
+                          onChanged: (value) {},
+                          validationMessages: {
+                            'required': (_) =>
+                                'Please mention the relationship with the guardian',
+                          },
+                        ),
+                        DigitTextFormField(
+                          label: 'Mobile Number',
+                          formControlName: mobileNumberKey,
+                          isRequired: true,
+                          minLength: 10,
+                          maxLength: 10,
+                          keyboardType: TextInputType.phone,
+                          validationMessages: {
+                            'required': (_) => 'Mobile number is required',
+                            'minLength': (_) =>
+                                'Mobile number should be of 10 digits',
+                            'maxLength': (_) =>
+                                'Mobile number should be of 10 digits',
+                            'min': (_) => 'Mobile number should start from 6-9',
+                            'max': (_) => 'Mobile number should start from 6-9',
+                          },
+                        ),
+                        DigitReactiveDropdown<String>(
+                          label: 'Gender',
+                          menuItems: const ['male', 'female', 'transgender'],
+                          formControlName: genderKey,
+                          valueMapper: (value) => value.toUpperCase(),
+                          onChanged: (value) {},
+                        ),
+                        DigitDateFormPicker(
+                          label: 'Date of birth',
+                          isRequired: true,
+                          icon: Icons.info_outline_rounded,
+                          formControlName: dobKey,
+                          autoValidation: AutovalidateMode.always,
+                          requiredMessage: 'Date of birth is required',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: DigitElevatedButton(
+                              onPressed: () {
+                                form.markAllAsTouched();
+                                if (!form.valid) return;
+                                DigitDialog.show(context,
+                                    options: DigitDialogOptions(
+                                        titleIcon: const Icon(
+                                          Icons.warning,
+                                          color: Colors.red,
+                                        ),
+                                        titleText: 'Warning',
+                                        contentText:
+                                            'Please confirm your details before submitting',
+                                        primaryAction: DigitDialogActions(
+                                          label: 'Confirm',
+                                          action: (BuildContext context) {
+                                            if (form.valid) {
+                                              form.reset();
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(const SnackBar(
+                                                      content: Text(
+                                                          'Yay ! Details Submitted')));
+                                            } else {
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(const SnackBar(
+                                                      content: Text(
+                                                          'Oops ! Please fill the mandatory details')));
+                                            }
+                                            Navigator.of(context,
+                                                    rootNavigator: true)
+                                                .pop();
+                                          },
+                                        ),
+                                        secondaryAction: DigitDialogActions(
+                                          label: 'Back',
+                                          action: (BuildContext context) =>
+                                              Navigator.of(context,
+                                                      rootNavigator: true)
+                                                  .pop(),
+                                        )));
+                              },
+                              child: const Text('Submit')),
+                        )
+                      ],
+                    ),
+                  );
+                })
+          ],
         ),
-        children: [
-          ReactiveFormBuilder(
-              form: buildForm,
-              builder: (context, form, child) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      DigitTextFormField(
-                        label: 'Name',
-                        formControlName: nameKey,
-                        isRequired: true,
-                        validationMessages: {
-                          'required': (_) => 'Name is required',
-                          'minLength': (_) =>
-                              'Name should be minimum of 2 characters',
-                          'maxLength': (_) =>
-                              'Name should be maximum of 128 characters',
-                        },
-                      ),
-                      DigitTextFormField(
-                        formControlName: fatherNameKey,
-                        isRequired: true,
-                        label: 'Guardian Name',
-                        validationMessages: {
-                          'required': (_) => 'Guardian name is required',
-                          'minLength': (_) =>
-                              'Guardian name should be minimum of 2 characters',
-                          'maxLength': (_) =>
-                              'Guardian name should be maximum of 128 characters',
-                        },
-                      ),
-                      DigitReactiveDropdown<String>(
-                        label: 'Relationship',
-                        menuItems: const ['father', 'husband'],
-                        isRequired: true,
-                        formControlName: relationshipKey,
-                        valueMapper: (value) => value.toUpperCase(),
-                        onChanged: (value) {},
-                        validationMessages: {
-                          'required': (_) =>
-                              'Please mention the relationship with the guardian',
-                        },
-                      ),
-                      DigitTextFormField(
-                        label: 'Mobile Number',
-                        formControlName: mobileNumberKey,
-                        isRequired: true,
-                        minLength: 10,
-                        maxLength: 10,
-                        keyboardType: TextInputType.phone,
-                        validationMessages: {
-                          'required': (_) => 'Mobile number is required',
-                          'minLength': (_) =>
-                              'Mobile number should be of 10 digits',
-                          'maxLength': (_) =>
-                              'Mobile number should be of 10 digits',
-                          'min': (_) => 'Mobile number should start from 6-9',
-                          'max': (_) => 'Mobile number should start from 6-9',
-                        },
-                      ),
-                      DigitDateFormPicker(
-                        label: 'Date of birth',
-                        isRequired: true,
-                        icon: Icons.info_outline_rounded,
-                        formControlName: dobKey,
-                        autoValidation: AutovalidateMode.always,
-                        requiredMessage: 'Date of birth is required',
-                      ),
-                      DigitReactiveDropdown<String>(
-                        label: 'Gender',
-                        menuItems: const ['male', 'female', 'transgender'],
-                        formControlName: genderKey,
-                        valueMapper: (value) => value.toUpperCase(),
-                        onChanged: (value) {},
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: DigitElevatedButton(
-                            onPressed: () {
-                              if (form.valid) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content:
-                                            Text('Yay ! Details Submitted')));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text(
-                                            'Oops ! Please fill the mandatory details')));
-                              }
-                            },
-                            child: const Text('Submit')),
-                      )
-                    ],
-                  ),
-                );
-              })
-        ],
       ), // This trailing comma makes auto-formattig nicer for build methods.
     );
   }
