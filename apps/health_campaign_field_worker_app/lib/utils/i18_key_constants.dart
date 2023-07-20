ab53cf3963f116aa72c1dc9376ce4a9a8fdeeb08
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2023-07-20 10:10:07 +0530

    HLM-3161:: Age Split in Table and css fixes for member card

--- apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
@@ -179,6 +179,8 @@ class SearchBeneficiary {
   String get beneficiaryAddActionLabel => 'BENEFICIARY_ADD_ACTION_LABEL';
 
   String get iconLabel => 'ICON_LABEL';
+  String get yearsAbbr => 'YEARS_ABBR';
+  String get monthsAbbr => 'MONTHS_ABBR';
 }
 
 class IndividualDetails {
