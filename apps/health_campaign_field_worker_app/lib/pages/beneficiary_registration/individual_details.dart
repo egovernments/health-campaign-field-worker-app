984549fd573febf85b0a769d659080d1e8f79a81
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2023-10-04 18:15:33 +0530

    HLM-1890:: If Default identifier is ID, "ID Number" field should be hidden from the UI. (#208)

--- apps/health_campaign_field_worker_app/lib/pages/beneficiary_registration/individual_details.dart
+++ apps/health_campaign_field_worker_app/lib/pages/beneficiary_registration/individual_details.dart
@@ -310,6 +310,8 @@ class _IndividualDetailsPageState
                               },
                             ),
                           ),
+                          if(form.control(_idTypeKey).value !=
+                              'DEFAULT')
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
