9dada80cb32eb1a17677ecf2a43c73fcf9fb209c
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2024-01-19 20:15:21 +0530

    Downsync remote repository Initialization fix (#348)

--- apps/health_campaign_field_worker_app/lib/utils/constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/constants.dart
@@ -35,6 +35,7 @@ import '../data/repositories/local/stock_reconciliation.dart';
 import '../data/repositories/local/task.dart';
 import '../data/repositories/oplog/oplog.dart';
 import '../data/repositories/remote/boundary.dart';
+import '../data/repositories/remote/downsync.dart';
 import '../data/repositories/remote/facility.dart';
 import '../data/repositories/remote/h_f_referral.dart';
 import '../data/repositories/remote/household.dart';
@@ -227,6 +228,8 @@ class Constants {
           SideEffectRemoteRepository(dio, actionMap: actions),
         if (value == DataModelType.referral)
           ReferralRemoteRepository(dio, actionMap: actions),
+        if (value == DataModelType.downsync)
+          DownsyncRemoteRepository(dio, actionMap: actions),
         if (value == DataModelType.hFReferral)
           HFReferralRemoteRepository(dio, actionMap: actions),
       ]);
