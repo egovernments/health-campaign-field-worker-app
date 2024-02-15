29a389919d6576c06e24ead893c49fcc6207b4ca
Naveen Renati <naveen.renati@egovernments.org>
2024-02-15 18:22:51 +0530

    modified sql_store path for address import fix

--- tools/install_bricks.sh
+++ tools/install_bricks.sh
@@ -89,6 +89,13 @@ rm "$temp_mappers"
 
 # for adding import as at.Address in sql_store.g.dart for resolving conflict with Address created inside sql_store.g.dart
 
-sql_g_dart_file="$app_root/data/local_store/sql_store/sql_store.g.dart"
+ls -la
+
+cd apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/ || exit
+app_root="$PWD"
+
+ls -la
+
+sql_g_dart_file="$app_root/sql_store.g.dart"
 
 sed -i '0,/class \$AddressTable extends Address/s//class \$AddressTable extends at.Address/' sql_store.g.dart
