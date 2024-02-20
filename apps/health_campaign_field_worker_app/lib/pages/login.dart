07da03bcf02efd4008d0666e4d297ec63fc21a84
naveen-egov <naveen.j@egovernments.org>
2024-02-20 19:08:31 +0530

    removed login placehoulder

--- apps/health_campaign_field_worker_app/lib/pages/login.dart
+++ apps/health_campaign_field_worker_app/lib/pages/login.dart
@@ -201,12 +201,12 @@ class _LoginPageState extends LocalizedState<LoginPage> {
 
   FormGroup buildForm() => fb.group(<String, Object>{
         _userId: FormControl<String>(
-          value: 'amp-user',
+          value: '',
           validators: [Validators.required],
         ),
         _password: FormControl<String>(
           validators: [Validators.required],
-          value: 'eGov@1234',
+          value: '',
         ),
       });
 }
