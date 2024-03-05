fde49ed299b8fc231acf0e37b583aabab570550e
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2024-03-05 12:48:38 +0530

    Added service request mapper for up sync

--- apps/health_campaign_field_worker_app/lib/data/local_store/no_sql/schema/oplog.dart
+++ apps/health_campaign_field_worker_app/lib/data/local_store/no_sql/schema/oplog.dart
@@ -52,6 +52,10 @@ class OpLog {
         final entity = HFReferralModelMapper.fromJson(entityString);
         return entity;
 
+      case "service":
+        final entity = ServiceModelMapper.fromJson(entityString);
+        return entity;
+
       default:
         final entity = EntityModelMapper.fromJson(entityString);
         return entity;
