398ddf5a8c41535aa74e7148a7d36ee4b78c3856
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2024-03-18 12:18:32 +0530

    Revert "Added auto generated files"
    This reverts commit eb1df08b89e72d81eb3dfbf90d5a069c93e20e90.

--- apps/health_campaign_field_worker_app/lib/pages/login.dart
+++ apps/health_campaign_field_worker_app/lib/pages/login.dart
@@ -195,7 +195,7 @@ class _LoginPageState extends LocalizedState<LoginPage> {
 
   FormGroup buildForm() => fb.group(<String, Object>{
         _userId: FormControl<String>(
-          value: 'Distributor',
+          value: '',
           validators: [Validators.required],
         ),
         _password: FormControl<String>(
