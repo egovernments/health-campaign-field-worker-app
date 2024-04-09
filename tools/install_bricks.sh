b116f7b8e840b52bc42e087d09ecb7df909c5a4c
Naveen Renati <naveen.renati@egovernments.org>
2024-04-09 16:02:22 +0530

    corrected path for sql_store file to fix build fail

--- tools/install_bricks.sh
+++ tools/install_bricks.sh
@@ -36,7 +36,7 @@ bash inventory_package_imports.sh
 bash digit_scanner_imports.sh
 
 # for adding import as at.Address in sql_store.g.dart for resolving conflict with Address created inside sql_store.g.dart
-cd apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/ || exit
+cd ../apps/health_campaign_field_worker_app/lib/data/local_store/sql_store/ || exit
 
 app_root="$PWD"
 
