d3c874a45bd901ce737c2f735f0d8027c0135715
Satwik-egov <venkata.satwik@egoverments.org>
2024-04-30 16:13:03 +0530

    changed boundary selection method

---
+++ apps/health_campaign_field_worker_app/integration_test/test_variables.dart
@@ -0,0 +1,62 @@
+import 'package:digit_components/digit_components.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:health_campaign_field_worker_app/widgets/home/home_item_card.dart';
+
+Map<String, dynamic> getTestData() {
+  return {
+    'session': 'Evening session',
+    'date': '25',
+    'username': 'Reg-2',
+    'password': 'eGov@1234',
+    'attendees': [
+      'Ram',
+      'Syed',
+    ],
+  };
+}
+
+// if the names of the following buttons change in the app, you can change them here
+Map<String, Finder> getWidgets() {
+  //login
+  final username = find.byType(DigitTextFormField).first;
+  final password = find.byType(DigitTextFormField).last;
+  final continueButton = find.widgetWithText(DigitElevatedButton, 'Continue');
+  final login = find.byType(DigitElevatedButton);
+  final submit = find.widgetWithText(
+    DigitElevatedButton,
+    'Submit',
+  );
+
+  //attendance
+  final markAttendanceButton =
+      find.widgetWithText(DigitElevatedButton, 'Mark Attendance');
+  final manageAttendanceButton =
+      find.widgetWithText(HomeItemCard, 'Manage Attendance');
+  final openRegister = find.text('Open Register').at(3);
+
+  //inventory
+  final manageStock = find.widgetWithText(HomeItemCard, 'Manage Stock');
+
+  return {
+    'username': username,
+    'password': password,
+    'continue': continueButton,
+    'login': login,
+    'submit': submit,
+    'manageAttendance': manageAttendanceButton,
+    'markAttendance': markAttendanceButton,
+    'openRegister': openRegister,
+    'manageStock': manageStock,
+  };
+}
+
+Map<String, String> getBoundaryName() {
+  return {
+    'Country': 'MOÇAMBIQUE',
+    'Provincia': 'NAMPULA',
+    'Distrito': 'MURRUPULA',
+    'Posto Administrativo': 'NIHESSIUE',
+    'Localidade': 'NIHESSIUE',
+    'Aldeia': 'CAVINA1',
+  };
+}
