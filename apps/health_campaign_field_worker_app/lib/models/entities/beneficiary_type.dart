7cd04f4f0657f44b5e8a81ab1137e0b9ef370caa
amitverma-egov <amit.verma@egovernments.org>
2024-05-10 12:40:03 +0530

    updated beneficiary type

--- apps/health_campaign_field_worker_app/lib/models/entities/beneficiary_type.dart
+++ apps/health_campaign_field_worker_app/lib/models/entities/beneficiary_type.dart
@@ -7,5 +7,10 @@ enum BeneficiaryType {
   @MappableValue("INDIVIDUAL") individual,
   @MappableValue("HOUSEHOLD") household,
   @MappableValue("PRODUCT") product,
+  @MappableValue("5-14YEARS") individual1,
+  @MappableValue("14+YEARS") individual2,
+  @MappableValue("15+YEARS") individual3,
+  @MappableValue("IVERMECTIN") ivermectin,
+  @MappableValue("ALBENDAZOLE") albendazole,
   ;
 }
