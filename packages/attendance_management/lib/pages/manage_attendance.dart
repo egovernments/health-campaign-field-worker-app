007b791470568053b8fb0ecc0cbdc66638ea613c
Naveen Renati <naveen.renati@egovernments.org>
2024-05-11 19:25:10 +0530

    pub.dev publish related changes

--- packages/attendance_management/lib/pages/manage_attendance.dart
+++ packages/attendance_management/lib/pages/manage_attendance.dart
@@ -5,15 +5,12 @@ import 'package:auto_route/auto_route.dart';
 import 'package:digit_components/digit_components.dart';
 import 'package:digit_components/utils/date_utils.dart';
 import 'package:digit_components/widgets/atoms/digit_toaster.dart';
-import 'package:digit_data_model/data/repositories/local/individual.dart';
 import 'package:digit_data_model/models/entities/individual.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../utils/i18_key_constants.dart' as i18;
-import '../models/entities/enum_values.dart';
 import '../router/attendance_router.gm.dart';
-import '../utils/utils.dart';
 import '../widgets/back_navigation_help_header.dart';
 import '../widgets/localized.dart';
 import '../widgets/no_result_card.dart';
