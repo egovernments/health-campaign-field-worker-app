051612cb6d7ed12f1bf15997b4080f92b95a1f36
Satwik-egov <venkata.satwik@egoverments.org>
2024-05-14 15:13:51 +0530

    checking if new entry is added

--- apps/health_campaign_field_worker_app/integration_test/inventory_package/pages/view_reports.dart
+++ apps/health_campaign_field_worker_app/integration_test/inventory_package/pages/view_reports.dart
@@ -1,6 +1,7 @@
 import 'package:digit_components/digit_components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
 
 import '../../test_variables.dart';
 
@@ -13,21 +14,31 @@ Future<void> testViewReportsPage(WidgetTester widgetTester) async {
   await widgetTester.pumpAndSettle(const Duration(milliseconds: 1000));
 
   for (int i = 0; i < 6; i++) {
-    testOnePage(widgetTester);
+    await testOnePage(widgetTester);
   }
 }
 
 Future<void> testOnePage(WidgetTester widgetTester) async {
   await widgetTester.tap(find.byType(DigitListView).first);
-  await widgetTester.pumpAndSettle(const Duration(milliseconds: 1000));
+  await widgetTester.pumpAndSettle(const Duration(milliseconds: 2000));
 
   await widgetTester.tap(find.byKey(const Key('viewReportsFacility')));
-  await widgetTester.pumpAndSettle(const Duration(milliseconds: 50));
+  await widgetTester.pumpAndSettle(const Duration(milliseconds: 1000));
   await widgetTester.tap(find.bySemanticsLabel(RegExp('FAC_F*')).first);
-  await widgetTester.pumpAndSettle(const Duration(milliseconds: 50));
+  await widgetTester.pumpAndSettle(const Duration(milliseconds: 1000));
 
   await widgetTester.tap(find.byKey(const Key('viewReportSelectProduct')));
-  await widgetTester.pumpAndSettle(const Duration(milliseconds: 50));
+  await widgetTester.pumpAndSettle(const Duration(milliseconds: 1000));
   await widgetTester.tap(find.text('Bednet Grade 1'));
-  await widgetTester.pumpAndSettle(const Duration(milliseconds: 50));
+  await widgetTester.pumpAndSettle(const Duration(milliseconds: 1000));
+
+  expect(
+      find.text(
+          '${DateTime.now().day} ${DateFormat('MMMM').format(DateTime.now())} ${DateTime.now().year}'),
+      findsAtLeast(1));
+
+  //come back to the home page
+  await widgetTester
+      .tap(find.widgetWithText(DigitElevatedButton, 'Back To Home'));
+  await widgetTester.pumpAndSettle(const Duration(milliseconds: 1000));
 }
