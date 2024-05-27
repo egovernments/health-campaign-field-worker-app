fc161298c4f1c55dc95c7f178125f961351a0411
Naveen Renati <naveen.renati@egovernments.org>
2024-05-27 16:02:09 +0530

    updated pub versions for digit_components, digit_showcase, inventory_management
    replaced deprecated .pop method

--- apps/health_campaign_field_worker_app/lib/pages/complaints_acknowledgement.dart
+++ apps/health_campaign_field_worker_app/lib/pages/complaints_acknowledgement.dart
@@ -24,7 +24,7 @@ class _ComplaintsAcknowledgementPageState
     return Scaffold(
       body: DigitAcknowledgement.success(
         action: () {
-          context.router.pop();
+          context.router.maybePop();
         },
         actionLabel:
             localizations.translate(i18.complaints.acknowledgementAction),
