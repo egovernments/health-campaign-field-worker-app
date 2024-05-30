643ee5aeb8dd7897b3d9c1ada44a8d1321f1fdaa
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2024-05-30 12:55:08 +0530

    Reason for deletion-Added mandatory check and removed duplicate repository initialization

--- apps/health_campaign_field_worker_app/lib/widgets/network_manager_provider_wrapper.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/network_manager_provider_wrapper.dart
@@ -257,21 +257,6 @@ class NetworkManagerProviderWrapper extends StatelessWidget {
           StockReconciliationOpLogManager(isar),
         ),
       ),
-      RepositoryProvider<
-          LocalRepository<AttendanceRegisterModel,
-              AttendanceRegisterSearchModel>>(
-        create: (_) => AttendanceLocalRepository(
-          sql,
-          AttendanceOpLogManager(isar),
-        ),
-      ),
-      RepositoryProvider<
-          LocalRepository<AttendanceLogModel, AttendanceLogSearchModel>>(
-        create: (_) => AttendanceLogsLocalRepository(
-          sql,
-          AttendanceLogOpLogManager(isar),
-        ),
-      ),
       RepositoryProvider<RegistrationDeliveryAddressRepo>(
         create: (_) => RegistrationDeliveryAddressRepo(
           sql,
