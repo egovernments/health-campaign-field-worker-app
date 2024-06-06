4b01d5d8f32eaf31e9653345bb3721c10658e706
amitverma-egov <amit.verma@egovernments.org>
2024-06-06 18:06:41 +0530

    revert changes to install bricks

--- tools/install_bricks.sh
+++ tools/install_bricks.sh
@@ -1,12 +1,12 @@
 home="$PWD"
 
-flutter pub global activate mason_cli
-flutter pub global activate melos
+dart pub global activate mason_cli
+dart pub global activate melos
 
-melos.bat run generate-hooks
+melos run generate-hooks
 
-mason.bat add digit_entity --path ./mason_templates/digit_entity -g
-mason.bat add freezed_bloc --path ./mason_templates/freezed_bloc -g
+mason add digit_entity --path ./mason_templates/digit_entity -g
+mason add freezed_bloc --path ./mason_templates/freezed_bloc -g
 
 cd apps/health_campaign_field_worker_app/lib || exit
 app_root="$PWD"
@@ -15,14 +15,14 @@ cd models/model_configs || exit
 
 for file in *.json ; do
   echo "$file"
-  mason.bat make digit_entity -c "$file" --on-conflict overwrite -o "$app_root"
+  mason make digit_entity -c "$file" --on-conflict overwrite -o "$app_root"
 done
 
 cd "$home" || exit
 echo "$PWD"
 
-melos.bat clean
-melos.bat bootstrap
+melos clean
+melos bootstrap
 
 # for adding imports and mappers of attendance
 
