f9e71b51adbd3a0e6d7a0b58c6429b0b009bd967
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2024-06-28 10:28:40 +0530

    Added disable battery optimization ask permission on App start and background service start

--- apps/health_campaign_field_worker_app/lib/app.dart
+++ apps/health_campaign_field_worker_app/lib/app.dart
@@ -3,7 +3,6 @@ import 'package:digit_components/digit_components.dart';
 import 'package:digit_data_model/data_model.dart';
 import 'package:digit_scanner/blocs/scanner.dart';
 import 'package:dio/dio.dart';
-import 'package:disable_battery_optimization/disable_battery_optimization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:inventory_management/inventory_management.dart';
@@ -50,16 +49,7 @@ class MainApplicationState extends State<MainApplication>
   @override
   void initState() {
     super.initState();
-    _requestDisableBatteryOptimization();
-  }
-
-  Future<void> _requestDisableBatteryOptimization() async {
-    bool isIgnoringBatteryOptimizations =
-        await DisableBatteryOptimization.isBatteryOptimizationDisabled ?? false;
-
-    if (!isIgnoringBatteryOptimizations) {
-      await DisableBatteryOptimization.showDisableBatteryOptimizationSettings();
-    }
+    requestDisableBatteryOptimization();
   }
 
   @override
