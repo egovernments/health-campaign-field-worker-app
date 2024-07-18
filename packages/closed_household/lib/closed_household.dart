885be409ec9a5ac92e090d470faf41519c4d8d09
rachana-egov <137176770+rachana-egov@users.noreply.github.com>
2024-07-18 17:23:20 +0530

    updated remote repository and some miner fixes (#461)
    Co-authored-by: rachana-egov <rachana.singh@egovernment.org>

--- packages/closed_household/lib/closed_household.dart
+++ packages/closed_household/lib/closed_household.dart
@@ -1,7 +1,14 @@
 library closed_household;
 
 export 'blocs/app_localization.dart';
+export 'blocs/closed_household.dart';
+export 'models/entities/user_action.dart';
+export 'models/entities/action.dart';
+export 'models/entities/status.dart';
 export 'utils/utils.dart' show ClosedHouseholdSingleton;
 export 'pages/closed_household_details.dart';
 export 'pages/closed_household_summary.dart';
+export 'pages/closed_household_acknowledgement.dart';
+export 'pages/closed_household_wrapper.dart';
+
 
