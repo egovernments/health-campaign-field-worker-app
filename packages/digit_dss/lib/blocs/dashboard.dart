3f3fb2bc8148f31ad0dd6cd2f29f5e9d3fd9546a
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2024-08-02 10:25:16 +0530

    Added code comments for digit_dss package

--- packages/digit_dss/lib/blocs/dashboard.dart
+++ packages/digit_dss/lib/blocs/dashboard.dart
@@ -1,61 +1,66 @@
-import 'dart:async';
+import 'dart:async'; // Import the dart:async library for asynchronous operations
 
-import 'package:collection/collection.dart';
-import 'package:digit_components/models/digit_table_model.dart';
-import 'package:digit_components/theme/colors.dart';
-import 'package:digit_components/theme/digit_theme.dart';
-import 'package:digit_dss/digit_dss.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:isar/isar.dart';
+import 'package:collection/collection.dart'; // Import the collection package for collection utilities
+import 'package:digit_components/models/digit_table_model.dart'; // Import the digit_table_model.dart file from the digit_components package
+import 'package:digit_components/theme/colors.dart'; // Import the colors.dart file from the digit_components package
+import 'package:digit_components/theme/digit_theme.dart'; // Import the digit_theme.dart file from the digit_components package
+import 'package:digit_dss/digit_dss.dart'; // Import the digit_dss.dart file from the digit_dss package
+import 'package:flutter/cupertino.dart'; // Import the cupertino.dart file from the flutter package
+import 'package:flutter_bloc/flutter_bloc.dart'; // Import the flutter_bloc package for state management
+import 'package:freezed_annotation/freezed_annotation.dart'; // Import the freezed_annotation package for code generation
+import 'package:isar/isar.dart'; // Import the isar package for database management
 
-part 'dashboard.freezed.dart';
+part 'dashboard.freezed.dart'; // Generate the part file for freezed annotations
 
-typedef DashboardEmitter = Emitter<DashboardState>;
+typedef DashboardEmitter = Emitter<
+    DashboardState>; // Define a typedef for the Emitter of DashboardState
 
 class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
-  final Isar isar;
-  BuildContext context;
-  final DashboardRemoteRepository dashboardRemoteRepo;
+  final Isar isar; // Isar database instance
+  BuildContext context; // BuildContext for accessing UI elements
+  final DashboardRemoteRepository
+      dashboardRemoteRepo; // Remote repository for dashboard data
+
   DashboardBloc(
     super.initialState, {
     required this.context,
     required this.isar,
     required this.dashboardRemoteRepo,
   }) {
-    on(_handleSearch);
-    on(_handleRefresh);
+    on(_handleSearch); // Register the _handleSearch event handler
+    on(_handleRefresh); // Register the _handleRefresh event handler
   }
 
+  // Event handler for refreshing the dashboard
   FutureOr<void> _handleRefresh(
     DashboardRefreshEvent event,
     DashboardEmitter emit,
   ) async {
-    emit(const DashboardState.loading());
+    emit(const DashboardState.loading()); // Emit loading state
     bool enableDashboard = DashboardSingleton()
             .selectedProject
             ?.additionalDetails
             ?.enableDashboard ??
-        false;
+        false; // Check if dashboard is enabled
     Map<String, List<String>> dashboardConfig = DashboardSingleton()
             .selectedProject
             ?.additionalDetails
             ?.dashboardConfig ??
-        {};
+        {}; // Get dashboard configuration
     if (!enableDashboard || dashboardConfig.keys.isEmpty) {
-      emit(const DashboardErrorState());
+      emit(
+          const DashboardErrorState()); // Emit error state if dashboard is not enabled or config is empty
     } else {
-      final isConnected = await getIsConnected();
+      final isConnected = await getIsConnected(); // Check network connectivity
       if (isConnected && event.syncFromServer) {
         final startDate = DateTime(event.selectedDate.year,
                 event.selectedDate.month, event.selectedDate.day)
             .toLocal()
-            .millisecondsSinceEpoch;
+            .millisecondsSinceEpoch; // Get start date in milliseconds
         final endDate = DateTime(event.selectedDate.year,
                 event.selectedDate.month, event.selectedDate.day, 11, 59)
             .toLocal()
-            .millisecondsSinceEpoch;
+            .millisecondsSinceEpoch; // Get end date in milliseconds
 
         try {
           await processDashboardConfig(
@@ -68,21 +73,24 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
             DashboardSingleton().actionPath,
             DashboardSingleton().tenantId,
             DashboardSingleton().projectId,
-          );
+          ); // Process dashboard configuration
 
-          add(DashboardEvent.handleSearch(selectedDate: event.selectedDate));
+          add(DashboardEvent.handleSearch(
+              selectedDate: event.selectedDate)); // Trigger search event
         } catch (e) {
-          print(e);
-          emit(const DashboardErrorState());
+          print(e); // Print error
+          emit(const DashboardErrorState()); // Emit error state
         }
       } else if (!isConnected && event.syncFromServer) {
-        emit(const DashboardErrorState());
+        emit(
+            const DashboardErrorState()); // Emit error state if not connected and sync is required
       } else {
-        add(const DashboardEvent.handleSearch());
+        add(const DashboardEvent.handleSearch()); // Trigger search event
       }
     }
   }
 
+  // Event handler for searching the dashboard
   FutureOr<void> _handleSearch(
     DashboardSearchEvent event,
     DashboardEmitter emit,
@@ -92,9 +100,9 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
           .where()
           .filter()
           .chartTypeEqualTo('metric')
-          .findAll();
-      Map<String, MetricWrapper> metrics = {};
-      List<TableWrapper> tableWrapperList = [];
+          .findAll(); // Query metric charts from Isar database
+      Map<String, MetricWrapper> metrics = {}; // Initialize metrics map
+      List<TableWrapper> tableWrapperList = []; // Initialize table wrapper list
       for (DashboardResponse chart in metricCharts) {
         if ((chart.data ?? []).isNotEmpty) {
           for (DashboardChartData data in (chart.data ?? [])) {
@@ -104,7 +112,7 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
                 value: data.headerValue ?? '0',
                 insight: data.insight,
               ),
-            });
+            }); // Populate metrics map
           }
         }
       }
@@ -112,10 +120,10 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
           .where()
           .filter()
           .chartTypeEqualTo('xtable')
-          .findAll();
+          .findAll(); // Query table charts from Isar database
       for (DashboardResponse chart in tableCharts) {
         if ((chart.data ?? []).isNotEmpty) {
-          // TotalDetails totalDetails = TotalDetails();
+          // Create table headers
           final List<TableHeader> tableHeaderList = chart.data?.first.plots
                   ?.where((p) =>
                       p.name != "S.N." &&
@@ -131,6 +139,7 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
               }).toList() ??
               [];
 
+          // Create table details
           final tableDetails = chart.data?.map((e) {
             final rowTableData = e.plots
                 ?.where((p) =>
@@ -164,37 +173,6 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
             headerList: tableHeaderList,
             tableData: tableDetails ?? [],
           ));
-
-          // for (int i = 0; i < (chart.data ?? []).length; i++) {
-          //   totalDetails.noOfHousesVisited +=
-          //       int.parse(tableDetails?[i]['noOfHousesVisited'].toString());
-          //   totalDetails.noOfHousesSprayed +=
-          //       int.parse(chartData[i]['noOfHousesSprayed'].toString());
-          //   totalDetails.noOfHousesNotSprayed +=
-          //       (int.parse(chartData[i]['noOfHousesVisited'].toString()) -
-          //           int.parse(chartData[i]['noOfHousesSprayed'].toString()));
-          //   totalDetails.bottlesUsed +=
-          //       int.parse(chartData[i]['bottlesUsed'].toString());
-          //   totalDetails.noOfRemainingBottles +=
-          //       (int.parse(chartData[i]['totalBottles'].toString()) -
-          //           int.parse(chartData[i]['bottlesUsed'].toString()));
-          // }
-          //
-          // tableData.add(TableDataRow([
-          //   TableData('Total'),
-          //   TableData(
-          //     totalDetails.noOfHousesVisited.toString(),
-          //     style:
-          //         DigitTheme.instance.mobileTheme.textTheme.bodyMedium?.apply(
-          //       color: const DigitColors().darkSpringGreen,
-          //     ),
-          //   ),
-          //   TableData(totalDetails.noOfHousesSprayed.toString()),
-          //   TableData(totalDetails.noOfHousesNotSprayed.toString()),
-          //   TableData(totalDetails.bottlesUsed.toString()),
-          //   TableData(totalDetails.noOfRemainingBottles.toString()),
-          //   TableData(''),
-          // ]));
         }
       }
 
@@ -204,40 +182,45 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
         selectedDate: metricCharts.firstOrNull?.lastSelectedDate ??
             event.selectedDate ??
             DateTime.now(),
-      ));
+      )); // Emit fetched state with metric and table data
     } catch (error) {
-      rethrow;
+      rethrow; // Rethrow any caught errors
     }
   }
 }
 
+// Define DashboardEvent with freezed annotations
 @freezed
 class DashboardEvent with _$DashboardEvent {
   const factory DashboardEvent.handleSearch({
     DateTime? selectedDate,
-  }) = DashboardSearchEvent;
+  }) = DashboardSearchEvent; // Define handleSearch event
   const factory DashboardEvent.handleRefresh({
     required DateTime selectedDate,
     required String projectId,
     @Default(false) bool syncFromServer,
-  }) = DashboardRefreshEvent;
+  }) = DashboardRefreshEvent; // Define handleRefresh event
 }
 
+// Define DashboardState with freezed annotations
 @freezed
 class DashboardState with _$DashboardState {
-  const factory DashboardState.loading() = DashboardLoadingState;
+  const factory DashboardState.loading() =
+      DashboardLoadingState; // Define loading state
   const factory DashboardState.fetched({
     Map<String, MetricWrapper>? metricData,
     List<TableWrapper>? tableData,
     DateTime? selectedDate,
-  }) = DashboardFetchedState;
-  const factory DashboardState.error() = DashboardErrorState;
+  }) = DashboardFetchedState; // Define fetched state
+  const factory DashboardState.error() =
+      DashboardErrorState; // Define error state
 }
 
+// Class for wrapping metric data
 class MetricWrapper {
-  final String header;
-  final String value;
-  final Insight? insight;
+  final String header; // Header of the metric
+  final String value; // Value of the metric
+  final Insight? insight; // Insight related to the metric
 
   MetricWrapper({
     required this.header,
@@ -246,9 +229,10 @@ class MetricWrapper {
   });
 }
 
+// Class for wrapping table data
 class TableWrapper {
-  final List<TableHeader> headerList;
-  final List<TableDataRow> tableData;
+  final List<TableHeader> headerList; // List of table headers
+  final List<TableDataRow> tableData; // List of table data rows
 
   TableWrapper({
     required this.headerList,
--- packages/digit_dss/lib/data/remote/dashboard.dart
+++ packages/digit_dss/lib/data/remote/dashboard.dart
@@ -1,35 +1,40 @@
-import 'dart:async';
-import 'dart:convert';
+import 'dart:async'; // Import the dart:async library for asynchronous operations
+import 'dart:convert'; // Import the dart:convert library for JSON encoding and decoding
 
-import 'package:digit_components/utils/app_logger.dart';
-import 'package:digit_dss/data/local_store/no_sql/schema/dashboard_response.dart';
-import 'package:dio/dio.dart';
-import 'package:isar/isar.dart';
+import 'package:digit_components/utils/app_logger.dart'; // Import the app_logger.dart file from the digit_components package
+import 'package:digit_dss/data/local_store/no_sql/schema/dashboard_response.dart'; // Import the dashboard_response.dart file from the digit_dss package
+import 'package:dio/dio.dart'; // Import the dio package for HTTP client functionality
+import 'package:isar/isar.dart'; // Import the isar package for database management
 
-import '../../models/entities/dashboard_response_model.dart';
+import '../../models/entities/dashboard_response_model.dart'; // Import the dashboard_response_model.dart file from the models/entities directory
 
+// DashboardRemoteRepository class handles remote API requests and database transactions
 class DashboardRemoteRepository {
-  final Dio _client;
-  DashboardRemoteRepository(this._client);
+  final Dio _client; // Dio HTTP client instance
+  DashboardRemoteRepository(
+      this._client); // Constructor to initialize the Dio client
 
+  // Method to search data from the API and write it to the Isar database
   FutureOr<void> searchAndWriteToDB({
-    required String apiEndPoint,
-    Object? query,
-    required String projectId,
-    DateTime? lastSelectedDate,
-    required Isar isar,
+    required String apiEndPoint, // API endpoint URL
+    Object? query, // Query parameters for the API request
+    required String projectId, // Project ID for filtering data
+    DateTime? lastSelectedDate, // Last selected date for filtering data
+    required Isar isar, // Isar database instance
   }) async {
     try {
       final response = await _client.post(
         apiEndPoint,
         data: query,
-      );
+      ); // Make a POST request to the API endpoint with the query parameters
 
+      // Map the response data to the DashboardResponseModel
       final dashboardResponse = DashboardResponseModelMapper.fromMap(
         json.decode(response.data)['responseData'],
       );
 
       if (dashboardResponse != null) {
+        // Write transaction to delete existing data and insert new data
         await isar.writeTxn(() async {
           await isar.dashboardResponses
               .where()
@@ -37,18 +42,27 @@ class DashboardRemoteRepository {
               .projectIdEqualTo(projectId)
               .visualizationCodeEqualTo(dashboardResponse.visualizationCode)
               .chartTypeEqualTo(dashboardResponse.chartType)
-              .deleteAll();
+              .deleteAll(); // Delete existing data matching the project ID, visualization code, and chart type
         });
-        final data = dashboardResponse;
-        final chart = DashboardResponse();
-        chart.chartType = data.chartType;
-        chart.hideHeaderDenomination = data.hideHeaderDenomination;
-        chart.lastSelectedDate = lastSelectedDate ?? DateTime.now();
-        chart.visualizationCode = data.visualizationCode;
-        chart.projectId = projectId;
-        chart.drillDownChartId = data.drillDownChartId;
-        chart.hideInsights = data.hideInsights;
-        chart.showLabel = data.showLabel;
+
+        final data =
+            dashboardResponse; // Assign the dashboard response data to a local variable
+        final chart =
+            DashboardResponse(); // Create a new DashboardResponse instance
+        chart.chartType = data.chartType; // Set chart type
+        chart.hideHeaderDenomination =
+            data.hideHeaderDenomination; // Set hide header denomination flag
+        chart.lastSelectedDate =
+            lastSelectedDate ?? DateTime.now(); // Set last selected date
+        chart.visualizationCode =
+            data.visualizationCode; // Set visualization code
+        chart.projectId = projectId; // Set project ID
+        chart.drillDownChartId =
+            data.drillDownChartId; // Set drill down chart ID
+        chart.hideInsights = data.hideInsights; // Set hide insights flag
+        chart.showLabel = data.showLabel; // Set show label flag
+
+        // Map the data plots to the DashboardChartData and DashboardPlot instances
         final dataPlots = data.data?.map((c) {
           final chartData = DashboardChartData();
           chartData.headerValue = c.headerValue;
@@ -63,6 +77,8 @@ class DashboardRemoteRepository {
             plot.symbol = p.symbol;
             return plot;
           }).toList();
+
+          // Map the insight data to the Insight instance
           final insight = Insight();
           insight.value = c.insight?.value;
           insight.name = c.insight?.name;
@@ -71,19 +87,22 @@ class DashboardRemoteRepository {
           chartData.insight = insight;
           return chartData;
         }).toList();
-        chart.data = dataPlots;
+        chart.data = dataPlots; // Set the data plots
+
+        // Write transaction to insert the new chart data
         return await isar.writeTxn(() async {
           await isar.dashboardResponses.put(chart);
         });
       }
     } on DioException catch (e) {
+      // Handle Dio exceptions and log the error
       print(e);
       AppLogger.instance.error(
         title: 'Dashboard Repository',
         message: '$e',
         stackTrace: e.stackTrace,
       );
-      rethrow;
+      rethrow; // Rethrow the exception
     }
   }
 }
