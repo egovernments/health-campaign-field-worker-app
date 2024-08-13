d1e2fc760f68e67a881e909bbad6de3072f7b30e
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2024-08-13 11:40:28 +0530

    Added Task address mapping on refused delivery (#514)

--- packages/registration_delivery/lib/pages/beneficiary/deliver_intervention.dart
+++ packages/registration_delivery/lib/pages/beneficiary/deliver_intervention.dart
@@ -625,6 +625,9 @@ class DeliverInterventionPageState
     task ??= TaskModel(
       projectBeneficiaryClientReferenceId: projectBeneficiaryClientReferenceId,
       clientReferenceId: clientReferenceId,
+      address: address?.copyWith(
+        relatedClientReferenceId: clientReferenceId,
+      ),
       tenantId: RegistrationDeliverySingleton().tenantId,
       rowVersion: 1,
       auditDetails: AuditDetails(
