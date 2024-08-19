6d79981496ea6f1fd8f1220415a28045e32f50db
yashita-egov <yashita.bansal@egovernments.org>
2024-08-19 10:10:43 +0530

    test added

---
+++ packages/complaints/test/constants/test_constants.dart
@@ -0,0 +1,41 @@
+import 'package:complaints/complaints.dart';
+import 'package:complaints/models/entities/complaint_types.dart';
+import 'package:digit_data_model/data_model.dart';
+import 'package:digit_data_model/models/entities/pgr_application_status.dart';
+
+class ComplaintsRegistrationConstants{
+  static final ComplaintsDetailsModel complaintsDetailsModel = ComplaintsDetailsModel(
+      dateOfComplaint: DateTime(2024),
+      administrativeArea: "abcd",
+      complaintRaisedFor: "myself",
+      complainantName: "test",
+      complainantContactNumber: "8888888",
+      complaintDescription: "test complaint"
+  );
+
+  static final BoundaryModel boundaryModel = BoundaryModel(
+    latitude: "76",
+    longitude: "88",
+  );
+
+  static const String complaintType = "test-type";
+
+  static final PgrAddressModel addressModel = PgrAddressModel(
+    street: "XYZ",
+    city: "ABC",
+  );
+
+  static var mockPgrServiceModel = PgrServiceModel(
+      clientReferenceId: '123',
+      tenantId: 'abc',
+      serviceCode: complaintType,
+      description: 'test-complaint',
+      applicationStatus: PgrServiceApplicationStatus.created,
+      user: PgrComplainantModel(
+        clientReferenceId: '123',
+        complaintClientReferenceId: '123',
+        tenantId: 'abc',
+      ),
+      address: addressModel
+  );
+}
