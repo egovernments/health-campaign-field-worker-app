7040916394036bdbae4076120f191fa2bf3a5208
Shashwat12-egov <shashwat.k@egovernments.org>
2024-08-23 12:16:27 +0530

    Cleaned the example project

--- packages/checklist/example/lib/main.dart
+++ packages/checklist/example/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:checklist/blocs/app_localization.dart';
 import 'package:checklist/blocs/checklist_localization_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:checklist/pages/acknowledgement.dart';
-import 'package:checklist/blocs/app_localization.dart' as checklist_localization;
 import 'package:flutter/services.dart';
 
 import 'localizedString.dart';
@@ -22,21 +21,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -96,27 +80,23 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (BuildContext context) => ChecklistAcknowledgementPage(
+                      appLocalizations: ChecklistLocalization(
+                          Locale("en","MZ"),
+                          localizedStrings,
+                          languages
+                      ),
+                    ))
+            );
+          },
+          tooltip: 'Open Checklist',
+          child: const Icon(Icons.open_in_browser_rounded),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (BuildContext context) => ChecklistAcknowledgementPage(
-                appLocalizations: ChecklistLocalization(
-                    Locale("en","MZ"),
-                    localizedStrings,
-                    languages
-                ),
-              ))
-          );
-        },
-        tooltip: 'Open Checklist',
-        child: const Icon(Icons.open_in_browser_rounded),
       ),
     );
   }
