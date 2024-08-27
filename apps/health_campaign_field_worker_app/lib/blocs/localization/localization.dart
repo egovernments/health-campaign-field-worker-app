c1924c45f07e0c9113735784e65f3d0e535d22e8
Tanishi Goyal <tanishi.goyal@egovernments.org>
2024-08-27 16:53:25 +0530

    moved survey_form override to pubspec

--- apps/health_campaign_field_worker_app/lib/blocs/localization/localization.dart
+++ apps/health_campaign_field_worker_app/lib/blocs/localization/localization.dart
@@ -3,7 +3,6 @@ import 'package:digit_data_model/data/local_store/sql_store/sql_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:isar/isar.dart';
 import '../../data/local_store/app_shared_preferences.dart';
 import '../../data/repositories/remote/localization.dart';
 import '../../utils/utils.dart';
