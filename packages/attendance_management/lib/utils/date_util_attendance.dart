a2361ecad677557fb0f3f0030ecbd3b9aacb23ac
Shashwat12-egov <shashwat.k@egovernments.org>
2024-09-10 14:42:18 +0530

    Added some date functions in date_util_attendence file and upgraded the digit components in manage_attendence,dart file

--- packages/attendance_management/lib/utils/date_util_attendance.dart
+++ packages/attendance_management/lib/utils/date_util_attendance.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
+
 class AttendanceDateTimeManagement {
   static int getMillisecondEpoch(
     DateTime k,
@@ -33,5 +36,35 @@ class AttendanceDateTimeManagement {
         return millisecondsSinceEpoch;
       }
     }
+  }
+
+  static String getDateString(DateTime date) {
+    final DateFormat formatter = DateFormat('yyyy-MM-dd');
+    return formatter.format(date);
+  }
+
+  // Function to parse the provided date string and return a DateTime object.
+  static DateTime? getFormattedDateToDateTime(String date) {
+    try {
+      DateFormat inputFormat;
+      inputFormat = date.contains('-')
+          ? DateFormat('dd-MM-yyyy')
+          : DateFormat('dd/MM/yyyy');
+      DateTime inputDate = inputFormat.parse(date);
+
+      return inputDate;
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+
+      return null;
+    }
+  }
+
+  // Function to get a formatted date string from the provided timestamp in milliseconds.
+  static String getDateFromTimestamp(int timestamp, {String? dateFormat}) {
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    return DateFormat(dateFormat ?? "dd/MM/yyyy").format(date);
   }
 }
