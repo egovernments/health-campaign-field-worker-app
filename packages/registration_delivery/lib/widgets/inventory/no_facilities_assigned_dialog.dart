03b5b383d23518577ced2507d96dee6dc9adb95b
yashita-egov <yashita.bansal@egovernments.org>
2024-09-19 14:26:53 +0530

    widgets updated

--- packages/registration_delivery/lib/widgets/inventory/no_facilities_assigned_dialog.dart
+++ packages/registration_delivery/lib/widgets/inventory/no_facilities_assigned_dialog.dart
@@ -1,27 +1,34 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:digit_components/digit_components.dart';
+import 'package:digit_ui_components/enum/app_enums.dart';
+import 'package:digit_ui_components/theme/digit_theme.dart';
+import 'package:digit_ui_components/widgets/atoms/digit_button.dart';
+import 'package:digit_ui_components/widgets/atoms/pop_up_card.dart';
 import 'package:flutter/material.dart';
 
 class NoFacilitiesAssignedDialog {
   static Future<void> show(BuildContext context) {
-    return DigitDialog.show(
-      context,
-      options: DigitDialogOptions(
+    return showDialog(
+      context: context,
+      builder: (ctx)=> Popup(
         titleIcon: Icon(
           Icons.warning,
-          color: Theme.of(context).colorScheme.error,
+          color: DigitTheme.instance.colors.light.alertError,
         ),
-        titleText: 'No facilities assigned',
-        contentText: 'Please select another boundary or '
+        title: 'No facilities assigned',
+        description: 'Please select another boundary or '
             'contact the system administrator to assign '
             'a facility.',
-        primaryAction: DigitDialogActions(
-          label: 'Close',
-          action: (dialogContext) {
-            Navigator.of(context, rootNavigator: true).pop();
-            context.router.maybePop();
-          },
-        ),
+        actions: [
+          Button(
+            label: 'Close',
+            type: ButtonType.primary,
+            size: ButtonSize.large,
+            onPressed: (){
+              Navigator.of(context, rootNavigator: true).pop();
+              context.router.maybePop();
+            },
+          ),
+        ],
       ),
     );
   }
