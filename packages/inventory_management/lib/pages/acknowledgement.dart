ac598555e881b4b8929fe91f4844e892a857390c
rachana-egov <rachana.singh@egovernment.org>
2024-12-11 15:42:26 +0530

    updated inventory package with new components

--- packages/inventory_management/lib/pages/acknowledgement.dart
+++ packages/inventory_management/lib/pages/acknowledgement.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:digit_components/digit_components.dart';
+import 'package:digit_ui_components/digit_components.dart';
+import 'package:digit_ui_components/widgets/molecules/digit_card.dart';
+import 'package:digit_ui_components/widgets/molecules/panel_cards.dart';
 import 'package:flutter/material.dart';
 import '../utils/i18_key_constants.dart' as i18;
 import '../widgets/localized.dart';
@@ -31,26 +33,48 @@ class AcknowledgementPageState
     final theme = Theme.of(context);
 
     return Scaffold(
-      body: DigitAcknowledgement.success(
+      body: PanelCard(
+        title:  widget.label ??
+            localizations.translate(
+              i18.acknowledgementSuccess.acknowledgementLabelText,
+            ),
+        type: PanelType.success,
         description: widget.description ??
             localizations.translate(
               i18.acknowledgementSuccess.acknowledgementDescriptionText,
             ),
-        descriptionWidget: widget.isDataRecordSuccess
-            ? DigitTableCard(
-                element: widget.descriptionTableData ?? {},
-              )
-            : null,
-        label: widget.label ??
-            localizations.translate(
-              i18.acknowledgementSuccess.acknowledgementLabelText,
-            ),
-        action: () {
-          context.router.maybePop();
-        },
-        enableBackToSearch: widget.isDataRecordSuccess ? false : true,
-        actionLabel:
-            localizations.translate(i18.acknowledgementSuccess.actionLabelText),
+        /// TODO: need to update this as listview card
+        // additionWidgets: widget.isDataRecordSuccess
+        //     ? DigitTableCard(
+        //         element: widget.descriptionTableData ?? {},
+        //       )
+        //     : null,
+        actions: [
+          DigitButton(
+              label: localizations.translate(i18.acknowledgementSuccess.actionLabelText),
+              onPressed: () {
+                context.router.maybePop();
+              },
+              type: DigitButtonType.primary,
+              size: DigitButtonSize.large),
+          if(!widget.isDataRecordSuccess)
+          DigitButton(
+              label: localizations
+                  .translate(i18.acknowledgementSuccess.actionLabelText),
+              onPressed: () {
+                final parent = context.router.parent() as StackRouter;
+                // Pop twice to navigate back to the previous screen
+                parent.popUntilRoot();
+              },
+              type: DigitButtonType.secondary,
+              size: DigitButtonSize.large),
+        ],
+        // action: () {
+        //   context.router.maybePop();
+        // },
+        // enableBackToSearch: widget.isDataRecordSuccess ? false : true,
+        // actionLabel:
+        //     localizations.translate(i18.acknowledgementSuccess.actionLabelText),
       ),
       bottomNavigationBar: Offstage(
         offstage: !widget.isDataRecordSuccess,
@@ -58,13 +82,13 @@ class AcknowledgementPageState
         child: SizedBox(
           height: 150,
           child: DigitCard(
-            margin: const EdgeInsets.fromLTRB(0, kPadding, 0, 0),
-            padding: const EdgeInsets.fromLTRB(kPadding, 0, kPadding, 0),
-            child: Column(
-              children: [
-                DigitElevatedButton(
-                  child: Text(localizations
-                      .translate(i18.acknowledgementSuccess.goToHome)),
+            margin: const EdgeInsets.fromLTRB(0, spacer2, 0, 0),
+            children: [
+                DigitButton(
+                  size: DigitButtonSize.large,
+                  type: DigitButtonType.secondary,
+                  label: localizations
+              .translate(i18.acknowledgementSuccess.goToHome),
                   onPressed: () {
                     context.router.popUntilRoot();
                   },
@@ -72,32 +96,19 @@ class AcknowledgementPageState
                 const SizedBox(
                   height: 12,
                 ),
-                DigitOutLineButton(
+                DigitButton(
+                  size: DigitButtonSize.large,
+                  type: DigitButtonType.primary,
                   onPressed: () {
                     context.router.popUntilRoot();
                   },
                   label: localizations
                       .translate(i18.acknowledgementSuccess.downloadmoredata),
-                  buttonStyle: OutlinedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.zero,
-                    ),
-                    side: BorderSide(
-                      width: 1.0,
-                      color: theme.colorScheme.secondary,
-                    ),
-                    minimumSize: Size(
-                      MediaQuery.of(context).size.width,
-                      50,
-                    ),
-                  ),
                 ),
               ],
             ),
           ),
         ),
-      ),
     );
   }
 }
