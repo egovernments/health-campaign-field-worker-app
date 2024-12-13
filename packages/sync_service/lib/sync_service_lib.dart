cc02ec3fbc048a67d6cbf8f779eb3718fb905e40
Naveen Renati <154230258+naveenr-egov@users.noreply.github.com>
2024-12-13 09:15:31 +0530

    reverted super annotations for sync package

--- packages/sync_service/lib/sync_service_lib.dart
+++ packages/sync_service/lib/sync_service_lib.dart
@@ -1,11 +1,10 @@
 library sync_service;
 
-export 'data/sync_entity_mapper_listener.dart';
-export 'data/sync_service.dart';
+export 'blocs/sync/sync.dart';
 export 'data/repositories/sync/remote_type.dart';
 export 'data/repositories/sync/sync_down.dart';
 export 'data/repositories/sync/sync_up.dart';
+export 'data/sync_entity_mapper_listener.dart';
+export 'data/sync_service.dart';
 export 'models/bandwidth/bandwidth_model.dart';
-export 'blocs/sync/sync.dart';
 export 'utils/utils.dart';
-export 'data/sync_service_annotation.dart';
