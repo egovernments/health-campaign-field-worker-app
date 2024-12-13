69c8d65198df505d6988c8d2cec254649b19d0c5
Naveen Renati <154230258+naveenr-egov@users.noreply.github.com>
2024-12-13 15:47:42 +0530

    removed creds

--- apps/health_campaign_field_worker_app/lib/pages/login.dart
+++ apps/health_campaign_field_worker_app/lib/pages/login.dart
@@ -229,12 +229,12 @@ class _LoginPageState extends LocalizedState<LoginPage> {
 
   FormGroup buildForm() => fb.group(<String, Object>{
         _userId: FormControl<String>(
-          value: 'USR-260849',
+          value: '',
           validators: [Validators.required],
         ),
         _password: FormControl<String>(
           validators: [Validators.required],
-          value: 'eGov@123',
+          value: '',
         ),
         _privacyCheck: FormControl<bool>(
           value: false,
