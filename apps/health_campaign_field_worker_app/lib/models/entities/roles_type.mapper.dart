7ca9fa6b1af8ca8b92cf176fc9552f924ed26960
amitverma-egov <amit.verma@egovernments.org>
2024-12-17 12:25:15 +0530

    community distributor role added

--- apps/health_campaign_field_worker_app/lib/models/entities/roles_type.mapper.dart
+++ apps/health_campaign_field_worker_app/lib/models/entities/roles_type.mapper.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, unnecessary_cast, override_on_non_overriding_member
+// ignore_for_file: unused_element, unnecessary_cast
 // ignore_for_file: strict_raw_type, inference_failure_on_untyped_parameter
 
 part of 'roles_type.dart';
@@ -41,6 +41,8 @@ class RolesTypeMapper extends EnumMapper<RolesType> {
         return RolesType.registrar;
       case "ATTENDANCE_STAFF":
         return RolesType.attendanceStaff;
+      case "COMMUNITY_DISTRIBUTOR":
+        return RolesType.communityDistributor;
       default:
         throw MapperException.unknownEnumValue(value);
     }
@@ -65,6 +67,8 @@ class RolesTypeMapper extends EnumMapper<RolesType> {
         return "REGISTRAR";
       case RolesType.attendanceStaff:
         return "ATTENDANCE_STAFF";
+      case RolesType.communityDistributor:
+        return "COMMUNITY_DISTRIBUTOR";
     }
   }
 }
