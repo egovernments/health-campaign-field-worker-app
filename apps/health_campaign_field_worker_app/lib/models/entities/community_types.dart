4f685b3f9ec74f78eda6a9fa2ccb37e5e3b2eb6f
amitverma-egov <amit.verma@egovernments.org>
2025-01-09 20:08:29 +0530

    added new enum to identify community types , like refugee or special groups

---
+++ apps/health_campaign_field_worker_app/lib/models/entities/community_types.dart
@@ -0,0 +1,12 @@
+// Generated using mason. Do not modify by hand
+import 'package:dart_mappable/dart_mappable.dart';
+
+part 'community_types.mapper.dart';
+
+@MappableEnum(caseStyle: CaseStyle.upperCase)
+enum CommunityTypes {
+  @MappableValue("refugee_camps")
+  refugeeCamps,
+  @MappableValue("special_groups")
+  specialGroups,
+}
