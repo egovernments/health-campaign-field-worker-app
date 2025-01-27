88aa68560d6b27d1ce9f8a833eaba21e21a247f1
Naveen Renati <154230258+naveenr-egov@users.noreply.github.com>
2025-01-27 11:59:38 +0530

    Fetching checklist by projectType instead of projectName

--- packages/registration_delivery/lib/widgets/member_card/member_card.dart
+++ packages/registration_delivery/lib/widgets/member_card/member_card.dart
@@ -335,7 +335,7 @@ class MemberCard extends StatelessWidget {
                                                       .code
                                                       .toString()
                                                       .contains(
-                                                          '${RegistrationDeliverySingleton().selectedProject!.name}.${RegistrationDeliveryEnums.eligibility.toValue()}'))
+                                                          '${RegistrationDeliverySingleton().selectedProject!.projectType}.${RegistrationDeliveryEnums.eligibility.toValue()}'))
                                                   .toList()
                                                   .isEmpty) {
                                                 context.router.push(
