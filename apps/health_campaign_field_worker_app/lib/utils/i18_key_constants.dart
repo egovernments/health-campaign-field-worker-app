343d2789e6b0c741967bd6ba1cae8f56cd28b0de
psbskb <susantakumar.behera@beehyv.com>
2025-02-14 19:31:05 +0530

    added localization to refugee camp

--- apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
@@ -999,6 +999,8 @@ class HouseholdLocation {
   String get postalCodeFormLabel => 'POSTAL_CODE_FORM_LABEL';
 
   String get actionLabel => 'HOUSEHOLD_LOCATION_ACTION_LABEL';
+
+  String get refugeeCampLabel => 'REFUGEE_CAMP_LABEL';
 }
 
 class SearchBeneficiary {
