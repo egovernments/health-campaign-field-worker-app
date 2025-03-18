9fb732b81d0ec40b566450d2b282946af1825a6b
psbskb <psb.skb22@gmail.com>
2025-03-18 18:35:21 +0530

    fix: update boundary name setting to prioritize code over name in context utility

--- apps/health_campaign_field_worker_app/lib/utils/extensions/context_utility.dart
+++ apps/health_campaign_field_worker_app/lib/utils/extensions/context_utility.dart
@@ -117,7 +117,8 @@ extension ContextUtilityExtensions on BuildContext {
 
     // INFO: Set Boundary for packages
     ComplaintsSingleton().setBoundary(boundary: selectedBoundary);
-    InventorySingleton().setBoundaryName(boundaryName: selectedBoundary.name!);
+    InventorySingleton().setBoundaryName(
+        boundaryName: selectedBoundary.code! ?? selectedBoundary.name!);
     RegistrationDeliverySingleton().setBoundary(boundary: selectedBoundary);
     ClosedHouseholdSingleton().setBoundary(boundary: selectedBoundary);
     SurveyFormSingleton().setBoundary(boundary: selectedBoundary);
