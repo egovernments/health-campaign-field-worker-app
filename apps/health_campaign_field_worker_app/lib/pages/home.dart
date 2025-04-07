6858cd43e1b622a887ac71ffeddbcb172e6c8164
rachana-egov <rachana.singh@egovernment.org>
2025-04-07 16:34:13 +0530

    fixing sync issue

--- apps/health_campaign_field_worker_app/lib/pages/home.dart
+++ apps/health_campaign_field_worker_app/lib/pages/home.dart
@@ -459,19 +459,19 @@ class _HomePageState extends LocalizedState<HomePage> {
               icon: Icons.sync_alt,
               label: i18.home.syncDataLabel,
               onPressed: () async {
-                // if (context.mounted) _attemptSyncUp(context);
-                if (snapshot.data?['enablesManualSync'] == true) {
-                  if (context.mounted) _attemptSyncUp(context);
-                } else {
-                  if (context.mounted) {
-                    Toast.showToast(
-                      context,
-                      message: localizations
-                          .translate(i18.common.coreCommonSyncInProgress),
-                      type: ToastType.success,
-                    );
-                  }
-                }
+                if (context.mounted) _attemptSyncUp(context);
+                // if (snapshot.data?['enablesManualSync'] == true) {
+                //   if (context.mounted) _attemptSyncUp(context);
+                // } else {
+                //   if (context.mounted) {
+                //     Toast.showToast(
+                //       context,
+                //       message: localizations
+                //           .translate(i18.common.coreCommonSyncInProgress),
+                //       type: ToastType.success,
+                //     );
+                //   }
+                // }
               },
             );
           },
