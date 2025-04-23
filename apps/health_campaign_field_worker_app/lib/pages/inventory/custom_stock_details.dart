3e8153ed83b44bcf028cf07301a2f6ac2d299b6a
satviktm7 <satvik.t@egovernments.org>
2025-04-23 11:39:33 +0530

    changed Driver Name to be mandatory

--- apps/health_campaign_field_worker_app/lib/pages/inventory/custom_stock_details.dart
+++ apps/health_campaign_field_worker_app/lib/pages/inventory/custom_stock_details.dart
@@ -87,7 +87,9 @@ class CustomStockDetailsPageState
       _vehicleNumberKey: FormControl<String>(),
       _typeOfTransportKey: FormControl<String>(),
       _driverNameKey: FormControl<String>(
-        validators: [],
+        validators: [
+          Validators.required,
+        ],
       ),
       _balesQuantityKey: FormControl<int>(
         validators: [
@@ -878,12 +880,11 @@ class CustomStockDetailsPageState
                                                           'deliveryTeam',
                                                           deliveryTeamName,
                                                         ),
-                                                      if (driverName != null &&
-                                                          driverName.isNotEmpty)
-                                                        AdditionalField(
-                                                          'driver_name',
-                                                          driverName,
-                                                        ),
+
+                                                      AdditionalField(
+                                                        'driver_name',
+                                                        driverName,
+                                                      ),
                                                       if (balesQuantity != null)
                                                         AdditionalField(
                                                             _balesQuantityKey,
@@ -1181,6 +1182,7 @@ class CustomStockDetailsPageState
                                                   )
                                                       .setValidators(
                                                     [
+                                                      Validators.required,
                                                       Validators.minLength(2),
                                                       Validators.maxLength(200),
                                                     ],
@@ -1244,6 +1246,7 @@ class CustomStockDetailsPageState
                                                     )
                                                         .setValidators(
                                                       [
+                                                        Validators.required,
                                                         Validators.minLength(2),
                                                         Validators.maxLength(
                                                             200),
@@ -1276,10 +1279,8 @@ class CustomStockDetailsPageState
                                                     );
 
                                                     form
-                                                        .control(
-                                                          _driverNameKey,
-                                                        )
-                                                        .touched;
+                                                        .control(_driverNameKey)
+                                                        .markAsTouched();
                                                     form
                                                         .control(
                                                           _waybillNumberKey,
@@ -1375,7 +1376,7 @@ class CustomStockDetailsPageState
                                                         _driverNameKey,
                                                       )
                                                           .setValidators(
-                                                        [],
+                                                        [Validators.required],
                                                         updateParent: true,
                                                         autoValidate: true,
                                                       );
@@ -1435,6 +1436,7 @@ class CustomStockDetailsPageState
                                                         )
                                                             .setValidators(
                                                           [
+                                                            Validators.required,
                                                             Validators
                                                                 .minLength(2),
                                                             Validators
@@ -1479,9 +1481,8 @@ class CustomStockDetailsPageState
 
                                                         form
                                                             .control(
-                                                              _driverNameKey,
-                                                            )
-                                                            .touched;
+                                                                _driverNameKey)
+                                                            .markAsTouched();
                                                         form
                                                             .control(
                                                               _waybillNumberKey,
@@ -1845,6 +1846,7 @@ class CustomStockDetailsPageState
                                       i18_local.stockDetailsReceiptShowcase
                                           .driverName,
                                     ),
+                                    isRequired: true,
                                     formControlName: _driverNameKey,
                                     validationMessages: {
                                       'required': (object) =>
