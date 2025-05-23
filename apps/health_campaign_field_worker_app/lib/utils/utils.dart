f4afbc995491520cf217e1f349ecc654e02d3ad7
rachana-egov <rachana.singh@egovernment.org>
2025-05-23 12:25:20 +0530

    Added sync failure error for project sync

--- apps/health_campaign_field_worker_app/lib/utils/utils.dart
+++ apps/health_campaign_field_worker_app/lib/utils/utils.dart
@@ -517,44 +517,52 @@ class LocalizationParams {
 // Transforms the input JSON into a new structure where each page's properties
 // are keyed by their fieldName and the main structure is flattened for use.
 Map<String, dynamic> transformJson(Map<String, dynamic> inputJson) {
-  final transformed = <String, dynamic>{};
+  try {
+    final transformed = <String, dynamic>{};
 
-  transformed['name'] = inputJson['name'];
-  transformed['pages'] = <String, dynamic>{};
-  transformed['version'] = inputJson['version'];
+    transformed['name'] = inputJson['name'];
+    transformed['pages'] = <String, dynamic>{};
+    transformed['version'] = inputJson['version'];
 
-  for (final page in inputJson['pages'] as List<dynamic>) {
-    final pageMap = page as Map<String, dynamic>;
-    final pageKey = pageMap['page'];
+    for (final page in inputJson['pages'] as List<dynamic>) {
+      final pageMap = page as Map<String, dynamic>;
+      final pageKey = pageMap['page'];
 
-    final Map<String, dynamic> properties = {};
+      final Map<String, dynamic> properties = {};
 
-    for (final prop in pageMap['properties'] as List<dynamic>) {
-      final property = prop as Map<String, dynamic>;
-      final fieldName = property['fieldName'];
-      if (fieldName != null) {
-        properties[fieldName] = Map<String, dynamic>.from(property);
+      for (final prop in pageMap['properties'] as List<dynamic>) {
+        final property = prop as Map<String, dynamic>;
+        final fieldName = property['fieldName'];
+        if (fieldName != null) {
+          properties[fieldName] = Map<String, dynamic>.from(property);
+        }
       }
+
+      final transformedPage = <String, dynamic>{
+        'label': pageMap['label'],
+        'order': pageMap['order'],
+        'type': pageMap['type'],
+        'format': pageMap['format'],
+        'description': pageMap['description'],
+        'actionLabel': pageMap['actionLabel'],
+        'properties': properties,
+        'value': pageMap['value'],
+        'required': pageMap['required'],
+        'hidden': pageMap['hidden'],
+        'helpText': pageMap['helpText'],
+        'innerLabel': pageMap['innerLabel']
+      };
+
+      (transformed['pages'] as Map<String, dynamic>)[pageKey] = transformedPage;
     }
 
-    final transformedPage = <String, dynamic>{
-      'label': pageMap['label'],
-      'order': pageMap['order'],
-      'type': pageMap['type'],
-      'description': pageMap['description'],
-      'actionLabel': pageMap['actionLabel'],
-      'properties': properties,
-      'value': pageMap['value'],
-      'required': pageMap['required'],
-      'hidden': pageMap['hidden'],
-      'helpText': pageMap['helpText'],
-      'innerLabel': pageMap['innerLabel']
-    };
-
-    (transformed['pages'] as Map<String, dynamic>)[pageKey] = transformedPage;
+    return transformed;
+  } catch (e, stackTrace) {
+    // Log and rethrow to propagate error to the outer try-catch
+    debugPrint('Error inside transformJson: $e');
+    debugPrint('$stackTrace');
+    rethrow;
   }
-
-  return transformed;
 }
 
 
