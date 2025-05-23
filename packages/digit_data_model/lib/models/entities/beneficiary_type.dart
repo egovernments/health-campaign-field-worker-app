c719d468f2ea464e9981c78a92ac78bdd62fe16f
nikhilesh <nikhilesh@beehyv.com>
2025-05-23 16:27:45 +0530

    add additional beneficiary types

--- packages/digit_data_model/lib/models/entities/beneficiary_type.dart
+++ packages/digit_data_model/lib/models/entities/beneficiary_type.dart
@@ -2,9 +2,32 @@
 import 'package:dart_mappable/dart_mappable.dart';
 
 part 'beneficiary_type.mapper.dart';
+
 @MappableEnum(caseStyle: CaseStyle.upperCase)
 enum BeneficiaryType {
-  @MappableValue("INDIVIDUAL") individual,
-  @MappableValue("HOUSEHOLD") household,
+  @MappableValue("INDIVIDUAL")
+  individual,
+  @MappableValue("HOUSEHOLD")
+  household,
+  @MappableValue("PRODUCT")
+  product,
+  @MappableValue("3-11MONTH")
+  individual1,
+  @MappableValue("12-59MONTH")
+  individual2,
+  @MappableValue("1-11MONTH")
+  individual3,
+  @MappableValue("6-11MONTH")
+  individual4,
+  @MappableValue("SPAQ1")
+  spaq1,
+  @MappableValue("SPAQ2")
+  spaq2,
+  @MappableValue("AZM")
+  azm,
+  @MappableValue("VAS-RED")
+  vasRed,
+  @MappableValue("VAS-BLUE")
+  vasBlue,
   ;
 }
