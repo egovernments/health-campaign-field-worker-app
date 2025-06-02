ca8559b52291a9c7c9a5951091665c93721b66a8
rachana-egov <rachana.singh@egovernment.org>
2025-06-02 15:50:09 +0530

    added enums in radio

--- packages/forms_engine/lib/widgets/radio_builder.dart
+++ packages/forms_engine/lib/widgets/radio_builder.dart
@@ -1,14 +1,20 @@
 part of 'json_schema_builder.dart';
 
 class JsonSchemaRadioBuilder extends JsonSchemaBuilder<bool> {
+  final List<Option> enums;
+  final bool isBoolean;
+
   const JsonSchemaRadioBuilder({
     required super.formControlName,
     required super.form,
+    this.enums = const [],
     super.key,
     super.value,
     super.label,
     super.readOnly,
+    this.isBoolean = false,
     super.validations,
+    super.tooltipText,
   });
 
   /// TODO: need to check if they are sending as list or a single radio button
@@ -23,10 +29,31 @@ class JsonSchemaRadioBuilder extends JsonSchemaBuilder<bool> {
       validationMessages: validationMessages,
       showErrors: (control) => control.invalid && control.touched,
       builder: (field){
-        return RadioList(
-          errorMessage: field.errorText,
-          radioDigitButtons: const [],
-          onChanged: (value) => field.control.value = value,
+        return LabeledField(
+          isRequired: hasRequiredValidation(validations),
+          label: label,
+          infoText: tooltipText,
+          child: RadioList(
+            readOnly: readOnly,
+            groupValue: form.control(formControlName).value.toString(),
+            errorMessage: field.errorText,
+            radioDigitButtons: enums
+                .map(
+                  (e) => RadioButtonModel(
+                code: e.code,
+                name: loc.translate(e.name),
+              ),
+            ).toList(),
+            onChanged: (value){
+              form.control(formControlName).markAsTouched();
+              if(isBoolean){
+                form.control(formControlName).value = value.code == 'true';
+              }else{
+                form.control(formControlName).value = value.code;
+              }
+
+            },
+          ),
         );
       },
     );
