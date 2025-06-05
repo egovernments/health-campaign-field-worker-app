a83124a3239a080c96dcda3530ffe0e746e46617
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2025-06-05 18:31:25 +0530

    added the custom Pop dialog for QRcode scanning and component for user details

--- apps/health_campaign_field_worker_app/lib/widgets/non_system_user/non_system_user_card.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/non_system_user/non_system_user_card.dart
@@ -1,35 +1,99 @@
 import 'package:digit_ui_components/digit_components.dart';
-import 'package:digit_ui_components/widgets/atoms/label_value_list.dart';
+import 'package:digit_ui_components/theme/TextTheme/digit_text_theme.dart';
+import 'package:digit_ui_components/theme/digit_extended_theme.dart';
+import 'package:digit_ui_components/widgets/atoms/text_block.dart';
 import 'package:digit_ui_components/widgets/molecules/digit_card.dart';
-import 'package:digit_ui_components/widgets/molecules/label_value_summary.dart';
 import 'package:flutter/material.dart';
+
 import '../localized.dart';
 
 class NonSystemUserCard extends LocalizedStatefulWidget {
-  const NonSystemUserCard({super.key});
+  final String userName;
+  final String role;
+  final String gender;
+  final String age;
+  final String? mobilenumber;
+  final void Function(dynamic value) onScanMe;
+
+  const NonSystemUserCard({
+    super.key,
+    required this.userName,
+    required this.role,
+    required this.gender,
+    required this.age,
+    this.mobilenumber,
+    required this.onScanMe,
+  });
 
   @override
-  State<NonSystemUserCard> createState() => _MyWidgetState();
+  State<NonSystemUserCard> createState() => _NonSystemUserCardState();
 }
 
-class _MyWidgetState extends LocalizedState<NonSystemUserCard> {
+class _NonSystemUserCardState extends LocalizedState<NonSystemUserCard> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.digitTextTheme(context);
+
     return DigitCard(
-        margin: const EdgeInsets.all(spacer2),
+      spacing: spacer2,
+      margin: const EdgeInsets.all(spacer2),
+      padding: const EdgeInsets.all(spacer3),
+      children: [
+        _buildCenteredTextBlock(widget.userName, textTheme.headingM),
+        _buildCenteredTextBlock(widget.role, textTheme.bodyL),
+        _buildCenteredTextBlock("${widget.gender}, ${widget.age}", textTheme.bodyL),
+        if (widget.mobilenumber != null)
+          _buildCenteredTextBlock("+91 ${widget.mobilenumber}", textTheme.bodyL),
+        _buildIdContainer(context, textTheme),
+        _buildQRButton(context),
+      ],
+    );
+  }
+
+  Widget _buildCenteredTextBlock(String text, TextStyle? style) {
+    return Center(
+      child: DigitTextBlock(
+        spacing: 0,
+        padding: EdgeInsets.zero,
+        description: text,
+        descriptionStyle: style,
+      ),
+    );
+  }
+
+  Widget _buildIdContainer(BuildContext context, DigitTextTheme textTheme) {
+    final theme = Theme.of(context);
+
+    return Container(
+      alignment: Alignment.center,
+      width: MediaQuery.sizeOf(context).width,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color(theme.colorTheme.paper.secondary.value),
+          width: 2,
+        ),
+        borderRadius: BorderRadius.circular(10),
+        color: Color(theme.colorTheme.paper.secondary.value),
+      ),
+      child: DigitTextBlock(
+        spacing: 0,
         padding: const EdgeInsets.all(spacer1),
-        children: [
-          const LabelValueSummary(items: [
-            LabelValueItem(labelFlex: 5, label: "name", value: "Prak"),
-            LabelValueItem(labelFlex: 5, label: "do", value: ""),
-          ]),
-          DigitButton(
-            label: "Scan Me",
-            mainAxisSize: MainAxisSize.max,
-            type: DigitButtonType.secondary,
-            size: DigitButtonSize.large,
-            onPressed: () {},
-          ),
-        ]);
+        description: "ID 56565656",
+        descriptionStyle: textTheme.label,
+      ),
+    );
+  }
+
+  Widget _buildQRButton(BuildContext context) {
+    return DigitButton(
+      capitalizeLetters: false,
+      type: DigitButtonType.secondary,
+      size: DigitButtonSize.large,
+      mainAxisSize: MainAxisSize.max,
+      onPressed: () => widget.onScanMe("data to be sent"),
+      prefixIcon: Icons.qr_code,
+      label: localizations.translate("Show QR Code"),
+    );
   }
 }
---
+++ apps/health_campaign_field_worker_app/lib/widgets/non_system_user/show_qr_code_non_system_user.dart
@@ -0,0 +1,83 @@
+import 'package:digit_ui_components/enum/app_enums.dart';
+import 'package:digit_ui_components/theme/TextTheme/digit_text_theme.dart';
+import 'package:digit_ui_components/theme/digit_extended_theme.dart';
+import 'package:digit_ui_components/theme/spacers.dart';
+import 'package:digit_ui_components/widgets/atoms/digit_button.dart';
+import 'package:digit_ui_components/widgets/atoms/pop_up_card.dart';
+import 'package:digit_ui_components/widgets/atoms/text_block.dart';
+import 'package:digit_ui_components/widgets/molecules/show_pop_up.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import '../../../utils/i18_key_constants.dart' as i18;
+import '../../blocs/localization/app_localization.dart';
+
+void showQRForNonSystemUser(
+    {required BuildContext context,
+    required AppLocalizations localizations,
+    required DigitTextTheme textTheme}) {
+  final theme = Theme.of(context);
+  showCustomPopup(
+      context: context,
+      builder: (ctx) {
+        return Popup(
+          actionAlignment: MainAxisAlignment.center,
+          type: PopUpType.simple,
+          onCrossTap: () {
+            Navigator.of(ctx).pop();
+          },
+          additionalWidgets: [
+            const Center(
+                child: DigitTextBlock(
+              subHeading: "Pitabash Choudhdry",
+            )),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.sizeOf(context).width,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color(theme.colorTheme.paper.secondary.value),
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(10),
+                color: Color(theme.colorTheme.paper.secondary.value),
+              ),
+              child: DigitTextBlock(
+                spacing: 0,
+                padding: const EdgeInsets.all(spacer1),
+                description: "ID 56565656",
+                descriptionStyle: textTheme.label,
+              ),
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: theme.colorTheme.primary.primary1,
+                  width: 2,
+                )),
+                child: QrImageView(
+                  data: "PitabshChoudjdhr23/03/2025",
+                  version: QrVersions.auto,
+                  size: 220.0,
+                ),
+              ),
+            )
+          ],
+          actions: [
+            DigitButton(
+              capitalizeLetters: false,
+              type: DigitButtonType.primary,
+              size: DigitButtonSize.large,
+              mainAxisSize: MainAxisSize.max,
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+              label: localizations.translate(
+                i18.common.coreCommonGoback,
+              ),
+            ),
+          ],
+          title: localizations.translate("QR Code"),
+        );
+      });
+}
