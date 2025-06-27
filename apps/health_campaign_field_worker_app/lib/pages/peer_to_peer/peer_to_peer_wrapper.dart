b01b0f61e07d66e79c3939b00b143d2e69ef205b
Naveen Renati <naveen.renati@egovernments.org>
2025-06-27 14:15:47 +0530

    Added new animation file
    Updating file change logic

--- apps/health_campaign_field_worker_app/lib/pages/peer_to_peer/peer_to_peer_wrapper.dart
+++ apps/health_campaign_field_worker_app/lib/pages/peer_to_peer/peer_to_peer_wrapper.dart
@@ -2,6 +2,7 @@ import 'package:digit_data_model/data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:registration_delivery/registration_delivery.dart';
+import 'package:survey_form/models/entities/service.dart';
 
 import '../../blocs/peer_to_peer/peer_to_peer.dart';
 import '../../models/downsync/downsync.dart';
@@ -22,23 +23,26 @@ class PeerToPeerWrapperPage extends StatelessWidget
     return BlocProvider(
         child: this,
         create: (_) => PeerToPeerBloc(
-            individualLocalRepository: context.read<
-                LocalRepository<IndividualModel, IndividualSearchModel>>(),
-            householdLocalRepository: context
-                .read<LocalRepository<HouseholdModel, HouseholdSearchModel>>(),
-            householdMemberLocalRepository: context.read<
-                LocalRepository<HouseholdMemberModel,
-                    HouseholdMemberSearchModel>>(),
-            projectBeneficiaryLocalRepository: context.read<
-                LocalRepository<ProjectBeneficiaryModel,
-                    ProjectBeneficiarySearchModel>>(),
-            taskLocalRepository:
-                context.read<LocalRepository<TaskModel, TaskSearchModel>>(),
-            sideEffectLocalRepository: context.read<
-                LocalRepository<SideEffectModel, SideEffectSearchModel>>(),
-            referralLocalRepository: context
-                .read<LocalRepository<ReferralModel, ReferralSearchModel>>(),
-            downSyncLocalRepository: context
-                .read<LocalRepository<DownsyncModel, DownsyncSearchModel>>()));
+              individualLocalRepository: context.read<
+                  LocalRepository<IndividualModel, IndividualSearchModel>>(),
+              householdLocalRepository: context.read<
+                  LocalRepository<HouseholdModel, HouseholdSearchModel>>(),
+              householdMemberLocalRepository: context.read<
+                  LocalRepository<HouseholdMemberModel,
+                      HouseholdMemberSearchModel>>(),
+              projectBeneficiaryLocalRepository: context.read<
+                  LocalRepository<ProjectBeneficiaryModel,
+                      ProjectBeneficiarySearchModel>>(),
+              taskLocalRepository:
+                  context.read<LocalRepository<TaskModel, TaskSearchModel>>(),
+              sideEffectLocalRepository: context.read<
+                  LocalRepository<SideEffectModel, SideEffectSearchModel>>(),
+              referralLocalRepository: context
+                  .read<LocalRepository<ReferralModel, ReferralSearchModel>>(),
+              downSyncLocalRepository: context
+                  .read<LocalRepository<DownsyncModel, DownsyncSearchModel>>(),
+              serviceLocalRepository: context
+                  .read<LocalRepository<ServiceModel, ServiceSearchModel>>(),
+            ));
   }
 }
