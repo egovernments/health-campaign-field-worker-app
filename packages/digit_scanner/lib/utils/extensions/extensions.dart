a9ecefeed3592a51153faf5b32da27740a61ac73
rachana-egov <rachana.singh@egovernment.org>
2025-07-15 15:50:25 +0530

    reverted scanner file changes

--- packages/digit_scanner/lib/utils/extensions/extensions.dart
+++
@@ -1,8 +0,0 @@
-// Extension to remove duplicates from a list based on a provided constraint.
-extension UniqueListItem<E> on List<E> {
-  void removeDuplicates<I>(I Function(E element) constraintMapper) {
-    final distinctList = map(constraintMapper).toSet();
-
-    retainWhere((element) => distinctList.remove(constraintMapper(element)));
-  }
-}
