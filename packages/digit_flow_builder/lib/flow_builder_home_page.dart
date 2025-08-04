88ebf2e6b2558b67db3a23cdbf26acde0e9995af
rachana-egov <rachana.singh@egovernment.org>
2025-08-04 14:05:30 +0530

    Added forms rendering for layout

--- packages/digit_flow_builder/lib/flow_builder_home_page.dart
+++ packages/digit_flow_builder/lib/flow_builder_home_page.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:auto_route/annotations.dart';
 import 'package:digit_crud_bloc/bloc/crud_bloc.dart';
 import 'package:digit_crud_bloc/utils/utils.dart';
+import 'package:digit_flow_builder/util/utils.dart';
 import 'package:digit_flow_builder/widgets/localized.dart';
+import 'package:digit_forms_engine/blocs/forms/forms.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,13 +23,29 @@ class FlowBuilderHomePage extends LocalizedStatefulWidget {
 
 class _FlowBuilderHomePageState extends State<FlowBuilderHomePage> {
   @override
-  void initState() {
-    super.initState();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final config = FlowRegistry.getByName(widget.pageName);
+    if (config?['screenType'] == 'FORM') {
+      final normalized = transformJson(config!);
+      final schemaJsonString = jsonEncode(normalized);
+      context
+          .read<FormsBloc>()
+          .add(FormsEvent.load(schemas: [schemaJsonString]));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final config = FlowRegistry.getByName(widget.pageName);
+    if (config?['screenType'] == 'FORM') {
+      /// TODO: NEED TO REMOVE TRANSFORMATION FROM HERE::: AS THIS WILL HAPPEN FROM MAIN APP
+      final normalized = transformJson(config!);
+      final schemaJsonString = jsonEncode(normalized);
+      context
+          .read<FormsBloc>()
+          .add(FormsEvent.load(schemas: [schemaJsonString]));
+    }
     if (config == null) return const Center(child: Text('Page not found'));
 
     return MultiBlocProvider(providers: [
