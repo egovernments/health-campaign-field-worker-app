4a4eb874b8114642b0ca4978fdd6d00183613308
rachana-egov <rachana.singh@egovernment.org>
2025-08-19 14:09:04 +0530

    modified config

--- apps/health_campaign_field_worker_app/lib/main.dart
+++ apps/health_campaign_field_worker_app/lib/main.dart
@@ -838,6 +838,89 @@ final List<Map<String, dynamic>> sampleFlows = [
             "APPONE_REGISTRATION_BENEFICIARYDETAILS_SCREEN_DESCRIPTION"
       }
     ],
+    "wrapperConfig": {
+      "wrapperName": "HouseholdWrapper",
+      "rootEntity": "HouseholdMemberModel",
+      "filters": [
+        {"field": "isHeadOfHousehold", "equals": true}
+      ],
+      "relations": [
+        {
+          "name": "household",
+          "entity": "HouseholdModel",
+          "match": {
+            "field": "clientReferenceId",
+            "equalsFrom": "householdClientReferenceId"
+          }
+        },
+        {
+          "name": "members",
+          "entity": "HouseholdMemberModel",
+          "match": {
+            "field": "householdClientReferenceId",
+            "equalsFrom": "household.clientReferenceId"
+          }
+        },
+        {
+          "name": "headOfHousehold",
+          "entity": "IndividualModel",
+          "match": {
+            "field": "clientReferenceId",
+            "equalsFrom": "HouseholdMemberModel.individualClientReferenceId"
+          }
+        },
+        {
+          "name": "individuals",
+          "entity": "IndividualModel",
+          "match": {
+            "field": "clientReferenceId",
+            "inFrom": "members.individualClientReferenceId"
+          }
+        },
+        {
+          "name": "projectBeneficiaries",
+          "entity": "ProjectBeneficiaryModel",
+          "match": {
+            "field": "beneficiaryClientReferenceId",
+            "equalsFrom": "household.clientReferenceId"
+          }
+        },
+        {
+          "name": "tasks",
+          "entity": "TaskModel",
+          "match": {
+            "field": "projectBeneficiaryClientReferenceId",
+            "inFrom": "projectBeneficiaries.clientReferenceId"
+          }
+        },
+        {
+          "name": "sideEffects",
+          "entity": "SideEffectModel",
+          "match": {
+            "field": "clientReferenceId",
+            "equalsFrom": "household.clientReferenceId"
+          }
+        },
+        {
+          "name": "referrals",
+          "entity": "ReferralModel",
+          "match": {
+            "field": "clientReferenceId",
+            "equalsFrom": "household.clientReferenceId"
+          }
+        }
+      ],
+      "searchConfig": {
+        "primary": "household",
+        "select": [
+          "individual",
+          "household",
+          "householdMember",
+          "projectBeneficiary",
+          "task"
+        ]
+      }
+    },
     "onSubmit": [
       {
         "actionType": "FETCH_TRANSFORMER_CONFIG",
@@ -967,7 +1050,17 @@ final List<Map<String, dynamic>> sampleFlows = [
             "equalsFrom": "household.clientReferenceId"
           }
         }
-      ]
+      ],
+      "searchConfig": {
+        "primary": "household",
+        "select": [
+          "individual",
+          "household",
+          "householdMember",
+          "projectBeneficiary",
+          "task"
+        ]
+      }
     },
     "body": [
       {
