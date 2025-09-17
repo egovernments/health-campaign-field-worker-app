1dde4685fb0b6d95526e363753119fd0226b4cbd
rachana-egov <rachana.singh@egovernment.org>
2025-09-17 12:12:47 +0530

    Added success screen

--- packages/digit_flow_builder/lib/widget_registry.dart
+++ packages/digit_flow_builder/lib/widget_registry.dart
@@ -1,7 +1,7 @@
 import 'package:digit_flow_builder/blocs/app_localization.dart';
+import 'package:digit_flow_builder/flow_builder.dart';
 import 'package:digit_flow_builder/utils/function_registry.dart';
 import 'package:digit_flow_builder/utils/interpolation.dart';
-import 'package:digit_flow_builder/utils/utils.dart';
 import 'package:digit_ui_components/digit_components.dart';
 import 'package:digit_ui_components/widgets/atoms/digit_search_bar.dart';
 import 'package:digit_ui_components/widgets/atoms/digit_tag.dart';
@@ -14,9 +14,7 @@ import 'package:digit_ui_components/widgets/molecules/label_value_summary.dart';
 import 'package:digit_ui_components/widgets/molecules/panel_cards.dart';
 import 'package:flutter/material.dart';
 
-import 'action_handler/action_config.dart';
 import 'blocs/flow_crud_bloc.dart';
-import 'layout_renderer.dart';
 
 /// Provides stateData, listIndex, item, screenKey automatically down the tree
 class CrudItemContext extends InheritedWidget {
@@ -476,10 +474,81 @@ class WidgetRegistry {
       final crudCtx = CrudItemContext.of(context);
       final label = json['label'] ?? '';
       final description = json['description'] ?? '';
+      // Extract actions from JSON
+      Map<String, dynamic>? primaryAction = json['primaryAction'];
+      Map<String, dynamic>? secondaryAction = json['secondaryAction'];
+
+      // Helper to trigger actions like in your provided snippet
+      void handleAction(Map<String, dynamic>? actionJson) {
+        if (actionJson == null) return;
+
+        final actionsList = actionJson['onAction'];
+
+        final stateData = (crudCtx?.item != null && crudCtx!.item!.isNotEmpty)
+            ? crudCtx.item
+            : crudCtx?.stateData?.rawState != null &&
+                    crudCtx!.stateData!.rawState.isNotEmpty
+                ? crudCtx.stateData!.rawState.first
+                : null;
+
+        final currentKey = getScreenKeyFromArgs(context) ?? '';
+
+        final navigationData =
+            FlowCrudStateRegistry().getNavigationParams(currentKey);
+        for (var actionMap in actionsList) {
+          var action = ActionConfig.fromJson(actionMap);
+
+          final navData = action.properties['data'] as List<dynamic>?;
+
+          if (navData != null &&
+              (stateData != null || navigationData != null)) {
+            final resolvedData = navData.map((entry) {
+              final key = entry['key'] as String;
+              final rawValue = entry['value'];
+              final resolvedValue = resolveValue(
+                  rawValue, {...stateData, ...(navigationData ?? {})});
+              return {
+                "key": key,
+                "value": resolvedValue,
+              };
+            }).toList();
+
+            action = ActionConfig(
+              action: action.action,
+              actionType: action.actionType,
+              properties: {
+                ...action.properties,
+                'data': resolvedData,
+              },
+              condition: action.condition,
+              actions: action.actions,
+            );
+          }
+
+          onAction(action);
+        }
+      }
+
       return PanelCard(
         title: label,
         type: PanelType.success,
         description: description,
+        actions: [
+          if (primaryAction != null)
+            DigitButton(
+              type: DigitButtonType.primary,
+              size: DigitButtonSize.large,
+              label: primaryAction['label'] ?? '',
+              onPressed: () => handleAction(json['primaryAction']),
+            ),
+          if (secondaryAction != null)
+            DigitButton(
+              type: DigitButtonType.secondary,
+              size: DigitButtonSize.large,
+              label: secondaryAction['label'] ?? '',
+              onPressed: () => handleAction(json['secondaryAction']),
+            ),
+        ],
       );
     });
 
