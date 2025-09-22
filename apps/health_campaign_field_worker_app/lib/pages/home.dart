1b89576e3583d383cbfe1da41e990704cbda79ac
rachana-egov <rachana.singh@egovernment.org>
2025-09-22 17:48:24 +0530

    Added new delivery card and modified some flows

--- apps/health_campaign_field_worker_app/lib/pages/home.dart
+++ apps/health_campaign_field_worker_app/lib/pages/home.dart
@@ -368,7 +368,7 @@ class _HomePageState extends LocalizedState<HomePage> {
           onPressed: () async {
             if (isTriggerLocalisation) {
               final moduleName =
-                  'hcm-registrationflow-${context.selectedProject.referenceID},hcm-deliveryflow-${context.selectedProject.referenceID}';
+                  'hcm-registrationflow-${context.selectedProject.referenceID}';
               triggerLocalization(module: moduleName);
               isTriggerLocalisation = false;
             }
@@ -382,22 +382,66 @@ class _HomePageState extends LocalizedState<HomePage> {
 
               final registrationSchemaEntry =
                   allSchemas['REGISTRATIONFLOW'] as Map<String, dynamic>?;
-              final deliverySchemaEntry =
-                  allSchemas['DELIVERYFLOW'] as Map<String, dynamic>?;
 
               final registrationSchemaData = registrationSchemaEntry?['data'];
-              final deliverySchemaData = deliverySchemaEntry?['data'];
 
-              if (registrationSchemaData != null ||
-                  deliverySchemaData != null) {
+              if (registrationSchemaData != null) {
                 // Extract templates from both schemas
                 final regTemplatesRaw = registrationSchemaData?['templates'];
-                final delTemplatesRaw = deliverySchemaData?['templates'];
 
                 final Map<String, dynamic> regTemplateMap =
                     regTemplatesRaw is Map<String, dynamic>
                         ? regTemplatesRaw
                         : {};
+
+                final templates = {
+                  for (final entry in {...regTemplateMap}.entries)
+                    entry.key: TemplateConfig.fromJson(
+                        entry.value as Map<String, dynamic>)
+                };
+
+                final registrationConfig = json.encode(registrationSchemaData);
+
+                RegistrationDeliverySingleton().setTemplateConfigs(templates);
+                RegistrationDeliverySingleton()
+                    .setRegistrationConfig(registrationConfig);
+              }
+            }
+            RegistrationDeliverySingleton()
+                .setHouseholdType(HouseholdType.family);
+
+            await context.router.push(const RegistrationDeliveryWrapperRoute());
+          },
+        ),
+      ),
+
+      i18.home.distributionLabel: homeShowcaseData.distributionPoint.buildWith(
+        child: HomeItemCard(
+          icon: Icons.person,
+          label: i18.home.distributionLabel,
+          onPressed: () async {
+            if (isTriggerLocalisation) {
+              final moduleName =
+                  'hcm-deliveryflow-${context.selectedProject.referenceID}';
+              triggerLocalization(module: moduleName);
+              isTriggerLocalisation = false;
+            }
+
+            final prefs = await SharedPreferences.getInstance();
+            final schemaJsonRaw = prefs.getString('app_config_schemas');
+
+            if (schemaJsonRaw != null) {
+              final allSchemas =
+                  json.decode(schemaJsonRaw) as Map<String, dynamic>;
+
+              final deliverySchemaEntry =
+                  allSchemas['DELIVERYFLOW'] as Map<String, dynamic>?;
+
+              final deliverySchemaData = deliverySchemaEntry?['data'];
+
+              if (deliverySchemaData != null) {
+                // Extract templates from both schemas
+                final delTemplatesRaw = deliverySchemaData?['templates'];
 
                 final Map<String, dynamic> delTemplateMap =
                     delTemplatesRaw is Map<String, dynamic>
@@ -405,18 +449,14 @@ class _HomePageState extends LocalizedState<HomePage> {
                         : {};
 
                 final templates = {
-                  for (final entry
-                      in {...regTemplateMap, ...delTemplateMap}.entries)
+                  for (final entry in {...delTemplateMap}.entries)
                     entry.key: TemplateConfig.fromJson(
                         entry.value as Map<String, dynamic>)
                 };
 
-                final registrationConfig = json.encode(registrationSchemaData);
                 final deliveryConfig = json.encode(deliverySchemaData);
 
                 RegistrationDeliverySingleton().setTemplateConfigs(templates);
-                RegistrationDeliverySingleton()
-                    .setRegistrationConfig(registrationConfig);
                 RegistrationDeliverySingleton()
                     .setDeliveryConfig(deliveryConfig);
               }
@@ -675,6 +715,8 @@ class _HomePageState extends LocalizedState<HomePage> {
       i18.home.beneficiaryIdLabel: homeShowcaseData.beneficiaryId.showcaseKey,
       i18.home.dataShare: homeShowcaseData.dataShare.showcaseKey,
       i18.home.db: homeShowcaseData.db.showcaseKey,
+      i18.home.distributionLabel:
+          homeShowcaseData.distributionPoint.showcaseKey,
     };
 
     final homeItemsLabel = <String>[
@@ -695,6 +737,7 @@ class _HomePageState extends LocalizedState<HomePage> {
       i18.home.beneficiaryIdLabel,
       i18.home.dataShare,
       i18.home.db,
+      i18.home.distributionLabel
     ];
 
     final List<String> filteredLabels = homeItemsLabel
