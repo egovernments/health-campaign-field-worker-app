eff72f4abe00d03652e7255acbaaff6295e4725c
Naveen Renati <naveen.renati@egovernments.org>
2025-11-04 16:02:48 +0530

    removed login creds

--- apps/health_campaign_field_worker_app/lib/pages/login.dart
+++ apps/health_campaign_field_worker_app/lib/pages/login.dart
@@ -234,12 +234,12 @@ class _LoginPageState extends LocalizedState<LoginPage> {
 
   FormGroup buildForm() => fb.group(<String, Object>{
         _userId: FormControl<String>(
-          value: 'USR-024475',
+          value: '',
           validators: [Validators.required],
         ),
         _password: FormControl<String>(
           validators: [Validators.required],
-          value: 'eGov@123',
+          value: '',
         ),
         _privacyCheck: FormControl<bool>(
           value: false,
