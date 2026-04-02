f638cfb07707eecdb8a431544e463dfc46f9560b
psbskb <susanta.behera@equidhi.org>
2026-04-02 15:30:08 +0530

    removed unused par in attendance_management package

--- packages/attendance_management/lib/attendance_management.dart
+++ packages/attendance_management/lib/attendance_management.dart
@@ -7,7 +7,4 @@ import 'package:dart_mappable/dart_mappable.dart';
 
 export 'blocs/app_localization.dart';
 export 'blocs/attendance_bloc.dart';
-export 'pages/manage_attendance.dart';
-export 'pages/mark_attendance.dart';
-export 'router/attendance_router.dart';
 export 'utils/utils.dart';
