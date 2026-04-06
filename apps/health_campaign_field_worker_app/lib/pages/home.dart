72d5150c9d07ac103c1976dd45161ff26a861c56
egov-skb <susanta.behera@egov.global>
2026-04-06 11:09:27 +0530

    refactor: removed hard coded attendance card

--- apps/health_campaign_field_worker_app/lib/pages/home.dart
+++ apps/health_campaign_field_worker_app/lib/pages/home.dart
@@ -3009,8 +3009,7 @@ class _HomePageState extends LocalizedState<HomePage> {
                 .map((e) => e.displayName)
                 .toList()
                 .contains(element) ||
-            element == i18.home.db ||
-            element == i18.home.manageAttendanceLabel)
+            element == i18.home.db)
         .toList();
 
     final showcaseKeys = filteredLabels
