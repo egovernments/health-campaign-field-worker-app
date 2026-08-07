6310cce271cd577df763124c137bfca0c2085465
Nikhil-07egov <nikhil.jose@egovernments.org>
2026-08-07 12:16:51 +0530

    Revert "perf(boundary): remove duplicate SyncRefreshEvent, consolidate localization to single _loadLocale per drill-down, remove loading:true from search"
    This reverts commit 0b22b662e79e76f604fd3d574d04e7a5f9d4aab2.

--- packages/digit_data_model/lib/blocs/boundary/boundary.dart
+++ packages/digit_data_model/lib/blocs/boundary/boundary.dart
@@ -69,6 +69,7 @@ class BoundaryBloc extends Bloc<BoundaryEvent, BoundaryState> {
     BoundarySearchEvent event,
     BoundaryEmitter emit,
   ) async {
+    emit(state.copyWith(loading: true));
     List<BoundaryModel> boundaryList = await boundaryRepository.search(
       BoundarySearchModel(
         codes: event.code,
